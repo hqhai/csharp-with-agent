c3a982babd1e407493dc1e39a9b5aa04b0a12858
@Tunghung641 <dohung642001@gmail.com>
2023-03-10 11:51:16 +0700

    video Model

--- Services/Fsel.Course/Fsel.Course.Common/Models/Entities/VideoModel.cs
+++ Services/Fsel.Course/Fsel.Course.Common/Models/Entities/VideoModel.cs
@@ -20,6 +20,42 @@ namespace Fsel.Course.Common.Models.Entities
 
         public Guid? TeacherId { get; set; }
 
+<<<<<<< Updated upstream
         public EnumCourseLevel CourseLevel { get; set; }
+=======
+        /// <summary>
+        /// Time code information
+        /// </summary>
+        ///
+        public Guid? VideoTimeCodeId { get; set; }
+
+        public long DisplayTimeTicks { get; set; }
+
+        public long ExecutionTimeTicks { get; set; }
+
+        public EnumTimeCodeType TimeCodeType { get; set; }
+
+        /// <summary>
+        /// Excercise  information
+        /// </summary>
+
+        public Guid ExcerciseId { get; set; }
+        public EnumCourseLevel CourseLevel { get; set; }
+
+        public string? ExcerciseName { get; set; }
+
+        public EnumQuestionType QuestionType { get; set; }
+
+        public string? MediaPost { get; set; }
+
+        /// <summary>
+        ///  Question information
+        /// </summary>
+        public Guid QuestionId { get; set; }
+
+        public EnumQuestionType EnumQuestionType { get; set; }
+
+        public string? Config { get; set; }
+>>>>>>> Stashed changes
     }
 }
