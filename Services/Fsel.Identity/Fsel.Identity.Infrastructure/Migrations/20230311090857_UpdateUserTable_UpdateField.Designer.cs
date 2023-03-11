d92cbcfe0b375c89bb3e3509b6da96ec89fb4bb4
Đô Nguyễn Thành <thanhdo@atlantic.edu.vn>
2023-03-11 16:10:25 +0700

    Change migration Identity User

--- Services/Fsel.Identity/Fsel.Identity.Infrastructure/Migrations/20230311090857_UpdateUserTable_UpdateField.Designer.cs
+++ Services/Fsel.Identity/Fsel.Identity.Infrastructure/Migrations/20230311090857_UpdateUserTable_UpdateField.Designer.cs
@@ -4,6 +4,7 @@ using Fsel.Identity.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fsel.Identity.Infrastructure.Migrations
 {
     [DbContext(typeof(UserDbContext))]
-    partial class UserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230311090857_UpdateUserTable_UpdateField")]
+    partial class UpdateUserTable_UpdateField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
