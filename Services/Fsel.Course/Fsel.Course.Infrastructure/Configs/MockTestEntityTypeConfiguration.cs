626fc6468d21c8d28866ea38c15940981eb7d28d
Đô Nguyễn Thành <thanhdo@atlantic.edu.vn>
2023-03-11 17:55:08 +0700

    Update config enum MockTest

---
+++ Services/Fsel.Course/Fsel.Course.Infrastructure/Configs/MockTestEntityTypeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Fsel.Course.Domain.Entities;
+using Fsel.Course.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Fsel.Course.Infrastructure.Configs
+{
+    public class MockTestEntityTypeConfiguration : IEntityTypeConfiguration<MockTest>
+    {
+        public void Configure(EntityTypeBuilder<MockTest> builder)
+        {
+            builder.Property(e => e.CourseType)
+                .HasMaxLength(100)
+                .HasConversion(
+                    v => v.ToString(),
+                    v => (EnumCourseType)Enum.Parse(typeof(EnumCourseType), v));
+
+            builder.Property(e => e.MockTestType)
+                .HasMaxLength(100)
+                .HasConversion(
+                    v => v.ToString(),
+                    v => (EnumMockTestType)Enum.Parse(typeof(EnumMockTestType), v));
+        }
+    }
+}
---
+++ Services/Fsel.Course/Fsel.Course.Infrastructure/Migrations/20230311105311_Update_MockTestEnumConfig.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fsel.Course.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_MockTestEnumConfig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MockTestType",
+                table: "MockTests",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CourseType",
+                table: "MockTests",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MockTestType",
+                table: "MockTests",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CourseType",
+                table: "MockTests",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
