2f54f2a0d00fd705616b0ef55f04ebf70147ed12
Phuc <phanminhphucdctb@gmail.com>
2023-03-15 13:14:13 +0700

    Update Human

--- Services/Fsel.Identity/Fsel.Identity.Application/Commands/AuthCmd/SignUpCommand.cs
+++ Services/Fsel.Identity/Fsel.Identity.Application/Commands/AuthCmd/SignUpCommand.cs
@@ -50,13 +50,13 @@ namespace Fsel.Identity.Application.Commands.AuthCmd
             MethodResult<UserModel> methodResult = new MethodResult<UserModel>();
 
             //Check User Exist
-            var userExit = await _userManager.FindByEmailAsync(request?.Email ?? string.Empty);
+            var userExit = await _userManager.FindByEmailAsync(request.Email ?? string.Empty);
             if (userExit != null)
             {
                 methodResult.StatusCode = StatusCodes.Status400BadRequest;
                 methodResult.AddErrorMessage(
                     nameof(EnumAuthErrorCode.AU04V),
-                    new[] { MethodHelper.GenerateErrorResult(nameof(request.Email), request?.Email) });
+                    new[] { MethodHelper.GenerateErrorResult(nameof(request.Email), request.Email) });
                 return methodResult;
             }
 
@@ -103,7 +103,7 @@ namespace Fsel.Identity.Application.Commands.AuthCmd
 
             await _senderService.SendEmailAsync(senderCommandModel);
 
-            var human = _mapper.Map<Human>(request.Human);
+            var human = _mapper.Map<Human>(request);
             if (request.Role == EnumRoleRegister.Student)
             {
                 human.Student = new Student
