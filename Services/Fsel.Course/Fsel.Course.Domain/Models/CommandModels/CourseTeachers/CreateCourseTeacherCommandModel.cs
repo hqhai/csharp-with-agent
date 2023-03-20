da54df8d491693c4da9824ec76d72a1bb47ed6d4
@Tunghung641 <dohung642001@gmail.com>
2023-03-20 08:52:08 +0700

    Update Course command

--- Services/Fsel.Course/Fsel.Course.Domain/Models/CommandModels/CourseTeachers/CreateCourseTeacherCommandModel.cs
+++ Services/Fsel.Course/Fsel.Course.Domain/Models/CommandModels/CourseTeachers/CreateCourseTeacherCommandModel.cs
@@ -3,9 +3,19 @@
 namespace Fsel.Course.Domain.Models.CommandModels.CourseTeachers
 {
     using System;
+    using Fsel.Course.Domain.Enums.ErrorCodes;
+    using System.ComponentModel.DataAnnotations;
 
     public class CreateCourseTeacherCommandModel
     {
         public Guid TeacherId { get; set; }
+
+        public string? Nationality { get; set; }
+
+        public string? Deggree { get; set; }
+
+        public string? Experience { get; set; }
+
+        public string? Strength { get; set; }
     }
 }
