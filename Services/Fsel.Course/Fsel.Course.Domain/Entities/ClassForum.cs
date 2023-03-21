b700beff765632ed673bcb66e673e0213e3b3fe4
Đô Nguyễn Thành <thanhdo@atlantic.edu.vn>
2023-03-21 18:39:30 +0700

    edit error enum

--- Services/Fsel.Course/Fsel.Course.Domain/Entities/ClassForum.cs
+++ Services/Fsel.Course/Fsel.Course.Domain/Entities/ClassForum.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using DataAnnotationsExtensions;
+using Fsel.Common.Enums;
 using Fsel.Common.Enums.ErrorCodes;
 using Fsel.Core.Entities;
 using Fsel.Course.Domain.Enums;
--- Services/Fsel.Course/Fsel.Course.Domain/Models/CommandModels/MockTests/UpdateMockTestCommandModel.cs
+++ Services/Fsel.Course/Fsel.Course.Domain/Models/CommandModels/MockTests/UpdateMockTestCommandModel.cs
@@ -3,6 +3,7 @@
 namespace Fsel.Course.Domain.Models.CommandModels.MockTests
 {
     using System.Collections.Generic;
+    using Fsel.Common.Enums;
     using Fsel.Course.Domain.Entities;
     using Fsel.Course.Domain.Enums;
 
