c5c5037813a823e152f65050f98162f5b821e5eb
@Tunghung641 <dohung642001@gmail.com>
2023-03-24 09:09:09 +0700

    update sln

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Services/StudentServices/IStudentService.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Services/StudentServices/IStudentService.cs
@@ -17,4 +17,3 @@ namespace Fsel.Course.Lms.Application.Services.StudentServices
         Task<IApiResponse<MethodResult<StudentModel>>> GetStudentByIdsAsync([Body] GetStudentByIdQueryModel command);
     }
 }
-}
