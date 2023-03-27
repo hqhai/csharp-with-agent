618e22a1e7f3184c5373625328a7617a022fe55d
Phuc <phanminhphucdctb@gmail.com>
2023-03-27 10:39:57 +0700

    Update code

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/UnitQuery/GetLessonByUnitQuery.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/UnitQuery/GetLessonByUnitQuery.cs
@@ -1,24 +1,16 @@
 // Copyright (c) Atlantic. All rights reserved.
 
-namespace Fsel.Course.Lms.Application.Queries
+namespace Fsel.Course.Lms.Application.Queries.UnitQuery
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
-    using AutoMapper;
     using Fsel.Common.ActionResults;
     using Fsel.Core.Base;
     using Fsel.Course.Domain.Enums.ErrorCodes;
-    using Fsel.Course.Domain.IRepositories;
     using Fsel.Course.Domain.Models.EntityModels;
     using Fsel.Course.Lms.Application.Services.StudentServices;
     using MediatR;
     using Microsoft.AspNetCore.Http;
-    using Microsoft.EntityFrameworkCore;
-    using Org.BouncyCastle.Math.EC.Rfc7748;
 
     public class GetLessonByUnitQuery : IRequest<MethodResult<LessonModel>>
     {
@@ -26,29 +18,24 @@ namespace Fsel.Course.Lms.Application.Queries
 
     public class GetLessonByUnitQueryHandler : IRequestHandler<GetLessonByUnitQuery, MethodResult<LessonModel>>
     {
-        private readonly IMapper _mapper;
         private readonly IStudentService _studentService;
-        private readonly IUnitLessonResultRepository _lessonStudentRepository;
+        private readonly ILessonStudentRepository _lessonStudentRepository;
         private readonly AuthContext _authContext;
-        private readonly ILessonRepository _lessonRepository;
 
-        public GetLessonByUnitQueryHandler(IMapper mapper,
+        public GetLessonByUnitQueryHandler(
             IStudentService studentService,
-            IUnitLessonResultRepository lessonStudentRepository,
+            ILessonStudentRepository lessonStudentRepository,
             AuthContext authContext,
             ILessonRepository lessonRepository
             )
         {
-            _mapper = mapper;
             _studentService = studentService;
-            _lessonStudentRepository = lessonStudentRepository;
             _authContext = authContext;
-            _lessonRepository = lessonRepository;
         }
 
         public async Task<MethodResult<LessonModel>> Handle(GetLessonByUnitQuery request, CancellationToken cancellationToken)
         {
-            MethodResult<LessonModel> methodResult = new MethodResult<LessonModel>();
+            var methodResult = new MethodResult<LessonModel>();
 
             var user = _authContext.CurrentUserId.ToString();
             var student = await _studentService.GetStudentByUserIdAsync(user);
@@ -57,13 +44,6 @@ namespace Fsel.Course.Lms.Application.Queries
                 methodResult.AddErrorBadRequest(nameof(EnumCourseErrorCode.NotStudent));
                 return methodResult;
             }
-            /*   var lessonStudent = await _lessonStudentRepository.Queryable.FirstOrDefault(x => x.StudentId)*/
-
-            /*   var lessonQuery = from i in _lessonRepository.Queryable
-                                 .Include(x => x.LessonStudents)*/
-            /*.ThenInclude(x => x.StudentId)*/
-
-            /*    .Include(x => x.UnitLessons).Where(x => x.)*/
             methodResult.StatusCode = StatusCodes.Status200OK;
             return methodResult;
         }
--- Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/UnitQuery/GetUnitByCourseQuery.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/UnitQuery/GetUnitByCourseQuery.cs
@@ -1,6 +1,6 @@
 // Copyright (c) Atlantic. All rights reserved.
 
-namespace Fsel.Course.Lms.Application.Queries
+namespace Fsel.Course.Lms.Application.Queries.UnitQuery
 {
     using System.Linq;
     using System.Threading.Tasks;
@@ -25,12 +25,12 @@ namespace Fsel.Course.Lms.Application.Queries
         private readonly IMapper _mapper;
         private readonly IStudentService _studentService;
         private readonly AuthContext _authContext;
-        private readonly ICourseClassStudentRepository _courseClassStudentRepository;
+        private readonly ICourseClassRepository _courseClassRepository;
 
         public GetUnitByCourseQueryHandler(IMapper mapper,
             AuthContext authContext,
             ICourseRepository courseRepository,
-            ICourseClassStudentRepository courseClassStudentRepository,
+            ICourseClassRepository courseClassRepository,
             IStudentService studentService)
         {
             _courseRepository = courseRepository;
@@ -42,7 +42,7 @@ namespace Fsel.Course.Lms.Application.Queries
 
         public async Task<MethodResult<CourseModel>> Handle(GetUnitByCourseQuery request, CancellationToken cancellationToken)
         {
-            MethodResult<CourseModel> methodResult = new MethodResult<CourseModel>();
+            var methodResult = new MethodResult<CourseModel>();
 
             var user = _authContext.CurrentUserId.ToString();
             var student = await _studentService.GetStudentByUserIdAsync(user);
@@ -51,7 +51,7 @@ namespace Fsel.Course.Lms.Application.Queries
                 methodResult.AddErrorBadRequest(nameof(EnumCourseErrorCode.NotStudent));
                 return methodResult;
             }
-            var classId = student.Content?.Result.ClassId;
+            var classId = student.Content?.Result?.ClassId;
 
             var courseClassStudent = await _courseClassStudentRepository.Queryable
                             .FirstOrDefaultAsync(x => x.ClassId == classId, cancellationToken: cancellationToken);
@@ -74,7 +74,7 @@ namespace Fsel.Course.Lms.Application.Queries
                                   Name = i.Name,
                                   CourseLevel = i.CourseLevel,
                                   CourseUnitMockTests = _mapper.Map<IList<CourseUnitMockTestModel>>(i.CourseUnitMockTests),
-                                  CourseClasses = _mapper.Map<IList<CourseClassStudentModel>>(i.CourseClassStudents),
+                                  CourseClasses = _mapper.Map<IList<CourseClassModel>>(i.CourseClasses),
                               };
             var course = courseQuery.FirstOrDefault();
             methodResult.Result = course;
