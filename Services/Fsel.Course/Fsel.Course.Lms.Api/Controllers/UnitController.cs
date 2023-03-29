48d7a5fb9c2e69fe09f7ecc10cdf40a8e4e54896
@Tunghung641 <dohung642001@gmail.com>
2023-03-29 14:28:42 +0700

    Update unit controller

--- Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/UnitController.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/UnitController.cs
@@ -13,40 +13,28 @@ namespace Fsel.Course.Lms.Api.Controllers
     using Microsoft.AspNetCore.Mvc;
 
     [ApiVersion(Settings.APIVersion)]
-    [Route(Settings.APIDefaultRoute + "/lesson")]
+    [Route(Settings.APIDefaultRoute + "/unit")]
     [Authorize(Roles = nameof(EnumRole.Student))]
     [ApiController]
-    public class LessonController : ControllerBase
+    public class UnitController : ControllerBase
     {
         private readonly IMediator _mediator;
 
-        public LessonController(IMediator mediator)
+        public UnitController(IMediator mediator)
         {
             _mediator = mediator;
         }
 
         /// <summary>
-        /// Get List Lesson
+        /// Get List Unit
         /// </summary>
-        [HttpGet("{unitId}")]
+        [HttpGet("{id}")]
         [ProducesResponseType(typeof(MethodResult<IList<LessonModel>>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
-        public async Task<IActionResult> GetListLesson([FromRoute] Guid Id)
+        public async Task<IActionResult> GetListUnit([FromRoute] Guid id)
         {
             MethodResult<IList<LessonModel>> queryResult = await _mediator.Send(new GetListLessonQuery
-            { UnitId = Id }).ConfigureAwait(false);
-            return queryResult.GetActionResult();
-        }
-
-        /// <summary>
-        /// Get Lesson score
-        /// </summary>
-        [HttpGet("get-lesson-score")]
-        [ProducesResponseType(typeof(MethodResult<LessonModel>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
-        public async Task<IActionResult> GetLessonScore([FromQuery] GetLessonScoreQuery query)
-        {
-            MethodResult<LessonScoreModel> queryResult = await _mediator.Send(query).ConfigureAwait(false);
+            { UnitId = id }).ConfigureAwait(false);
             return queryResult.GetActionResult();
         }
     }
