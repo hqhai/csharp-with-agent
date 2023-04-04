5621ba555ebadc7e23c6e20cd24f7e8a6daa4107
Phuc <phanminhphucdctb@gmail.com>
2023-04-04 10:25:25 +0700

    Update code

--- Services/Fsel.Course/Fsel.Course.Application/Commands/LessonCmd/UpdateLessonCommand.cs
+++ Services/Fsel.Course/Fsel.Course.Application/Commands/LessonCmd/UpdateLessonCommand.cs
@@ -49,7 +49,7 @@ namespace Fsel.Course.Application.Commands.LessonCmd
 
             #region Validation
 
-            var lesson = await _lessonRepository.GetByIdAsync(request.Id);
+            var lesson = await _lessonRepository.GetIncludeByIdAsync(request.Id);
             if (lesson == null)
             {
                 methodResult.AddErrorBadRequest(nameof(EnumLessonErrorCode.LessonNotExist), nameof(request.Id), request.Id);
