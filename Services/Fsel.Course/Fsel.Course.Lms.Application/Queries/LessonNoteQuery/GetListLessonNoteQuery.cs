69d33c73bbe19b63174557a27a48d23234c7f9de
@Tunghung641 <dohung642001@gmail.com>
2023-04-04 16:19:20 +0700

    fix list lesson note

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/LessonNoteQuery/GetListLessonNoteQuery.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/LessonNoteQuery/GetListLessonNoteQuery.cs
@@ -45,11 +45,14 @@ namespace Fsel.Course.Lms.Application.Queries.LessonNoteQuery
                                             Note = lessonResult.Note,
                                             LessonResultId = lessonResult.LessonResultId
                                         }).ToListAsync(cancellationToken: cancellationToken);
-            var lessonResult = await _lessonResultRepository.Queryable.Where(x => x.Id == request.LessonResultId).Select(x => x.SummaryNote).FirstOrDefaultAsync(cancellationToken: cancellationToken);
-            lessonNoteListModel.LessonNotes = lessonNotes;
-            lessonNoteListModel.SummaryNote = lessonResult;
-            lessonNoteListModel.LessonResultId = request.LessonResultId;
 
+            var lessonResult = await _lessonResultRepository.Queryable.Where(x => x.Id == request.LessonResultId).FirstOrDefaultAsync(cancellationToken: cancellationToken);
+            if (lessonResult != null)
+            {
+                lessonNoteListModel.SummaryNote = lessonResult.SummaryNote;
+            }
+            lessonNoteListModel.LessonResultId = request.LessonResultId;
+            lessonNoteListModel.LessonNotes = lessonNotes;
             methodResult.Result = lessonNoteListModel;
             methodResult.StatusCode = StatusCodes.Status200OK;
             return methodResult;
