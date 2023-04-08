db677c5e90d03df4a4e6a5a6cae1c4be2ecfd0c5
Phuc <phanminhphucdctb@gmail.com>
2023-04-08 14:30:21 +0700

    Update code

---
+++ Services/Fsel.Course/Fsel.Course.Infrastructure/Migrations/20230408072524_createHomeWordResult.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fsel.Course.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class createHomeWordResult : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HomeWorkResult",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UpdatedUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletedUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CreatedFullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    UpdatedFullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    DeletedFullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    Percent = table.Column<double>(type: "float", nullable: false),
+                    CorrectCount = table.Column<int>(type: "int", nullable: false),
+                    CorrectTotal = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    StudentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    HomeWorkId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LessonResultId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HomeWorkResult", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HomeWorkResult_HomeWorks_HomeWorkId",
+                        column: x => x.HomeWorkId,
+                        principalTable: "HomeWorks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HomeWorkResult_LessonResults_LessonResultId",
+                        column: x => x.LessonResultId,
+                        principalTable: "LessonResults",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HomeWorkAnswer",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UpdatedUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletedUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CreatedFullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    UpdatedFullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    DeletedFullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    Answer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CorrectCount = table.Column<int>(type: "int", nullable: false),
+                    HomeWorkQuestionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    HomeWorkResultId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HomeWorkAnswer", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HomeWorkAnswer_HomeWorkQuestions_HomeWorkQuestionId",
+                        column: x => x.HomeWorkQuestionId,
+                        principalTable: "HomeWorkQuestions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HomeWorkAnswer_HomeWorkResult_HomeWorkResultId",
+                        column: x => x.HomeWorkResultId,
+                        principalTable: "HomeWorkResult",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HomeWorkAnswer_HomeWorkQuestionId",
+                table: "HomeWorkAnswer",
+                column: "HomeWorkQuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HomeWorkAnswer_HomeWorkResultId",
+                table: "HomeWorkAnswer",
+                column: "HomeWorkResultId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HomeWorkResult_HomeWorkId",
+                table: "HomeWorkResult",
+                column: "HomeWorkId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HomeWorkResult_LessonResultId",
+                table: "HomeWorkResult",
+                column: "LessonResultId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HomeWorkAnswer");
+
+            migrationBuilder.DropTable(
+                name: "HomeWorkResult");
+        }
+    }
+}
