3e83528f5149700deaf25a1b1eebd0ba63f39ab2
Đô Nguyễn Thành <thanhdo@atlantic.edu.vn>
2023-04-13 10:28:59 +0700

    Update video result table

---
+++ Services/Fsel.Course/Fsel.Course.Infrastructure/Migrations/20230413032827_Update_VideoResultTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fsel.Course.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_VideoResultTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Feedback",
+                table: "VideoResults",
+                type: "nvarchar(max)",
+                maxLength: 100000000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "NumberOfStars",
+                table: "VideoResults",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Feedback",
+                table: "VideoResults");
+
+            migrationBuilder.DropColumn(
+                name: "NumberOfStars",
+                table: "VideoResults");
+        }
+    }
+}
