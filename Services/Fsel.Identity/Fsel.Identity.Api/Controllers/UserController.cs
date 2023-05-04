15b433b68868353234e33406c04e5115bd981b90
Phuc-PMP-38 <phanminhphucdctb@gmail.com>
2023-05-04 18:19:32 +0700

    Update code fix name active user

--- Services/Fsel.Identity/Fsel.Identity.Api/Controllers/UserController.cs
+++ Services/Fsel.Identity/Fsel.Identity.Api/Controllers/UserController.cs
@@ -127,7 +127,7 @@ namespace Fsel.Identity.Api.Controllers
         }
 
         /// <summary>
-        /// Get User
+        /// G User
         /// </summary>
         [Authorize]
         [HttpGet("get-user")]
@@ -157,7 +157,8 @@ namespace Fsel.Identity.Api.Controllers
         [Authorize(Roles = nameof(EnumRole.Admin))]
         [HttpPost("active-user")]
         [ProducesResponseType(typeof(MethodResult<UserModel>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]        public async Task<IActionResult> ActiveUser([FromBody] UpdateStatusUserCommand command)
+        [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> ActiveUser([FromBody] UpdateStatusUserCommand command)
         {
             MethodResult<UserModel> commandResult = await _mediator.Send(command).ConfigureAwait(false);
             return commandResult.GetActionResult();
