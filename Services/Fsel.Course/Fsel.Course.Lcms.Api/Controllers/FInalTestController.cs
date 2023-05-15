2cc08f989e8647003b81c17d23d21ffed5353d87
@Tunghung641 <dohung642001@gmail.com>
2023-05-15 16:25:55 +0700

    f

--- Services/Fsel.Course/Fsel.Course.Lcms.Api/Controllers/FInalTestController.cs
+++ Services/Fsel.Course/Fsel.Course.Lcms.Api/Controllers/FInalTestController.cs
@@ -9,12 +9,15 @@ namespace Fsel.Course.Lcms.Api.Controllers
     using Fsel.Course.Application.Commands.FinalTestCmd;
     using Fsel.Course.Application.Queries.FinalTestQuery;
     using Fsel.Course.Domain.Models.EntityModels;
+    using Fsel.Shared.Enums;
     using MediatR;
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
 
     [ApiVersion(Settings.APIVersion)]
     [Route(Settings.APIDefaultRoute + "/final-test")]
     [ApiController]
+    [Authorize(Roles = nameof(EnumRole.MasterAdmin))]
     public class FInalTestController : ControllerBase
     {
         private readonly IMediator _mediator;
