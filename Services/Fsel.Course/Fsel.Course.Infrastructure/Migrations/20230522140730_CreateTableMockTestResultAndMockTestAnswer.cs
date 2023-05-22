afd072a5903be3fc109c9c5df44f959629bfb2fb
@Tunghung641 <dohung642001@gmail.com>
2023-05-22 21:08:10 +0700

    update data base

---
+++ Services/Fsel.Course/Fsel.Course.Infrastructure/Migrations/20230522140730_CreateTableMockTestResultAndMockTestAnswer.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fsel.Course.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableMockTestResultAndMockTestAnswer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MockTestResults",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UpdatedUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletedUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CreatedFullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    UpdatedFullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    DeletedFullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    Percent = table.Column<double>(type: "float", nullable: false),
+                    CorrectCount = table.Column<int>(type: "int", nullable: false),
+                    CorrectTotal = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    CourseId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UnitId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MockTestId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StudentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MockTestResults", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MockTestResults_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MockTestResults_MockTests_MockTestId",
+                        column: x => x.MockTestId,
+                        principalTable: "MockTests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MockTestResults_Units_UnitId",
+                        column: x => x.UnitId,
+                        principalTable: "Units",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MockTestAnswer",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UpdatedUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletedUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CreatedFullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    UpdatedFullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    DeletedFullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    AnswerStr = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CorrectCount = table.Column<int>(type: "int", nullable: false),
+                    SectionQuestionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MockTestResultId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MockTestAnswer", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MockTestAnswer_MockTestResults_MockTestResultId",
+                        column: x => x.MockTestResultId,
+                        principalTable: "MockTestResults",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MockTestAnswer_SectionQuestions_SectionQuestionId",
+                        column: x => x.SectionQuestionId,
+                        principalTable: "SectionQuestions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MockTestAnswer_MockTestResultId",
+                table: "MockTestAnswer",
+                column: "MockTestResultId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MockTestAnswer_SectionQuestionId",
+                table: "MockTestAnswer",
+                column: "SectionQuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MockTestResults_CourseId",
+                table: "MockTestResults",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MockTestResults_MockTestId",
+                table: "MockTestResults",
+                column: "MockTestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MockTestResults_UnitId",
+                table: "MockTestResults",
+                column: "UnitId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MockTestAnswer");
+
+            migrationBuilder.DropTable(
+                name: "MockTestResults");
+        }
+    }
+}
