226c134a3d095b91b931fd42aa0cbff4af18df35
@Tunghung641 <dohung642001@gmail.com>
2023-05-29 15:21:02 +0700

    fix

--- Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/UnitModel.cs
+++ Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/UnitModel.cs
@@ -17,6 +17,6 @@ namespace Fsel.Course.Domain.Models.EntityModels
 
         public IList<LessonModel>? Lessons { get; set; }
 
-        public MockTestModel? SkillMockTests { get; set; }
+        public MockTestModel? SkillMockTest { get; set; }
     }
 }
