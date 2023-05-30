f6d27905679dcb7c796465a75842fb95d8cc2ae8
@Tunghung641 <dohung642001@gmail.com>
2023-05-30 09:48:15 +0700

    update

--- Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/ClassForumResultController.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/ClassForumResultController.cs
@@ -2,16 +2,16 @@
 
 namespace Fsel.Course.Lms.Api.Controllers
 {
-    using Fsel.Common.ActionResults;
     using System.Net;
+    using Fsel.Common.ActionResults;
     using Fsel.Common.Constants;
     using Fsel.Course.Domain.Models.EntityModels;
+    using Fsel.Course.Lms.Application.Commands.ClassForumCmd;
+    using Fsel.Course.Lms.Application.Queries.ClassForumResultQuery;
     using Fsel.Shared.Enums;
     using MediatR;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
-    using Fsel.Course.Lms.Application.Commands.ClassForumCmd;
-    using Fsel.Course.Lms.Application.Queries.ClassForumQuery;
 
     [ApiVersion(Settings.APIVersion)]
     [Route(Settings.APIDefaultRoute + "/class-forum-result")]
@@ -37,6 +37,7 @@ namespace Fsel.Course.Lms.Api.Controllers
             MethodResult<ClassForumResultModel> commandResult = await _mediator.Send(command).ConfigureAwait(false);
             return commandResult.GetActionResult();
         }
+
         /// <summary>
         /// Get Class Forum Result
         /// </summary>
