0397ff0692e722686273f3b0395c3b7d4f87c495
@Tunghung641 <dohung642001@gmail.com>
2023-06-06 10:41:22 +0700

    delete unused field

--- Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/ClassForumFileModel.cs
+++ Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/ClassForumFileModel.cs
@@ -5,11 +5,8 @@ namespace Fsel.Course.Domain.Models.EntityModels
     using System;
     using Fsel.Core.Base.BaseModels;
 
-    public class ClassForumFileModel : BaseModel
+    public class ClassForumFileModel
     {
         public string? FilePath { get; set; }
-
-        public Guid ClassForumId { get; set; }
-        public ClassForumModel? ClassForum { get; set; }
     }
 }
