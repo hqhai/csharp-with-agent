f4488c1d5007d1a770e87e2a6e4c74bfd1252ed5
@Tunghung641 <dohung642001@gmail.com>
2023-06-07 08:49:56 +0700

    update comment

--- Services/Fsel.Interaction/Fsel.Interaction.Application/Services/UserServices/IUserService.cs
+++ Services/Fsel.Interaction/Fsel.Interaction.Application/Services/UserServices/IUserService.cs
@@ -12,24 +12,6 @@ namespace Fsel.Interaction.Application.Services.UserServices
 
     public interface IUserService
     {
-        [Get("/student/get-by-user-id/{id}")]
-        Task<IApiResponse<MethodResult<StudentModel>>> GetStudentByUserIdAsync([FromRoute] Guid id);
-
-        [Get("/student/get-student-by-class-id/{id}")]
-        Task<IApiResponse<MethodResult<IList<StudentModel>>>> GetStudentByClassIdAsync([Body] Guid id);
-
-        [Get("/student/get-class-has-too-many-students/{id}")]
-        Task<IApiResponse<MethodResult<bool>>> GetStudentByClassIdCheckAsync([FromRoute] Guid id);
-
-        [Post("/teacher/get-by-ids")]
-        Task<IApiResponse<MethodResult<IList<TeacherModel>>>> GetTeacherByIdsAsync([Body] GetTeacherByIdsQueryModel command);
-
-        [Post("/teacher/get-by-id/{id}")]
-        Task<IApiResponse<MethodResult<TeacherModel>>> GetTeacherByIdAsync([Body] Guid id);
-
-        [Put("/student/update-student-level")]
-        Task<IApiResponse<MethodResult<bool>>> UpdateStudentByLevelAsync([Body] UpdateStudentByLevelModel command);
-
         [Get("/student/get-by-ids")]
         Task<IApiResponse<MethodResult<IList<StudentModel>>>> GetStudentByUserIdsAsync([FromRoute] IList<Guid> ids);
     }
