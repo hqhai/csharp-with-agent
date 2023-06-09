b3a6c875b8d0d072b164d6e8312bc8c00c7449c5
nguyenhuukhoa5462 <nguyenhuukhoa5462@gmail.com>
2023-06-09 14:44:24 +0700

    update app setting

---
+++ Services/Fsel.Ordering/Fsel.Ordering.Application/Services/UserService/Models/UpdateStudentByClassIdModel.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Ordering.Application.Services.UserService.Models
+{
+    using System;
+
+    public class UpdateStudentByClassIdModel
+    {
+        public Guid? ClassId { get; set; }
+        public Guid StudentId { get; set; }
+        public Guid? PackageId { get; set; }
+    }
+}
