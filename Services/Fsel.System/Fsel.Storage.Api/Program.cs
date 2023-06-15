ed339a65e6802cdff19c645f98c5b99f124cedfa
@Tunghung641 <dohung642001@gmail.com>
2023-06-15 17:19:17 +0700

    fix project

--- Services/Fsel.System/Fsel.Storage.Api/Program.cs
+++ Services/Fsel.System/Fsel.Storage.Api/Program.cs
@@ -1,27 +1,25 @@
 // Copyright (c) Atlantic. All rights reserved.
 
+using Fsel.Common.ValueSettings;
+using Fsel.Core.Extensions;
+using Fsel.System.Domain.IRepositories;
+using Fsel.System.Infrastructure;
+using Fsel.System.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var appSetting = builder.AddAppSettings<BaseAppSetting>();
+builder.AddServices();
+builder.AddSwaggerGens(appSetting);
+builder.AddAuthenticationJwtBearers(appSetting);
+builder.AddDbContexts<SystemDbContext>();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ILiveTimeFrameRepository, LiveTimeFrameRepository>();
+builder.Services.AddScoped<ICourseTimeConfigRepository, CourseTimeConfigRepository>();
 
+/*builder.AddRefitClients(typeof(IUserService), appSetting?.Services?.UserApiUrl);*/
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+app.UseServices();
 app.Run();
