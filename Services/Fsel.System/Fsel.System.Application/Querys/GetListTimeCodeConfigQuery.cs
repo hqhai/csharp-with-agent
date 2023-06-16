36ce4f76ce16c94095f96f820c63b6574cae9486
@Tunghung641 <dohung642001@gmail.com>
2023-06-16 11:33:52 +0700

    update

--- Services/Fsel.System/Fsel.System.Application/Querys/GetListTimeCodeConfigQuery.cs
+++ Services/Fsel.System/Fsel.System.Application/Querys/GetListTimeCodeConfigQuery.cs
@@ -1,14 +1,44 @@
-﻿// Copyright (c) Atlantic. All rights reserved.
+// Copyright (c) Atlantic. All rights reserved.
 
 namespace Fsel.System.Application.Querys
 {
+    using AutoMapper;
+    using Fsel.Common.ActionResults;
+    using Fsel.Core.Base.BaseModels;
+    using Fsel.System.Domain.IRepositories;
+    using Fsel.System.Domain.Models;
     using global::System;
-    using global::System.Collections.Generic;
-    using global::System.Linq;
-    using global::System.Text;
     using global::System.Threading.Tasks;
+    using MediatR;
+    using Microsoft.AspNetCore.Http;
 
-    internal class GetListTimeCodeConfigQuery
+    public class GetListTimeCodeConfigQuery : BaseQueryModel, IRequest<MethodResult<PagingItemsModel<CourseTimeConfigModel>>>
     {
+    }
+
+    public class GetListTimeCodeConfigQueryHandler : IRequestHandler<GetListTimeCodeConfigQuery, MethodResult<PagingItemsModel<CourseTimeConfigModel>>>
+    {
+        private readonly ICourseTimeConfigRepository _courseTimeConfigRepository;
+        private readonly IMapper _mapper;
+
+        public GetListTimeCodeConfigQueryHandler(ICourseTimeConfigRepository courseTimeConfigRepository, IMapper mapper)
+        {
+            _courseTimeConfigRepository = courseTimeConfigRepository;
+            _mapper = mapper;
+        }
+
+        public Task<MethodResult<PagingItemsModel<CourseTimeConfigModel>>> Handle(GetListTimeCodeConfigQuery request, CancellationToken cancellationToken)
+        {
+            ArgumentNullException.ThrowIfNull(request);
+
+            var methodResult = new MethodResult<PagingItemsModel<CourseTimeConfigModel>>();
+            methodResult.Result = new PagingItemsModel<CourseTimeConfigModel>();
+
+            if (request.PageSize > 100)
+            {
+                methodResult.StatusCode = StatusCodes.Status400BadRequest;
+                return methodResult;
+            }
+        }
     }
 }
