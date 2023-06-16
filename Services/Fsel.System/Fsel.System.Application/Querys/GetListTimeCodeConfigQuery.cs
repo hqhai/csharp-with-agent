0e0984c1200ae9ce31f9617a70d6f1add97fbfb8
@Tunghung641 <dohung642001@gmail.com>
2023-06-16 11:33:42 +0700

    update

---
+++ Services/Fsel.System/Fsel.System.Application/Querys/GetListTimeCodeConfigQuery.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.System.Application.Querys
+{
+    using global::System;
+    using global::System.Collections.Generic;
+    using global::System.Linq;
+    using global::System.Text;
+    using global::System.Threading.Tasks;
+
+    internal class GetListTimeCodeConfigQuery
+    {
+    }
+}
