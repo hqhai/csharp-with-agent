45fa33f5c3448e448ee72bbd6bb73bc5c7bc9723
@Tunghung641 <dohung642001@gmail.com>
2023-06-19 11:11:14 +0700

    update

--- Services/Fsel.Training/Fsel.Training.Domain/Enums/EnumClassLiveStatus.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Atlantic. All rights reserved.
-
-namespace Fsel.Training.Domain.Enums
-{
-    public enum EnumClassLiveStatus
-    {
-        WaitForConfirmation, Scheduled, AskToTeacherInstead
-    }
-}
