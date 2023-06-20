bb27a383aab2f949173093c67e8a19edf08ef853
@Tunghung641 <dohung642001@gmail.com>
2023-06-20 13:50:09 +0700

    update

---
+++ Services/Fsel.Training/Fsel.Training.Domain/Models/EntityModels/ClassStudentModel.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Training.Domain.Models.EntityModels
+{
+    using System;
+
+    public class ClassStudentModel
+    {
+        public Guid ClassId { get; set; }
+        public Guid StudentId { get; set; }
+    }
+}
