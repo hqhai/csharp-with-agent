396e50b55059a105a6da19d3b6ba8787c96d5e97
@Tunghung641 <dohung642001@gmail.com>
2023-06-21 18:44:27 +0700

    update api set month

--- Services/Fsel.System/Fsel.System.Application/Commands/CourseTimeConfigCmd/SetMonthToClassCommand.cs
+++ Services/Fsel.System/Fsel.System.Application/Commands/CourseTimeConfigCmd/SetMonthToClassCommand.cs
@@ -5,7 +5,6 @@ namespace Fsel.System.Application.Commands.CourseTimeConfigCmd
     using AutoMapper;
     using Fsel.Common.ActionResults;
     using Fsel.System.Domain.Entities;
-    using Fsel.System.Domain.Enums.ErrorCodes;
     using Fsel.System.Domain.IRepositories;
     using Fsel.System.Domain.Models;
     using Fsel.System.Domain.Models.CommandModels.CourseTimeConfigs;
@@ -34,31 +33,28 @@ namespace Fsel.System.Application.Commands.CourseTimeConfigCmd
             ArgumentNullException.ThrowIfNull(request);
             MethodResult<CourseTimeConfigModel> methodResult = new MethodResult<CourseTimeConfigModel>();
 
-            #region Validation
-
-            var courseTimeConfig = await _courseTimeConfigRepository.Queryable.FirstOrDefaultAsync(cancellationToken);
-            if (courseTimeConfig == null)
-            {
-                methodResult.AddErrorBadRequest(nameof(EnumCourseTimeConfigErrorCode.CourseTimeConfigNotEmpty), nameof(request.Id), request.Id);
-                return methodResult;
-            }
-
-            #endregion Validation
-
-            _mapper.Map(request, courseTimeConfig);
             await _courseTimeConfigRepository.ExecuteTransactionAsync(async () =>
             {
-                if (request.CourseId.HasValue)
+                var courseTimeConfig = await _courseTimeConfigRepository.Queryable.FirstOrDefaultAsync(x => x.CourseId == request.CourseId, cancellationToken);
+                if (courseTimeConfig != null)
                 {
+                    _mapper.Map(request, courseTimeConfig);
                     courseTimeConfig = _courseTimeConfigRepository.Update(courseTimeConfig);
                 }
-                else
+                else if (courseTimeConfig == null)
                 {
+                    courseTimeConfig = _mapper.Map<CourseTimeConfig>(request);
+                    if (!courseTimeConfig.IsValid())
+                    {
+                        methodResult.AddErrorBadRequest(courseTimeConfig.ErrorMessages);
+                        return methodResult;
+                    }
+                    var lastTimeWeek = await _courseTimeConfigRepository.Queryable.FirstOrDefaultAsync(cancellationToken);
+
+                    courseTimeConfig.EnrollmentWeek = lastTimeWeek?.EnrollmentWeek ?? 0;
                     courseTimeConfig = _courseTimeConfigRepository.Add(courseTimeConfig);
                 }
-
                 await _courseTimeConfigRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken).ConfigureAwait(false);
-
                 methodResult.StatusCode = StatusCodes.Status201Created;
                 methodResult.Result = _mapper.Map<CourseTimeConfigModel>(courseTimeConfig);
                 return methodResult;
