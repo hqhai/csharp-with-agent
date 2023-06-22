71eb61792dd416f7f555708ba6a16733e7ae8d35
nguyenhuukhoa5462 <nguyenhuukhoa5462@gmail.com>
2023-06-22 11:26:01 +0700

    feature create class by admin

---
+++ Services/Fsel.Training/Fsel.Training.Application/Commands/ClassCmd/CreateClassCommand.cs
@@ -0,0 +1,68 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Training.Application.Commands.ClassCmd
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AutoMapper;
+    using Fsel.Common.ActionResults;
+    using Fsel.Training.Application.Queries.ClassQuery;
+    using Fsel.Training.Domain.Entities;
+    using Fsel.Training.Domain.Enums.ErrorCodes;
+    using Fsel.Training.Domain.IRepositories;
+    using Fsel.Training.Domain.Models.CommandModels.Classes;
+    using Fsel.Training.Domain.Models.EntityModels;
+    using MediatR;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.EntityFrameworkCore;
+
+    public class CreateClassCommand : CreateClassCommandModel, IRequest<MethodResult<ClassModel>>
+    {
+    }
+
+    public class CreateClassCommandHandler : IRequestHandler<CreateClassCommand, MethodResult<ClassModel>>
+    {
+        private readonly IClassRepository _classRepository;
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+
+        public CreateClassCommandHandler(IClassRepository classRepository, IMapper mapper, IMediator mediator)
+        {
+            _classRepository = classRepository;
+            _mapper = mapper;
+            _mediator = mediator;
+        }
+
+        public async Task<MethodResult<ClassModel>> Handle(CreateClassCommand request, CancellationToken cancellationToken)
+        {
+            ArgumentNullException.ThrowIfNull(request);
+            MethodResult<ClassModel> methodResult = new MethodResult<ClassModel>();
+
+            var generateClassCode = await _mediator.Send(new GetNewClassCodeQuery { CourseLevel = request.CourseLevel, Code = request.CourseName }, cancellationToken).ConfigureAwait(false);
+            var checkClassCode = await _classRepository.Queryable.FirstOrDefaultAsync(p => p.Code == generateClassCode.Result, cancellationToken);
+            if(checkClassCode != null)
+            {
+                methodResult.AddErrorBadRequest(nameof(EnumClassErrorCode.CodeClassAlreadyExist));
+                return methodResult;
+            }
+
+            await _classRepository.ExecuteTransactionAsync(async () =>
+            {
+                var newClass = _mapper.Map<Class>(request);
+                if (!newClass.IsValid())
+                {
+                    methodResult.AddErrorBadRequest(newClass.ErrorMessages);
+                    return methodResult;
+                }
+                newClass.Code = generateClassCode.Result;
+                newClass.Name = generateClassCode.Result;
+                newClass = _classRepository.Add(newClass);
+                await _classRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken).ConfigureAwait(false);
+                methodResult.StatusCode = StatusCodes.Status201Created;
+                methodResult.Result = _mapper.Map<ClassModel>(newClass);
+                return methodResult;
+            });
+            return methodResult;
+        }
+    }
+}
