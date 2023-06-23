596e67e23c8354129f94df5128b983dd706ec15f
Đô Nguyễn Thành <thanhdo@atlantic.edu.vn>
2023-06-23 13:57:32 +0700

    Update error code

--- Services/Fsel.Interaction/Fsel.Interaction.Application/Services/UserServices/IUserService.cs
+++ Services/Fsel.Interaction/Fsel.Interaction.Application/Services/UserServices/IUserService.cs
@@ -6,12 +6,13 @@ namespace Fsel.Interaction.Application.Services.UserServices
     using System.Threading.Tasks;
     using Fsel.Common.ActionResults;
     using Fsel.Interaction.Application.Services.UserServices.Models;
+    using Microsoft.AspNetCore.Mvc;
     using Refit;
 
     public interface IUserService
     {
-        [Get("/student/get-by-ids")]
-        Task<IApiResponse<MethodResult<IList<StudentModel>>>> GetStudentByUserIdsAsync([FromRoute] IList<Guid> ids);
+        [Post("/student/get-by-user-ids")]
+        Task<IApiResponse<MethodResult<IList<StudentModel>>>> GetStudentByUserIdsAsync([Body] IList<string> ids);
 
         [Get("/student/get-by-user-id/{id}")]
         Task<IApiResponse<MethodResult<StudentModel>>> GetStudentByUserIdAsync([FromRoute] Guid id);
