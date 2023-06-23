436fefa11fe9e827661212673d89a370cc9de5a7
nguyenhuukhoa5462 <nguyenhuukhoa5462@gmail.com>
2023-06-23 13:26:15 +0700

    migration add classId into order table

--- Services/Fsel.Ordering/Fsel.Ordering.Infrastructure/Migrations/20230623062415_Edit_OrderTable_Add_ClassId.Designer.cs
+++ Services/Fsel.Ordering/Fsel.Ordering.Infrastructure/Migrations/20230623062415_Edit_OrderTable_Add_ClassId.Designer.cs
@@ -4,6 +4,7 @@ using Fsel.Ordering.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fsel.Ordering.Infrastructure.Migrations
 {
     [DbContext(typeof(OrderingDbContext))]
-    partial class OrderingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230623062415_Edit_OrderTable_Add_ClassId")]
+    partial class Edit_OrderTable_Add_ClassId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
