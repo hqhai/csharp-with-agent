01b36640708bdef6823deca76262c07b609f41ce
@Tunghung641 <dohung642001@gmail.com>
2023-06-27 13:52:36 +0700

    update api

---
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/MockTestResultQuery/GetMocktestReportQuery.cs
@@ -0,0 +1,69 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Course.Lms.Application.Queries.MockTestResultQuery
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Fsel.Common.ActionResults;
+    using Fsel.Core.Base;
+    using Fsel.Course.Domain.Enums.ErrorCodes;
+    using Fsel.Course.Domain.IRepositories;
+    using Fsel.Course.Domain.Models.EntityModels;
+    using Fsel.Course.Lms.Application.Services.UserServices;
+    using MediatR;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.EntityFrameworkCore;
+
+    public class GetMocktestReportQuery : IRequest<MethodResult<IList<MockTestResultModel>>>
+    {
+        public Guid? MockTestResultId { get; set; }
+    }
+
+    public class GetMocktestReportQueryHandler : IRequestHandler<GetMocktestReportQuery, MethodResult<IList<MockTestResultModel>>>
+    {
+        private readonly IMockTestResultRepository _mockTestResultRepository;
+        private readonly AuthContext _authContext;
+        private readonly IUserService _userService;
+
+        public GetMocktestReportQueryHandler(IMockTestResultRepository mockTestResultRepository, AuthContext authContext, IUserService userService)
+        {
+            _mockTestResultRepository = mockTestResultRepository;
+            _authContext = authContext;
+            _userService = userService;
+        }
+
+        public async Task<MethodResult<IList<MockTestResultModel>>> Handle(GetMocktestReportQuery request, CancellationToken cancellationToken)
+        {
+            ArgumentNullException.ThrowIfNull(request);
+            MethodResult<IList<MockTestResultModel>> methodResult = new MethodResult<IList<MockTestResultModel>>();
+
+            var student = await _userService.GetStudentByUserIdAsync(_authContext.CurrentUserId);
+            if (!student.IsSuccessStatusCode)
+            {
+                methodResult.AddErrorBadRequest(nameof(EnumLessonErrorCode.UserNotExist));
+                return methodResult;
+            }
+            var studentId = student?.Content?.Result?.Id;
+            var mockTestResultQuery = await _mockTestResultRepository.Queryable
+                                    .Where(x => x.Id == request.MockTestResultId)
+                                    .Select(x => new MockTestResultModel
+                                    {
+                                        Id = x.Id,
+                                        Percent = x.Percent,
+                                        CorrectCount = x.CorrectCount,
+                                        CorrectTotal = x.CorrectTotal,
+                                        CourseId = x.CourseId,
+                                        CreatedDate = x.CreatedDate,
+                                        SkillScores = x.SkillScores,
+                                        Status = x.Status,
+                                        StudentId = x.StudentId,
+                                    }).ToListAsync(cancellationToken);
+
+            methodResult.Result = mockTestResultQuery;
+            methodResult.StatusCode = StatusCodes.Status200OK;
+            return methodResult;
+        }
+    }
+}
