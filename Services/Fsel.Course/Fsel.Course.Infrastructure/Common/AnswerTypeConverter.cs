f6d82f301249a87d15453d61f01123330de2b0c5
Phuc-PMP-38 <phanminhphucdctb@gmail.com>
2023-06-28 16:02:05 +0700

    Update code fix commit

--- Services/Fsel.Course/Fsel.Course.Infrastructure/Common/AnswerTypeConverter.cs
+++ Services/Fsel.Course/Fsel.Course.Infrastructure/Common/AnswerTypeConverter.cs
@@ -239,13 +239,13 @@ namespace Fsel.Course.Infrastructure.Common
                 string[] questionWords = words[index].Split('|');
                 foreach (var item in questionWords)
                 {
-                    if (word == item)
+                    if (word.Trim() == item.Trim())
                     {
                         return true;
                     }
                 }
             }
-            else if (words[index] == word)
+            else if (words[index].Trim() == word.Trim())
             {
                 return true;
             }
