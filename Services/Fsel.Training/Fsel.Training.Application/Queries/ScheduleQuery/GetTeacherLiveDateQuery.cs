cfabd68cacc26ee7626a913ba1bd5d64ac33ce9e
@Tunghung641 <dohung642001@gmail.com>
2023-06-29 11:40:46 +0700

    update api

--- Services/Fsel.Training/Fsel.Training.Application/Queries/ScheduleQuery/GetTeacherLiveDateQuery.cs
+++ Services/Fsel.Training/Fsel.Training.Application/Queries/ScheduleQuery/GetTeacherLiveDateQuery.cs
@@ -3,47 +3,89 @@
 namespace Fsel.Training.Application.Queries.ScheduleQuery
 {
     using System;
+    using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
+    using AutoMapper;
     using Fsel.Common.ActionResults;
-    using Fsel.Training.Application.Queries.ClassQuery;
+    using Fsel.Training.Application.Services.SystemServices;
+    using Fsel.Training.Application.Services.UserServices;
+    using Fsel.Training.Domain.Enums.ErrorCodes;
     using Fsel.Training.Domain.IRepositories;
     using Fsel.Training.Domain.Models.EntityModels;
+    using Fsel.Training.Domain.Models.QueryModels;
     using MediatR;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.EntityFrameworkCore;
 
-    public class GetTeacherLiveDateQuery : IRequest<MethodResult<IList<TeacherFreeTimeModel>>>
+    public class GetTeacherLiveDateQuery : GetTeacherLiveDateQueryModel, IRequest<MethodResult<TeacherFreeDateModel>>
     {
     }
 
-    public class GetTeacherLiveDateQueryHandler : IRequestHandler<GetTeacherLiveDateQuery, MethodResult<IList<TeacherFreeTimeModel>>>
+    public class GetTeacherLiveDateQueryHandler : IRequestHandler<GetTeacherLiveDateQuery, MethodResult<TeacherFreeDateModel>>
     {
         private readonly ITeacherFreeTimeRepository _teacherFreeTimeRepository;
+        private readonly ITeacherFreeDateRepository _teacherFreeDateRepository;
         private readonly IClassRepository _classRepository;
+        private readonly ISystemService _systemService;
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
 
-        public async Task<MethodResult<IList<TeacherFreeTimeModel>>> Handle(GetTeacherLiveDateQuery request, CancellationToken cancellationToken)
+        public GetTeacherLiveDateQueryHandler(ITeacherFreeTimeRepository teacherFreeTimeRepository, IClassRepository classRepository, ISystemService systemService, IUserService userService, ITeacherFreeDateRepository teacherFreeDateRepository, IMapper mapper)
+        {
+            _teacherFreeTimeRepository = teacherFreeTimeRepository;
+            _classRepository = classRepository;
+            _systemService = systemService;
+            _userService = userService;
+            _teacherFreeDateRepository = teacherFreeDateRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<MethodResult<TeacherFreeDateModel>> Handle(GetTeacherLiveDateQuery request, CancellationToken cancellationToken)
         {
             ArgumentNullException.ThrowIfNull(request);
-            MethodResult<IList<TeacherFreeTimeModel>> methodResult = new MethodResult<IList<TeacherFreeTimeModel>>();
+            MethodResult<TeacherFreeDateModel> methodResult = new MethodResult<TeacherFreeDateModel>();
 
-            var classQuery = await _classRepository.Queryable
-                                    .Select(x => new ClassModel
-                                    {
-                                        Id = x.Id,
-                                        LiveTimeFrameId = x.LiveTimeFrameId,
-                                        LiveDays = x.LiveDays,
-                                    }).FirstOrDefaultAsync(cancellationToken);
+            var @class = await _classRepository.GetByIdAsync(request.ClassId);
 
-            var teacherFreeTimeQuery = await _teacherFreeTimeRepository.Queryable
-                                    .Select(x => new TeacherFreeTimeModel
-                                    {
-                                        Id = x.Id,
-                                        LiveTimeFrameId = x.LiveTimeFrameId,
-                                        DayOfWeek = x.DayOfWeek,
-                                    }).ToListAsync(cancellationToken);
+            if (@class == null)
+            {
+                methodResult.AddErrorBadRequest(nameof(EnumClassErrorCode.ClassesNotExits), nameof(@class));
+                return methodResult;
+            }
+            if (@class.TeacherId.HasValue)
+            {
+                methodResult.AddErrorBadRequest(nameof(EnumClassErrorCode.ClassesNotExits), nameof(@class));
+                return methodResult;
+            }
+            if (@class.StartDate == null || @class.EndDate == null)
+            {
+                methodResult.AddErrorBadRequest(nameof(EnumClassErrorCode.ClassesNotExits), nameof(@class));
+                return methodResult;
+            }
+            if (@class.LiveDays == null || @class.LiveDays.Count == 0)
+            {
+                methodResult.AddErrorBadRequest(nameof(EnumClassErrorCode.ClassesNotExits), nameof(@class));
+                return methodResult;
+            }
+
+            var teacherFreeDates = await _teacherFreeDateRepository.Queryable
+                                    .Include(x => x.TeacherFreeTimes)
+                                    .Where(x => x.StartDate.Date <= @class.StartDate.Value.Date && x.EndDate.Date >= @class.EndDate.Value.Date)
+                                    .ToArrayAsync(cancellationToken);
+
+            var teacherFreeDate = teacherFreeDates.FirstOrDefault(x => x.TeacherFreeTimes.All(n => @class.LiveDays.Contains(n.DayOfWeek) && @class.LiveTimeFrameId == n.LiveTimeFrameId));
+            var teacherFreeDateModel = _mapper.Map<TeacherFreeDateModel>(teacherFreeDate);
+
+            var teacherResult = await _userService.GetTeacherByIdAsync(teacherFreeDateModel.TeacherId);
+            var teacher = teacherResult.Content?.Result;
+            teacherFreeDateModel.TeacherName = teacher?.Human?.FullName;
+
+            methodResult.Result = teacherFreeDateModel;
+            methodResult.StatusCode = StatusCodes.Status200OK;
+            return methodResult;
         }
     }
 }
