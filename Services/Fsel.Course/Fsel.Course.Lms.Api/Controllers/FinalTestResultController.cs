8344babfd84669b4d18ca7fc0b7db9bce79e7501
@Tunghung641 <dohung642001@gmail.com>
2023-07-03 15:32:07 +0700

    Create api final test result

--- Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/FinalTestResultController.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/FinalTestResultController.cs
@@ -6,34 +6,35 @@ namespace Fsel.Course.Lms.Api.Controllers
     using System.Net;
     using Fsel.Common.Constants;
     using Fsel.Course.Domain.Models.EntityModels;
+    using Fsel.Course.Lms.Application.Queries.MockTestResultQuery;
     using Fsel.Shared.Enums;
     using MediatR;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
-    using Fsel.Course.Lms.Application.Queries.MockTestResultQuery;
+    using Fsel.Course.Lms.Application.Queries.FinalTestResultQuery;
 
     [ApiVersion(Settings.APIVersion)]
-    [Route(Settings.APIDefaultRoute + "/mock-test-result")]
+    [Route(Settings.APIDefaultRoute + "/final-test-result")]
     [ApiController]
     [Authorize(Roles = nameof(EnumRole.Student))]
-    public class MockTestResultController : ControllerBase
+    public class FinalTestResultController : ControllerBase
     {
         private readonly IMediator _mediator;
 
-        public MockTestResultController(IMediator mediator)
+        public FinalTestResultController(IMediator mediator)
         {
             _mediator = mediator;
         }
 
         /// <summary>
-        /// Get List Mock tesk result
+        /// Get List final test result
         /// </summary>
         [HttpGet]
-        [ProducesResponseType(typeof(MethodResult<IList<MockTestResultModel>>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(MethodResult<IList<FinalTestResultModel>>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
-        public async Task<IActionResult> GetListMockTestResultById([FromQuery] GetMockTestReportQuery query)
+        public async Task<IActionResult> GetListFinalTestResultById([FromQuery] GetFinalTestReportQuery query)
         {
-            MethodResult<IList<MockTestResultModel>> queryResult = await _mediator.Send(query).ConfigureAwait(false);
+            MethodResult<IList<FinalTestResultModel>> queryResult = await _mediator.Send(query).ConfigureAwait(false);
             return queryResult.GetActionResult();
         }
     }
