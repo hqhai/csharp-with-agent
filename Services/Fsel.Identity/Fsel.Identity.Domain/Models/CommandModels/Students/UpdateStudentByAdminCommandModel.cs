b1757e24ed096ab3406d8e9519c8284dfcb05f62
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2023-07-05 09:09:43 +0700

    Update code

--- Services/Fsel.Identity/Fsel.Identity.Domain/Models/CommandModels/Students/UpdateStudentByAdminCommandModel.cs
+++ Services/Fsel.Identity/Fsel.Identity.Domain/Models/CommandModels/Students/UpdateStudentByAdminCommandModel.cs
@@ -5,16 +5,16 @@ namespace Fsel.Identity.Domain.Models.CommandModels.Students
     using Fsel.Identity.Domain.Enums;
     using Fsel.Identity.Domain.Models.CommandModels.Parents;
 
-    public class UpdateStudentProfileCommandModel
+    public class UpdateStudentByAdminCommandModel
     {
-        public Guid? StudentId { get; set; }
-        public string? AvatarPath { get; set; }
         public string? FullName { get; set; }
+        public DateTime Birthday { get; set; }
         public EnumGender? Gender { get; set; }
-        public DateTime? Birthday { get; set; }
+        public string? School { get; set; }
         public string? Address { get; set; }
         public string? Occupation { get; set; }
-        public string? School { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
         public UpdateParentCommandModel? Parent { get; set; }
     }
 }
