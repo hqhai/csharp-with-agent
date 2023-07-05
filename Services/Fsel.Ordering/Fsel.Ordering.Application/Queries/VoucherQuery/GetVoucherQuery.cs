ce053265569caba08ab4d9e5e6f5bba4b967e362
@Tunghung641 <dohung642001@gmail.com>
2023-07-05 14:47:49 +0700

    fix comment

--- Services/Fsel.Ordering/Fsel.Ordering.Application/Queries/VoucherQuery/GetVoucherQuery.cs
+++ Services/Fsel.Ordering/Fsel.Ordering.Application/Queries/VoucherQuery/GetVoucherQuery.cs
@@ -37,6 +37,7 @@ namespace Fsel.Ordering.Application.Queries.VoucherQuery
 
             var voucherQuery = await _voucherRepository.Queryable
                                     .Include(x => x.VoucherPackages)
+                                    .ThenInclude(x => x.Package)
                                     .Select(x => new VoucherModel
                                     {
                                         Id = x.Id,
@@ -55,7 +56,7 @@ namespace Fsel.Ordering.Application.Queries.VoucherQuery
                                             PackageId = x.PackageId,
                                             Percentage = x.Percentage,
                                             VoucherId = x.VoucherId,
-                                            DiscountedPrice = (double)x.Package!.Price - x.Percentage * (double)x.Package.Price,
+                                            DiscountedPrice = x.Percentage * (double)x.Package!.Price / 100,
                                             Price = (double)x.Package!.Price,
                                         }).ToList(),
                                     }).FirstOrDefaultAsync(cancellationToken);
