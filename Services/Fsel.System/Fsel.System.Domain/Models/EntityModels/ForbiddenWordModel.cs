3c349b109dc173d86597a0c9878dace4a41ffbc9
tunghung <tunghung@atlantic.edu.vn>
2023-07-17 15:54:29 +0700

    update api

---
+++ Services/Fsel.System/Fsel.System.Domain/Models/EntityModels/ForbiddenWordModel.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.System.Domain.Models.EntityModels
+{
+    public class ForbiddenWordModel
+    {
+        public string? Word { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
