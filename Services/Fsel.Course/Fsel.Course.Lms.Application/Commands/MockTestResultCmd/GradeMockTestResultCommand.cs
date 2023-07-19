fe669fb3f5cc2936918ece2974a2c3f21e8c5084
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2023-07-19 17:41:13 +0700

    Update code

---
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/MockTestResultCmd/GradeMockTestResultCommand.cs
@@ -0,0 +1,8 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Course.Lms.Application.Commands.MockTestResultCmd
+{
+    public class GradeMockTestResultCommand
+    {
+    }
+}
---
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/MockTestResultQuery/SearchMockTestResultQuery.cs
@@ -0,0 +1,8 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Course.Lms.Application.Queries.MockTestResultQuery
+{
+    public class SearchMockTestResultQuery
+    {
+    }
+}
