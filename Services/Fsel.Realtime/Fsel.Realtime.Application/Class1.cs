4c16bf4e69add425976fadf9c1f80d413e07389c
Đô Nguyễn Thành <thanhdo@atlantic.edu.vn>
2023-08-01 17:58:07 +0700

    Update prj signalR

---
+++ Services/Fsel.Realtime/Fsel.Realtime.Application/Class1.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Realtime.Application
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Services/Fsel.Realtime/Fsel.Realtime.Domain/Class1.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Realtime.Domain
+{
+    public class Class1
+    {
+
+    }
+}
