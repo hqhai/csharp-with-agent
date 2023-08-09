d16e6e4dcf2916149334a51f7c40288c251cb4c7
tunghung <tunghung@atlantic.edu.vn>
2023-08-09 16:38:31 +0700

    update

--- Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/ClassForumResultModel.cs
+++ Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/ClassForumResultModel.cs
@@ -33,7 +33,9 @@ namespace Fsel.Course.Domain.Models.EntityModels
         public bool? IsFlagged { get; set; }
 
         public bool? IsLiked { get; set; }
+
         public string? WordContent { get; set; }
+
         public string? GradingAlFeedback { get; set; }
 
         public EnumCourseLevel CourseLevel { get; set; }
