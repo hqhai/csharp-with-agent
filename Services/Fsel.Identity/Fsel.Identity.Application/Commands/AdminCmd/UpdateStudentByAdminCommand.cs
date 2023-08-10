6fefaef80a408a6ff942759184e3f739a1808c39
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2023-08-10 11:02:40 +0700

    Update code fix

--- Services/Fsel.Identity/Fsel.Identity.Application/Commands/AdminCmd/UpdateStudentByAdminCommand.cs
+++ Services/Fsel.Identity/Fsel.Identity.Application/Commands/AdminCmd/UpdateStudentByAdminCommand.cs
@@ -64,179 +64,155 @@ namespace Fsel.Identity.Application.Commands.AdminCmd
         {
             ArgumentNullException.ThrowIfNull(request);
             var methodResult = new MethodResult<StudentModel>();
-            var userView = await GetUserViewAsync(request.Id, cancellationToken);
+            var userView = await _userManager.Users.Include(x => x.Human)
+                                                  .ThenInclude(x => x!.Student)
+                                                  .ThenInclude(x => x!.ParentStudents)
+                                                  .FirstOrDefaultAsync(x => x.Human != null && x.Human.Student != null && x.Human.Student.Id == request.Id, cancellationToken);
             if (userView == null)
             {
                 methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist), nameof(userView));
                 return methodResult;
             }
             var student = userView.Human?.Student;
-            var humanParent = userView.Human?.Student?.ParentStudents.FirstOrDefault()?.Parent?.Human;
-            if (student != null)
+
+            #region Update Parent
+
+            if (request.Parent != null && student != null)
             {
-                if (request.Parent != null && string.IsNullOrEmpty(request.Parent.FullName))
+                if (string.IsNullOrEmpty(request.Parent.FullName))
                 {
                     methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist), nameof(request.Parent.FullName));
                     return methodResult;
                 }
-                await SaveParent(request, student, humanParent, cancellationToken);
-            }
-            await UpdateUser(request, userView, cancellationToken);
-
-            var method = await SendOTP(request, userView, cancellationToken);
-            if (!method.IsOK)
-            {
-                methodResult.AddErrorBadRequest(method.ErrorMessages);
-                return methodResult;
-            }
-            var userModel = await GetStudentModel(userView);
-
-            methodResult.StatusCode = StatusCodes.Status200OK;
-            methodResult.Result = userModel;
-            return methodResult;
-        }
-
-        private async Task<User?> GetUserViewAsync(Guid studentId, CancellationToken cancellationToken)
-        {
-            return await _userManager.Users.Include(x => x.Human)
-                .ThenInclude(x => x!.Student)
-                .ThenInclude(x => x!.ParentStudents)
-                .ThenInclude(x => x.Parent)
-                .ThenInclude(x => x!.Human)
-                .FirstOrDefaultAsync(x => x.Human != null && x.Human.Student != null && x.Human.Student.Id == studentId, cancellationToken);
-        }
-
-        private async Task SaveParent(UpdateStudentByAdminCommand request, Student student, Human? human, CancellationToken cancellationToken)
-        {
-            ArgumentNullException.ThrowIfNull(request);
-            ArgumentNullException.ThrowIfNull(student);
-            if (human == null)
-            {
-                human = _mapper.Map<Human>(request.Parent);
-                human.Parent = _mapper.Map<Parent>(request.Parent);
-                human.Parent.ParentStudents.Add(new ParentStudent
+                if (student.ParentStudents == null || student.ParentStudents.Count == 0)
                 {
-                    Student = student
-                });
-                _humanRepository.Add(human);
-            }
-            else
-            {
-                if (request.Parent != null)
-                {
-                    _mapper.Map(request.Parent, human);
-                    _mapper.Map(request.Parent, human.Parent);
-                    _humanRepository.Update(human);
+                    Human newHuman = _mapper.Map<Human>(request.Parent);
+                    newHuman.Parent = _mapper.Map<Parent>(request.Parent);
+                    newHuman.Parent.ParentStudents.Add(new ParentStudent
+                    {
+                        Student = student
+                    });
+                    _humanRepository.Add(newHuman);
+                    await _humanRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken).ConfigureAwait(false);
                 }
                 else
                 {
-                    await _humanRepository.DeleteAsync(human);
-                }
-            }
-
-            await _humanRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken).ConfigureAwait(false);
-        }
-
-        private async Task<StudentModel> GetStudentModel(User? user)
-        {
-            StudentModel studentModel = new StudentModel();
-            if (user != null)
-            {
-                var student = user.Human?.Student;
-                var parentStudent = student?.ParentStudents?.FirstOrDefault();
-                _mapper.Map(user, studentModel);
-                _mapper.Map(user.Human, studentModel.Human);
-                _mapper.Map(student, studentModel);
-
-                if (parentStudent != null && parentStudent.Parent != null)
-                {
-                    studentModel.Parent = _mapper.Map<ParentProfileModel>(parentStudent.Parent.Human);
-                    studentModel.Parent.Occupation = parentStudent.Parent.Occupation;
-                }
-                var classStudent = await _trainingService.GetClassByStudentId(student?.Id ?? default);
-                var @class = classStudent?.Content?.Result;
-                if (@class != null)
-                {
-                    studentModel.CodeClass = classStudent?.Content?.Result?.Code;
-                    var package = await _orderService.GetPackages();
-                    if (package.IsSuccessStatusCode)
+                    userView = await _userManager.Users.Include(x => x.Human)
+                                               .ThenInclude(x => x!.Student)
+                                               .ThenInclude(x => x!.ParentStudents)
+                                               .ThenInclude(x => x.Parent)
+                                               .ThenInclude(x => x!.Human)
+                                               .FirstOrDefaultAsync(x => x.Human != null && x.Human.Student != null && x.Human.Student.Id == request.Id, cancellationToken);
+                    var human = userView?.Human?.Student?.ParentStudents.FirstOrDefault()?.Parent?.Human;
+                    if (human != null)
                     {
-                        studentModel.Membership = package.Content?.Result?.FirstOrDefault(p => p.Id == @class.PackageId)?.Code;
+                        _mapper.Map(request.Parent, human);
+                        _mapper.Map(request.Parent, human.Parent);
+                        _humanRepository.Update(human);
+                        await _humanRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken).ConfigureAwait(false);
                     }
                 }
             }
-            return studentModel;
-        }
-
-        public async Task UpdateUser(UpdateStudentByAdminCommand request, User? user, CancellationToken cancellationToken)
-        {
-            ArgumentNullException.ThrowIfNull(request);
-            if (user != null)
+            if (userView == null)
             {
-                _mapper.Map(request, user);
-                await _userManager.UpdateAsync(user);
-
-                _mapper.Map(request, user.Human);
-                _mapper.Map(request, user.Human?.Student);
-                _humanRepository.Update(user.Human ?? new Human());
-                await _humanRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken).ConfigureAwait(false);
+                methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist), nameof(userView));
+                return methodResult;
             }
-        }
 
-        public async Task<VoidMethodResult> SendOTP(UpdateStudentByAdminCommand request, User? user, CancellationToken cancellationToken)
-        {
-            ArgumentNullException.ThrowIfNull(request);
-            VoidMethodResult methodResult = new VoidMethodResult();
-            if (user != null)
+            #endregion Update Parent
+
+            #region validate Send OTP
+
+            var isCheckEmail = userView.Email != request.Email;
+            var isCheckPhone = userView.PhoneNumber != request.PhoneNumber;
+            if (isCheckEmail || isCheckPhone)
             {
-                var isCheckEmail = user.Email != request.Email;
-                var isCheckPhone = user.PhoneNumber != request.PhoneNumber;
-                if (isCheckEmail || isCheckPhone)
+                userView.EmailConfirmed = false;
+                userView.UserName = isCheckEmail ? request.Email : isCheckPhone ? request.PhoneNumber : userView.UserName;
+
+                var userOtpCode = await _userOtpCodeRepository.Queryable.FirstOrDefaultAsync(x => x.UserId == userView.Id && x.Status == EnumStatusUser.New && !x.IsDeleted, cancellationToken);
+                if (userOtpCode == null)
                 {
-                    user.EmailConfirmed = false;
-                    user.UserName = isCheckEmail ? request.Email : isCheckPhone ? request.PhoneNumber : user.UserName;
-                    var userOtpCode = await _userOtpCodeRepository.Queryable.FirstOrDefaultAsync(x => x.UserId == user.Id && x.Status == EnumStatusUser.New && !x.IsDeleted, cancellationToken);
-                    if (userOtpCode == null)
-                    {
-                        var randomSecure = new RandomSecureHelper();
-                        var totp = new Totp(Encoding.UTF8.GetBytes(randomSecure.Secretstrings()));
-                        var otp = totp.ComputeTotp();
+                    var randomSecure = new RandomSecureHelper();
+                    var totp = new Totp(Encoding.UTF8.GetBytes(randomSecure.Secretstrings()));
+                    var otp = totp.ComputeTotp();
 
-                        userOtpCode = new UserOtpCode
-                        {
-                            UserId = user.Id,
-                            OTPCode = otp,
-                            Status = EnumStatusUser.New,
-                            ExpiredTime = DateTime.Now.AddDays(_appSetting!.Otp!.StepDayWithAdmin)
-                        };
-                        _userOtpCodeRepository.Add(userOtpCode);
-                        await _userOtpCodeRepository.UnitOfWork.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-                    }
-
-                    var param = new SendOtpTemplateModel
+                    userOtpCode = new UserOtpCode
                     {
-                        OtpCode = userOtpCode.OTPCode,
-                        AccessLink = string.Format(CultureInfo.InvariantCulture, _appSetting!.ConstantUrl!.ConfirmOtpUrl!, userOtpCode.OTPCode),
-                        OtpValidTime = string.Format(CultureInfo.InvariantCulture, SenderSettings.OtpValidDay, _appSetting!.Otp!.StepDayWithAdmin)
+                        UserId = userView.Id,
+                        OTPCode = otp,
+                        Status = EnumStatusUser.New,
+                        ExpiredTime = DateTime.Now.AddDays(_appSetting!.Otp!.StepDayWithAdmin)
                     };
-                    var subject = string.Format(CultureInfo.InvariantCulture, SenderSettings.SendOtpSubjectFullName, user.FullName);
-                    var sendResult = new MethodResult<bool>();
-                    if (isCheckEmail)
-                    {
-                        sendResult = await _mediator.Send(new SenderCommand { Email = request.Email, Subject = subject, Params = param, Template = EnumSenderTemplate.SendOtpAndLink }, cancellationToken).ConfigureAwait(false);
-                    }
-                    else if (isCheckPhone)
-                    {
-                        sendResult = await _mediator.Send(new SenderCommand { Email = user.Email, Subject = subject, Params = param, Template = EnumSenderTemplate.SendOtpAndLink }, cancellationToken).ConfigureAwait(false);
-                    }
-                    if (!sendResult.IsOK)
-                    {
-                        methodResult.AddErrorBadRequest(sendResult?.ErrorMessages);
-                        return methodResult;
-                    }
+                    _userOtpCodeRepository.Add(userOtpCode);
+                    await _userOtpCodeRepository.UnitOfWork.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+                }
+
+                var param = new SendOtpTemplateModel
+                {
+                    OtpCode = userOtpCode.OTPCode,
+                    AccessLink = string.Format(CultureInfo.InvariantCulture, _appSetting!.ConstantUrl!.ConfirmOtpUrl!, userOtpCode.OTPCode),
+                    OtpValidTime = string.Format(CultureInfo.InvariantCulture, SenderSettings.OtpValidDay, _appSetting!.Otp!.StepDayWithAdmin)
+                };
+                var subject = string.Format(CultureInfo.InvariantCulture, SenderSettings.SendOtpSubjectFullName, userView.FullName);
+                var sendResult = new MethodResult<bool>();
+                if (isCheckEmail)
+                {
+                    sendResult = await _mediator.Send(new SenderCommand { Email = request.Email, Subject = subject, Params = param, Template = EnumSenderTemplate.SendOtpAndLink }, cancellationToken).ConfigureAwait(false);
+                }
+                else if (isCheckPhone)
+                {
+                    sendResult = await _mediator.Send(new SenderCommand { Email = userView.Email, Subject = subject, Params = param, Template = EnumSenderTemplate.SendOtpAndLink }, cancellationToken).ConfigureAwait(false);
+                }
+
+                if (!sendResult.IsOK)
+                {
+                    methodResult.AddErrorBadRequest(sendResult?.ErrorMessages);
+                    return methodResult;
                 }
             }
 
+            #endregion validate Send OTP
+
+            #region Update User
+
+            _mapper.Map(request, userView);
+            await _userManager.UpdateAsync(userView);
+
+            _mapper.Map(request, userView.Human);
+            student = _mapper.Map(request, userView.Human?.Student);
+            _humanRepository.Update(userView.Human ?? new Human());
+            await _humanRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken).ConfigureAwait(false);
+
+            #endregion Update User
+
+            #region Result Model
+
+            var parentStudent = student?.ParentStudents?.FirstOrDefault();
+            var userModel = _mapper.Map<StudentModel>(userView);
+            _mapper.Map(userView.Human, userModel.Human);
+            _mapper.Map(student, userModel);
+            if (parentStudent != null && parentStudent.Parent != null)
+            {
+                userModel.Parent = _mapper.Map<ParentProfileModel>(parentStudent.Parent.Human);
+                userModel.Parent.Occupation = parentStudent.Parent.Occupation;
+            }
+            var classStudent = await _trainingService.GetClassByStudentId(student?.Id ?? default);
+            var @class = classStudent?.Content?.Result;
+            if (@class != null)
+            {
+                userModel.CodeClass = classStudent?.Content?.Result?.Code;
+                var package = await _orderService.GetPackages();
+                if (package.IsSuccessStatusCode)
+                {
+                    userModel.Membership = package.Content?.Result?.FirstOrDefault(p => p.Id == @class.PackageId)?.Code;
+                }
+            }
+
+            #endregion Result Model
+
+            methodResult.StatusCode = StatusCodes.Status200OK;
+            methodResult.Result = userModel;
             return methodResult;
         }
     }
