7ad3fbcd379a7be9ee777c5005355e590204f983
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2023-08-15 08:59:31 +0700

    Update code fix commit 2

--- Services/Fsel.Training/Fsel.Training.Application/Commands/ClassCmd/AssignTeacherToClassCommand.cs
+++ Services/Fsel.Training/Fsel.Training.Application/Commands/ClassCmd/AssignTeacherToClassCommand.cs
@@ -4,68 +4,61 @@ namespace Fsel.Training.Application.Commands.ClassCmd
 {
     using System.Threading;
     using System.Threading.Tasks;
-    using AutoMapper;
     using Fsel.Common.ActionResults;
     using Fsel.Common.Enums.ErrorCodes;
+    using Fsel.Shared.Enums;
     using Fsel.Training.Application.Services.UserServices;
     using Fsel.Training.Domain.IRepositories;
     using Fsel.Training.Domain.Models.CommandModels.Classes;
-    using Fsel.Training.Domain.Models.EntityModels;
     using MediatR;
     using Microsoft.AspNetCore.Http;
 
-    public class AssignTeacherToClassCommand : AssignTeacherToClassCommandModel, IRequest<MethodResult<ClassModel>>
+    public class AssignTeacherToClassCommand : AssignTeacherToClassCommandModel, IRequest<MethodResult<bool>>
     {
     }
 
-    public class AssignTeacherToClassCommandHandler : IRequestHandler<AssignTeacherToClassCommand, MethodResult<ClassModel>>
+    public class AssignTeacherToClassCommandHandler : IRequestHandler<AssignTeacherToClassCommand, MethodResult<bool>>
     {
         private readonly IClassRepository _classRepository;
         private readonly IUserService _userService;
-        private readonly IMapper _mapper;
 
-        public AssignTeacherToClassCommandHandler(IClassRepository classRepository
-            , IUserService userService
-            , IMapper mapper)
+        public AssignTeacherToClassCommandHandler(IClassRepository classRepository, IUserService userService)
         {
             _classRepository = classRepository;
             _userService = userService;
-            _mapper = mapper;
         }
 
-        public async Task<MethodResult<ClassModel>> Handle(AssignTeacherToClassCommand request, CancellationToken cancellationToken)
+        public async Task<MethodResult<bool>> Handle(AssignTeacherToClassCommand request, CancellationToken cancellationToken)
         {
             ArgumentNullException.ThrowIfNull(request);
-            MethodResult<ClassModel> methodResult = new MethodResult<ClassModel>();
-            var @class = await _classRepository.GetByIdAsync(request.ClassId);
-            if (@class == null)
+            MethodResult<bool> methodResult = new MethodResult<bool>();
+            var classes = await _classRepository.GetByIdAsync(request.ClassId);
+            if (classes == null)
             {
-                methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist), nameof(@class));
+                methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist), nameof(classes));
                 return methodResult;
             }
-
             var teacherResult = await _userService.GetTeacherByIdAsync(request.TeacherId);
             if (!teacherResult.IsSuccessStatusCode)
             {
                 methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist), nameof(teacherResult));
                 return methodResult;
             }
+
             var teacher = teacherResult?.Content?.Result;
             if (teacher == null)
             {
                 methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist), nameof(teacher));
                 return methodResult;
             }
-            @class.Id = request.ClassId;
-            @class.TeacherId = request.TeacherId;
-
+            classes.TeacherId = request.TeacherId;
+            classes.TeacherApprovalStatus = EnumTeacherApprovalStatus.Pending;
             await _classRepository.ExecuteTransactionAsync(async () =>
             {
-                @class.TeacherApprovalStatus = Shared.Enums.EnumTeacherApprovalStatus.Pending;
-                _classRepository.Update(@class);
+                _classRepository.Update(classes);
                 await _classRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken).ConfigureAwait(false);
                 methodResult.StatusCode = StatusCodes.Status200OK;
-                methodResult.Result = _mapper.Map<ClassModel>(@class);
+                methodResult.Result = true;
                 return methodResult;
             });
             return methodResult;
