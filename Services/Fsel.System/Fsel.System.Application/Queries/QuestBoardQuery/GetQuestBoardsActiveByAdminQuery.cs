228693084a14a822f6e62b57f39869e7403bda59
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2023-08-18 11:47:41 +0700

    Update code and migration

--- Services/Fsel.System/Fsel.System.Application/Queries/QuestBoardQuery/GetQuestBoardsActiveByAdminQuery.cs
+++ Services/Fsel.System/Fsel.System.Application/Queries/QuestBoardQuery/GetQuestBoardsActiveByAdminQuery.cs
@@ -4,9 +4,13 @@ namespace Fsel.System.Application.Queries.QuestBoardQuery
 {
     using AutoMapper;
     using Fsel.Common.ActionResults;
+    using Fsel.Common.Enums.ErrorCodes;
+    using Fsel.Ordering.Domain.Enums;
     using Fsel.Shared.Enums;
+    using Fsel.System.Application.Services.OrderServices;
     using Fsel.System.Domain.IRepositories;
     using Fsel.System.Domain.Models.EntityModels;
+    using global::System.Linq;
     using MediatR;
     using Microsoft.AspNetCore.Http;
     using Microsoft.EntityFrameworkCore;
@@ -20,11 +24,13 @@ namespace Fsel.System.Application.Queries.QuestBoardQuery
     {
         private readonly IQuestBoardRepository _questBoardRepository;
         private readonly IMapper _mapper;
+        private readonly IOrderService _orderService;
 
-        public GetQuestBoardsActiveByAdminQueryHandler(IQuestBoardRepository questBoardRepository, IMapper mapper)
+        public GetQuestBoardsActiveByAdminQueryHandler(IQuestBoardRepository questBoardRepository, IMapper mapper, IOrderService orderService)
         {
             _questBoardRepository = questBoardRepository;
             _mapper = mapper;
+            _orderService = orderService;
         }
 
         public async Task<MethodResult<IList<QuestBoardModel>>> Handle(GetQuestBoardsActiveByAdminQuery request, CancellationToken cancellationToken)
@@ -38,8 +44,16 @@ namespace Fsel.System.Application.Queries.QuestBoardQuery
                 methodResult.StatusCode = StatusCodes.Status200OK;
                 return methodResult;
             }
-
+            var packageResults = await _orderService.GetPackages();
+            if (!packageResults.IsSuccessStatusCode)
+            {
+                methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist), nameof(packageResults));
+                return methodResult;
+            }
+            var packages = packageResults?.Content?.Result;
+            var packageId = packages?.FirstOrDefault(x => x.Code == EnumPackageCode.STANDARD)?.Id;
             var questBoards = await _questBoardRepository.Queryable.Where(x => x.Type == request.QuestBoardType && x.IsActive && x.DependentId == null).ToListAsync(cancellationToken);
+            questBoards = questBoards.Where(x => x.PackageIds != null && x.PackageIds!.Any(x => x == packageId)).ToList();
             if (questBoards == null || questBoards.Count == 0)
             {
                 methodResult.Result = null;
