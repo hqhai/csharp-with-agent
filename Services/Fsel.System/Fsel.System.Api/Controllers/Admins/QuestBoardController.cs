f831a59642117a2d9f086ebf0e9118a3ecfca63e
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2023-08-21 13:10:50 +0700

    Update commint1

--- Services/Fsel.System/Fsel.System.Api/Controllers/Admins/QuestBoardController.cs
+++ Services/Fsel.System/Fsel.System.Api/Controllers/Admins/QuestBoardController.cs
@@ -66,7 +66,7 @@ namespace Fsel.System.Api.Controllers.Admins
         /// <summary>
         /// Get Quest Board By DependentId
         /// </summary>
-        [HttpGet("dependent/{dependentId}")]
+        [HttpGet("get-list-by-dependent/{dependentId}")]
         [ProducesResponseType(typeof(MethodResult<IList<QuestBoardModel>>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
         public async Task<IActionResult> GetQuestBoardByDependentId([FromRoute] Guid dependentId)
