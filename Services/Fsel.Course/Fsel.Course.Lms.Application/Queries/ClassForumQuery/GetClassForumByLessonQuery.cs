79b333bc177f71525b9ce0724ed488805292e02a
tunghung <tunghung@atlantic.edu.vn>
2023-08-30 15:44:21 +0700

    fix bug api

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/ClassForumQuery/GetClassForumByLessonQuery.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/ClassForumQuery/GetClassForumByLessonQuery.cs
@@ -9,6 +9,8 @@ namespace Fsel.Course.Lms.Application.Queries.ClassForumQuery
     using Fsel.Course.Domain.Enums;
     using Fsel.Course.Domain.IRepositories;
     using Fsel.Course.Domain.Models.EntityModels;
+    using Fsel.Course.Lms.Application.Services.InteractionService;
+    using Fsel.Course.Lms.Application.Services.InteractionService.Models;
     using MediatR;
     using Microsoft.AspNetCore.Http;
     using Microsoft.EntityFrameworkCore;
@@ -22,11 +24,13 @@ namespace Fsel.Course.Lms.Application.Queries.ClassForumQuery
     {
         private readonly IClassForumRepository _classForumRepository;
         private readonly IMapper _mapper;
+        private readonly IInteractionService _interactionService;
 
-        public GetClassForumByLessonQueryHandler(IClassForumRepository classForumRepository, IMapper mapper)
+        public GetClassForumByLessonQueryHandler(IClassForumRepository classForumRepository, IMapper mapper, IInteractionService interactionService)
         {
-            _mapper = mapper;
             _classForumRepository = classForumRepository;
+            _mapper = mapper;
+            _interactionService = interactionService;
         }
 
         public async Task<MethodResult<ClassForumModel>> Handle(GetClassForumByLessonQuery request, CancellationToken cancellationToken)
@@ -39,7 +43,23 @@ namespace Fsel.Course.Lms.Application.Queries.ClassForumQuery
                                    .Where(x => x.ClassForumResults!.Any(x => x.Status == EnumClassForumResultStatus.PendingForGrading || x.Status == EnumClassForumResultStatus.Graded))
                                    .FirstOrDefaultAsync(x => x.LessonId == request.LessonId, cancellationToken);
 
-            methodResult.Result = _mapper.Map<ClassForumModel>(classForum);
+            var classFormQuery = _mapper.Map<ClassForumModel>(classForum);
+            var actionsResult = await _interactionService.GetsActionAsync(new InteractionActionCommandModel { ObjectIds = classForum?.ClassForumResults.Select(x => x.Id).ToList() });
+            var actions = actionsResult.Content?.Result;
+
+            if (actions != null)
+            {
+                classFormQuery!.ClassForumResults = classFormQuery.ClassForumResults?.Where(x => !actions.Any(n => n.IsDisable && n.ObjectId == x.Id)).ToList();
+                foreach (var item in classFormQuery.ClassForumResults!)
+                {
+                    var action = actions.FirstOrDefault(x => x.ObjectId == item.Id);
+                    item.CommentNumber = action?.CommentNumber;
+                    item.LikeNumber = action?.LikeNumber;
+                    item.IsLiked = action?.IsLiked;
+                }
+            }
+
+            methodResult.Result = classFormQuery;
             methodResult.StatusCode = StatusCodes.Status200OK;
             return methodResult;
         }
