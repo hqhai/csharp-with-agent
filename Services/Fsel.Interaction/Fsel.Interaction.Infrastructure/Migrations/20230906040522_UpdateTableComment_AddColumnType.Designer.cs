46daeee7b28180a98df3b9384b89205907b39cbf
hqhai <quanghai@atlantic.edu.vn>
2023-09-06 12:01:43 +0700

    Commit code

--- Services/Fsel.Interaction/Fsel.Interaction.Infrastructure/Migrations/20230906040522_UpdateTableComment_AddColumnType.Designer.cs
+++ Services/Fsel.Interaction/Fsel.Interaction.Infrastructure/Migrations/20230906040522_UpdateTableComment_AddColumnType.Designer.cs
@@ -4,6 +4,7 @@ using Fsel.Interaction.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fsel.Interaction.Infrastructure.Migrations
 {
     [DbContext(typeof(InteractionDbContext))]
-    partial class InteractionDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230906040522_UpdateTableComment_AddColumnType")]
+    partial class UpdateTableComment_AddColumnType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
