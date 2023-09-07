e41ea0bac455e8bcf787f0d56cbe5feab90ce386
tunghung <tunghung@atlantic.edu.vn>
2023-09-07 13:17:39 +0700

    update

--- Services/Fsel.Interaction/Fsel.Interaction.Api/Controllers/SurveyQuestionController.cs
+++ Services/Fsel.Interaction/Fsel.Interaction.Api/Controllers/SurveyQuestionController.cs
@@ -8,7 +8,6 @@ namespace Fsel.Interaction.Api.Controllers
     using Fsel.Interaction.Application.Queries.SurveyQuestionQuery;
     using Fsel.Interaction.Domain.Models.EntityModels;
     using MediatR;
-    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
 
     [ApiVersion(Settings.APIVersion)]
