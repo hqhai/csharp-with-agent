21212ffee5c6b4dcda9480a05a937c00ed05078f
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2023-09-13 17:40:32 +0700

    Update code model student setting

--- Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/StudentSettingModel.cs
+++ Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/StudentSettingModel.cs
@@ -11,6 +11,6 @@ namespace Fsel.Course.Domain.Models.EntityModels
         public EnumCourseLevel Level { get; set; }
         public EnumPlacementTestLevel? PTLevel { get; set; }
         public bool IsLockPT { get; set; }
-        public bool IsLockOrder { get; set; }
+        public bool IsLockOrder { get; set; } = true;
     }
 }
