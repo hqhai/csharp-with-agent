47f24cb7d4825f582a9b421ca5f810dfedc00555
tunghung <tunghung@atlantic.edu.vn>
2023-09-14 16:49:51 +0700

    update mocktest result

--- Services/Fsel.Course/Fsel.Course.Domain/Entities/MockTestResult.cs
+++ Services/Fsel.Course/Fsel.Course.Domain/Entities/MockTestResult.cs
@@ -83,6 +83,7 @@ namespace Fsel.Course.Domain.Entities
 
         [Required(ErrorMessage = nameof(EnumSystemErrorCode.Required))]
         public Guid StudentId { get; set; }
+        public DateTime? CheckStartDate { get; set; }
 
         public ICollection<MockTestAnswer> MockTestAnswers { get; set; } = new List<MockTestAnswer>();
         public ICollection<MockTestScore> MockTestScores { get; set; } = new List<MockTestScore>();
