5e64ad68c0f37b32d97b767e4dee703e950a99b5
hqhai <quanghai@atlantic.edu.vn>
2023-09-14 09:34:48 +0700

    Sửa lại trạng thái khóa học

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/UnitQuery/GetCurrentUnitIndicatorQuery.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/UnitQuery/GetCurrentUnitIndicatorQuery.cs
@@ -54,7 +54,7 @@ namespace Fsel.Course.Lms.Application.Queries.UnitQuery
 
             var currentUnitIndicator = await _unitResultRepository
                               .Queryable
-                              .Where(x => x.CourseId == request.CourseId && x.Status == EnumResultStatus.New)
+                              .Where(x => x.CourseId == request.CourseId && x.Status == EnumResultStatus.Done)
                               .Select(x => new
                               {
                                   UnitResult = x,
