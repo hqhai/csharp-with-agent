85a6942979085575c925bfafd612df0190e7743b
Đô Nguyễn Thành <thanhdo@atlantic.edu.vn>
2023-09-15 18:11:31 +0700

    Update NotificationHub

--- Services/Fsel.Realtime/Fsel.Realtime.Application/Hubs/NotificationHub.cs
+++ Services/Fsel.Realtime/Fsel.Realtime.Application/Hubs/NotificationHub.cs
@@ -12,17 +12,5 @@ namespace Fsel.Realtime.Application.Hubs
         {
             Clients.All.SendAsync(RealtimeSettings.NotificationHub.Methods.NotificationMessage, model);
         }
-
-        public override async Task OnConnectedAsync()
-        {
-            var httpContext = Context.GetHttpContext();
-            var userId = httpContext.Request.Query["UserId"];
-            //var userId = Context.User?.Claims.FirstOrDefault(x => x.Type == JwtClaimNames.UserId)?.Value ?? string.Empty;
-            if (!string.IsNullOrEmpty(userId))
-            {
-                await Groups.AddToGroupAsync(Context.ConnectionId, userId);
-            }
-            await base.OnConnectedAsync();
-        }
     }
 }
