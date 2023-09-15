d538f4311f339e0e3c39e123013c2f2bfdf9dd77
Đô Nguyễn Thành <thanhdo@atlantic.edu.vn>
2023-09-15 18:10:53 +0700

    Update new code signalR

--- Services/Fsel.Realtime/Fsel.Realtime.Application/Hubs/NotificationHub.cs
+++ Services/Fsel.Realtime/Fsel.Realtime.Application/Hubs/NotificationHub.cs
@@ -4,15 +4,25 @@ namespace Fsel.Realtime.Application.Hubs
 {
     using Fsel.Shared.Constants;
     using Fsel.Shared.Models.ShareModels;
-    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.SignalR;
 
-    [Authorize]
     public class NotificationHub : Hub
     {
         public void Send(NotificationQueueModel? model)
         {
             Clients.All.SendAsync(RealtimeSettings.NotificationHub.Methods.NotificationMessage, model);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var httpContext = Context.GetHttpContext();
+            var userId = httpContext.Request.Query["UserId"];
+            //var userId = Context.User?.Claims.FirstOrDefault(x => x.Type == JwtClaimNames.UserId)?.Value ?? string.Empty;
+            if (!string.IsNullOrEmpty(userId))
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, userId);
+            }
+            await base.OnConnectedAsync();
         }
     }
 }
