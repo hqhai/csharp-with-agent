e38c7d973422812fbb1a21efba49333f1ea7dcae
hqhai <quanghai@atlantic.edu.vn>
2023-09-19 15:53:40 +0700

    Bổ sung thông tin đã like/bỏ like

--- Services/Fsel.Interaction/Fsel.Interaction.Application/Queries/PostQuery/StudentPosts/GetActivePostListQuery.cs
+++ Services/Fsel.Interaction/Fsel.Interaction.Application/Queries/PostQuery/StudentPosts/GetActivePostListQuery.cs
@@ -76,6 +76,8 @@ namespace Fsel.Interaction.Application.Queries.PostQuery.StudentPosts
                     .Any(postTag => topicTagId.Contains(postTag.TopicTagId)));
             }
 
+            //var actionLikes = ;
+
             IQueryable<Post> sortedQuery = postQuery;
             switch (request.PostType)
             {
@@ -162,6 +164,7 @@ namespace Fsel.Interaction.Application.Queries.PostQuery.StudentPosts
                 UpdatedDate = post.UpdatedDate,
                 UpdatedUserId = post.UpdatedUserId,
                 FilePaths = post.FilePaths,
+                IsLiked = false,
                 PostTags = post.PostTags
                                .Select(postTag => new TopicTagModel
                                {
@@ -179,12 +182,17 @@ namespace Fsel.Interaction.Application.Queries.PostQuery.StudentPosts
 
             foreach (var post in lists)
             {
-                int likeCount = await _interactionActionRepository.Queryable
-                    .CountAsync(interaction => interaction.Type == EnumInteractionActionType.Like && interaction.ObjectId == post.Id, cancellationToken);
+                var likeQuery = _interactionActionRepository.Queryable
+                    .Where(interaction => interaction.Type == EnumInteractionActionType.Like && interaction.ObjectId == post.Id);
+
+                int? likeCount = likeQuery.Count();
 
                 int commentCount = await _commentRepository.Queryable
                     .CountAsync(comment => comment.ObjectId == post.Id, cancellationToken);
 
+                var likeAction = likeQuery.Any(i => i.UserId == _authContext.CurrentUserId);
+
+                post.IsLiked = likeAction;
                 post.LikeCount = likeCount;
                 post.CommentCount = commentCount;
             }
