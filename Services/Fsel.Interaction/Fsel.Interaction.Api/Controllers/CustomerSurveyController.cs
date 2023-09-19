6047e9b609ed07fa36415ae181b987c7a0122a63
tunghung <tunghung@atlantic.edu.vn>
2023-09-19 19:46:05 +0700

    update api

--- Services/Fsel.Interaction/Fsel.Interaction.Api/Controllers/CustomerSurveyController.cs
+++ Services/Fsel.Interaction/Fsel.Interaction.Api/Controllers/CustomerSurveyController.cs
@@ -27,7 +27,6 @@ namespace Fsel.Interaction.Api.Controllers
         /// <summary>
         /// Create a Customer Survey
         /// </summary>
-        [Authorize]
         [HttpPost]
         [ProducesResponseType(typeof(MethodResult<IList<CustomerSurveyModel>>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
