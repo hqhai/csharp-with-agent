33f05c2147888137731fb293239e1749bf5c8e78
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2023-09-22 17:35:23 +0700

    fixbug active class and change status order

--- Services/Fsel.Ordering/Fsel.Ordering.Application/Commands/OrderCmds/ChangeStatusOrderCommand.cs
+++ Services/Fsel.Ordering/Fsel.Ordering.Application/Commands/OrderCmds/ChangeStatusOrderCommand.cs
@@ -120,7 +120,17 @@ AuthContext authContext)
                 await _orderRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken).ConfigureAwait(false);
 
                 var orders = await _orderRepository.Queryable.Where(p => p.ClassId == order.ClassId && p.Status == EnumOrderStatus.Payment).ToListAsync(cancellationToken);
-                if (orders.Count == 12)
+                //if (orders.Count == 12)
+                //{
+                //    var activeClassResult = await _trainingService.ActiveClass(order.ClassId);
+                //    if (!activeClassResult.IsSuccessStatusCode)
+                //    {
+                //        methodResult.AddError(activeClassResult.Error);
+                //        return methodResult;
+                //    }
+                //}
+                #region for pilot
+                if (orders.Count == 100)
                 {
                     var activeClassResult = await _trainingService.ActiveClass(order.ClassId);
                     if (!activeClassResult.IsSuccessStatusCode)
@@ -129,6 +139,7 @@ AuthContext authContext)
                         return methodResult;
                     }
                 }
+                #endregion
                 methodResult.StatusCode = StatusCodes.Status200OK;
                 methodResult.Result = true;
                 return methodResult;
