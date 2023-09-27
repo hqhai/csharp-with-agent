691063580a138d8b5ba529b08fc5749d354c97ab
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2023-09-27 21:37:22 +0700

    Update code

--- Services/Shared/Fsel.Shared/Helpers/IeltsScoreHelper.cs
+++ Services/Shared/Fsel.Shared/Helpers/IeltsScoreHelper.cs
@@ -149,8 +149,15 @@ namespace Fsel.Shared.Helpers
                         break;
 
                     case EnumPlacementTestLevel.B2:
-                        isLock = value < 75;
-                        courseLevel = !isLock ? EnumCourseLevel.C1 : EnumCourseLevel.B2;
+                        isLock = true;
+                        if (yearOld <= 13)
+                        {
+                            courseLevel = value >= 75 ? EnumCourseLevel.B2 : EnumCourseLevel.B1;
+                        }
+                        else
+                        {
+                            courseLevel = value >= 75 ? EnumCourseLevel.C1 : EnumCourseLevel.B2;
+                        }
                         break;
 
                     case EnumPlacementTestLevel.C1:
