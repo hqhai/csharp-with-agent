f9e4a73b9bbc8db3cf37ca7899b657534d111326
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2023-09-29 17:45:36 +0700

    fixbug search class update sorted by pt

--- Services/Fsel.Training/Fsel.Training.Application/Queries/ClassQuery/Admin/SearchClassQuery.cs
+++ Services/Fsel.Training/Fsel.Training.Application/Queries/ClassQuery/Admin/SearchClassQuery.cs
@@ -29,15 +29,13 @@ namespace Fsel.Training.Application.Queries.ClassQuery.Admin
         private readonly IUserService _userService;
         private readonly IOrderService _orderService;
         private readonly ICourseService _courseService;
-        private readonly IClassStudentRepository _classStudentRepository;
 
-        public SearchClassQueryHandler(IClassRepository classRepository, IUserService userService, IOrderService orderService, ICourseService courseService, IClassStudentRepository classStudentRepository)
+        public SearchClassQueryHandler(IClassRepository classRepository, IUserService userService, IOrderService orderService, ICourseService courseService)
         {
             _classRepository = classRepository;
             _userService = userService;
             _orderService = orderService;
             _courseService = courseService;
-            _classStudentRepository = classStudentRepository;
         }
 
         public async Task<MethodResult<PagingItemsModel<ClassSearchModel>>> Handle(SearchClassQuery request, CancellationToken cancellationToken)
@@ -49,14 +47,8 @@ namespace Fsel.Training.Application.Queries.ClassQuery.Admin
                 methodResult.StatusCode = StatusCodes.Status400BadRequest;
                 return methodResult;
             }
-            List<Guid>? courseIds = new List<Guid>();
 
-            if (!string.IsNullOrEmpty(request.Level.ToString()))
-            {
-                var courses = await _courseService.GetCoursesByLevelAsync(request.Level);
-                courseIds = courses.Content?.Result?.Select(p => p.Id).ToList()!;
-            }
-            var classes = _classRepository.Queryable.Include(i => i.ClassStudents).Where(p => courseIds.Count == 0 || courseIds.Contains(p.CourseId)).Select(p => new ClassSearchModel
+            var classes = await _classRepository.Queryable.Include(i => i.ClassStudents).Select(p => new ClassSearchModel
             {
                 Id = p.Id,
                 ClassName = p.Name,
@@ -67,31 +59,32 @@ namespace Fsel.Training.Application.Queries.ClassQuery.Admin
                 ActivationDate = p.CreatedDate,
                 Status = p.Status,
                 PackageId = p.PackageId,
+                CourseId = p.CourseId,
                 StudentIds = p.ClassStudents.Select(x => x.StudentId).ToList(),
-            });
+            }).ToListAsync(cancellationToken);
             if (request.Status.HasValue)
             {
-                classes = classes.Where(p => p.Status == request.Status);
+                classes = classes.Where(p => p.Status == request.Status).ToList();
             }
-            int totalItem = await classes.CountAsync(cancellationToken: cancellationToken).ConfigureAwait(false);
-            var lists = await classes
-                    .ApplySortAndPaging(request)
-                    .AsNoTracking()
-                    .ToListAsync(cancellationToken: cancellationToken)
-                    .ConfigureAwait(false);
+            if (request.Level.HasValue)
+            {
+                var courses = await _courseService.GetCoursesByLevelAsync(request.Level);
+                var courseIds = courses.Content?.Result?.Select(p => p.Id).ToList();
 
+                classes = classes.Where(p => courseIds != null && courseIds.Contains(p.CourseId)).ToList();
+            }
             var teacherResult = _userService.GetTeacherByIdsAsync(new GetTeacherByIdsQueryModel
             {
-                Ids = lists.Select(x => x.TeacherId ?? default).ToList()
+                Ids = classes.Select(x => x.TeacherId ?? default).ToList()
             });
 
-            var csosResult = _userService.GetCSOByIds(lists.Select(x => x.CSOId ?? default).ToList());
+            var csosResult = _userService.GetCSOByIds(classes.Select(x => x.CSOId ?? default).ToList());
 
             var packagesResult = _orderService.GetPackages();
 
             var ptrResult = _courseService.GetAveragePTPoint(new GetAveragePTPointByIdsQueryModel
             {
-                PointByIdQueryModels = lists.Select(x => new GetAveragePTPointByIdQueryModel
+                PointByIdQueryModels = classes.Select(x => new GetAveragePTPointByIdQueryModel
                 {
                     ClassId = x.Id,
                     StudentIds = x.StudentIds
@@ -102,12 +95,32 @@ namespace Fsel.Training.Application.Queries.ClassQuery.Admin
 
             var ptr = ptrResult.GetAwaiter().GetResult().Content?.Result;
 
+            if (request.SortedByPT.HasValue && request.SortedByPT == true && ptr != null)
+            {
+                var ptrIds = ptr.OrderByDescending(p => p.AveragePTPoint).Select(x => x.ClassId).ToList();
+                classes = classes.OrderBy(p => ptrIds.IndexOf(p.Id)).ToList();
+            }
+            else if (request.SortedByPT.HasValue && request.SortedByPT == false && ptr != null)
+            {
+                var ptrIds = ptr.OrderBy(p => p.AveragePTPoint).Select(x => x.ClassId).ToList();
+                classes = classes.OrderBy(p => ptrIds.IndexOf(p.Id)).ToList();
+            }
+
+            IQueryable<ClassSearchModel> queryable = classes.AsQueryable();
+
+            int totalItem = queryable.Count();
+
+            var lists = queryable
+                    .ApplySortAndPaging(request)
+                    .AsNoTracking()
+                    .ToList();
+
             foreach (var item in lists)
             {
                 item.TeacherName = teacherResult.GetAwaiter().GetResult().Content?.Result?.FirstOrDefault(p => p.Id == item.TeacherId)?.Human?.FullName;
                 item.CSOName = csosResult.GetAwaiter().GetResult().Content?.Result?.FirstOrDefault(p => p.Id == item.CSOId)?.FullName;
                 item.PackageCode = packagesResult.GetAwaiter().GetResult().Content?.Result?.FirstOrDefault(p => p.Id == item.PackageId)?.Code;
-                item.AveragePT = ptr!.FirstOrDefault(p => p.ClassId == item.Id)!.AveragePTPoint;
+                item.AveragePT = Math.Round(ptr!.FirstOrDefault(p => p.ClassId == item.Id)!.AveragePTPoint, 2);
             }
 
             methodResult.Result = new PagingItemsModel<ClassSearchModel>(lists, request, totalItem);
