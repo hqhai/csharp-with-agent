9063ac711da163af3cb43a5cf6e62d1dc5199692
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2023-10-05 14:49:16 +0700

    Update code

--- Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/LessonOverviewModel.cs
+++ Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/LessonOverviewModel.cs
@@ -13,7 +13,6 @@ namespace Fsel.Course.Domain.Models.EntityModels
         public EnumCourseLevel CourseLevel { get; set; }
         public double PercentProgress { get; set; }
         public Guid? MockTestId { get; set; }
-        public Guid? FinalTestId { get; set; }
         public LessonResultModel? LessonResult { get; set; }
         public Guid UnitId { get; set; }
         public IList<LessonInstructionModel>? LessonInstructions { get; set; }
