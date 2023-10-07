577d3061656072409bb155a87e17199cd52e6dee
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2023-10-07 10:35:17 +0700

    Update code

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/VideoTimeCodeAnswerCmd/CreateVideoTimeCodeAnswerCommand.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/VideoTimeCodeAnswerCmd/CreateVideoTimeCodeAnswerCommand.cs
@@ -94,12 +94,17 @@ namespace Fsel.Course.Lms.Application.Commands.VideoTimeCodeAnswerCmd
             var exercise = questions.SelectMany(x => x.ExerciseQuestions).Select(x => x.Exercise).FirstOrDefault();
             var videoTimeCodeQuestion = exercise?.TimeCodeExercises.Select(x => x.VideoTimeCode).FirstOrDefault();
 
-            var videoTimeCode = await _videoTimeCodeRepository.Queryable.Include(x => x.VideoTimeCodeAnswers.Where(x => x.VideoResultId == videoResult.Id)).Where(x => x.Id == videoResult.CurrentVideoTimeCodeId).FirstOrDefaultAsync(cancellationToken);
-            if (videoTimeCode != null && videoTimeCode.Id != videoTimeCodeQuestion?.Id && videoTimeCode.VideoTimeCodeAnswers.Any() && videoTimeCode.VideoTimeCodeAnswers.All(x => x.Status == EnumCurrentStatus.Process))
-            {
-                methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataAlreadyExist), nameof(EnumVideoTimeCodeErrorCode.VideoTimeCodePreviousNotDone));
-                return methodResult;
-            }
+            #region Chặn Time Code Chưa Done
+
+            //var videoTimeCode = await _videoTimeCodeRepository.Queryable.Include(x => x.VideoTimeCodeAnswers.Where(x => x.VideoResultId == videoResult.Id)).Where(x => x.Id == videoResult.CurrentVideoTimeCodeId).FirstOrDefaultAsync(cancellationToken);
+            //if (videoTimeCode != null && videoTimeCode.Id != videoTimeCodeQuestion?.Id && videoTimeCode.VideoTimeCodeAnswers.Any() && videoTimeCode.VideoTimeCodeAnswers.All(x => x.Status == EnumCurrentStatus.Process))
+            //{
+            //    methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataAlreadyExist), nameof(EnumVideoTimeCodeErrorCode.VideoTimeCodePreviousNotDone));
+            //    return methodResult;
+            //}
+
+            #endregion Chặn Time Code Chưa Done
+
             videoResult.CurrentVideoTimeCodeId = videoTimeCodeQuestion?.Id ?? default;
 
             #endregion Validation
