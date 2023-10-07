c77d2d74cf7f9ce0af835371a292e85b56cbd0a8
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2023-10-07 09:05:10 +0700

    Update code Student Progress HomeWork

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/StudentProgressQuery/GetStudentProgressHomeWorkQuery.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/StudentProgressQuery/GetStudentProgressHomeWorkQuery.cs
@@ -96,7 +96,7 @@ namespace Fsel.Course.Lms.Application.Queries.StudentProgressQuery
                                       .Include(x => x!.LessonHomeWorks)
                                       .Include(x => x!.HomeWorkQuestions)
                                       .ThenInclude(x => x.Question)
-                                      .Include(x => x.HomeWorkResults)
+                                      .Include(x => x.HomeWorkResults.Where(x => x.LessonResultId == lessonResult.Id))
                                       .ThenInclude(x => x.HomeWorkAnswers)
                                       .Where(x => x.LessonHomeWorks.Any(x => x.LessonId == lessonResult.LessonId))
                                       .AsNoTracking()
@@ -109,7 +109,7 @@ namespace Fsel.Course.Lms.Application.Queries.StudentProgressQuery
                                           CourseSkill = h.CourseSkill,
                                           CourseLevel = h.CourseLevel,
                                           QuestionTotal = h.HomeWorkQuestions.Select(x => x.Question).Count(),
-                                          QuestionCompleted = h.HomeWorkResults.FirstOrDefault(x => x.HomeWorkId == h.Id && x.LessonResultId == lessonResult.Id) != null ? h.HomeWorkResults.FirstOrDefault(x => x.HomeWorkId == h.Id && x.LessonResultId == lessonResult.Id)!.HomeWorkAnswers.Count : default,
+                                          QuestionCompleted = h.HomeWorkResults.Any() ? h.HomeWorkResults.FirstOrDefault(x => x.HomeWorkId == h.Id && x.LessonResultId == lessonResult.Id)!.HomeWorkAnswers.Count : default,
                                           HomeWorkResult = _mapper.Map<HomeWorkResultModel>(h.HomeWorkResults.FirstOrDefault(x => x.HomeWorkId == h.Id && x.LessonResultId == lessonResult.Id))
                                       })
                                       .ToListAsync(cancellationToken);
