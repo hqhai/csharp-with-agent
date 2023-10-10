a6c5ce296698495bd0ada65fbaa4b654dbafe119
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2023-10-10 10:41:56 +0700

    Update code get pt

---
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/PlacementTestCmd/ChooseStudentCourseCommand.cs
@@ -0,0 +1,73 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+using Fsel.Common.ActionResults;
+using Fsel.Common.Enums.ErrorCodes;
+using Fsel.Core.Base;
+using Fsel.Course.Domain.Enums;
+using Fsel.Course.Domain.IRepositories;
+using Fsel.Course.Lms.Application.Services.OrderServices;
+using Fsel.Course.Lms.Application.Services.OrderServices.Model;
+using Fsel.Course.Lms.Application.Services.UserServices;
+using Fsel.Shared.Enums;
+using Fsel.Shared.Enums.ErrorCodes;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fsel.Course.Lms.Application.Commands.PlacementTestCmd
+{
+    public class ChooseStudentCourseCommand : IRequest<MethodResult<bool>>
+    {
+        public EnumCourseLevel Level { get; set; }
+    }
+
+    public class ChooseStudentCourseCommandHandler : IRequestHandler<ChooseStudentCourseCommand, MethodResult<bool>>
+    {
+        private readonly IUserService _userService;
+        private readonly IOrderService _orderService;
+        private readonly AuthContext _authContext;
+        private readonly ICourseRepository _courseRepository;
+
+        public ChooseStudentCourseCommandHandler(IUserService userService, IOrderService orderService, AuthContext authContext, ICourseRepository courseRepository)
+        {
+            _userService = userService;
+            _orderService = orderService;
+            _authContext = authContext;
+            _courseRepository = courseRepository;
+        }
+
+        public async Task<MethodResult<bool>> Handle(ChooseStudentCourseCommand request, CancellationToken cancellationToken)
+        {
+            ArgumentNullException.ThrowIfNull(request);
+            var methodResult = new MethodResult<bool>();
+
+            var studentResult = await _userService.GetStudentByUserIdAsync(_authContext.CurrentUserId);
+            if (!studentResult.IsSuccessStatusCode)
+            {
+                methodResult.AddErrorBadRequest(nameof(EnumServicesErrorCode.CallUserServiceError), nameof(studentResult));
+                return methodResult;
+            }
+            var student = studentResult?.Content?.Result;
+
+            Random random = new Random();
+            var courses = await _courseRepository.Queryable.Where(x => x.CourseLevel == request.Level && x.Status == EnumCourseStatus.Active).ToListAsync(cancellationToken);
+            var course = courses.OrderBy(x => random.Next(courses.Count)).FirstOrDefault();
+            if (course == null)
+            {
+                methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist), nameof(course));
+                return methodResult;
+            }
+            await _orderService.CreateOrder(new CreateOrderCommandModel
+            {
+                Address = "Viet Nam",
+                Country = "Viet Nam",
+                CourseId = course.Id,
+                CourseLevel = request.Level,
+                FullName = student?.Human?.FullName,
+                PaymentMethod = EnumPaymentMethodStatus.Card,
+                CodeCourse = course.Code,
+                UserId = _authContext.CurrentUserId
+            }).ConfigureAwait(false);
+            return methodResult;
+        }
+    }
+}
