5b30bd52429d1865af22cab22159e5fce1cdc773
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2023-10-16 10:31:42 +0700

    Update code

---
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/MockTestQuery/GetSectionQuestionBySectionGorupIdQuery.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Course.Lms.Application.Queries.MockTestQuery
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class GetSectionQuestionBySectionGorupIdQuery
+    {
+    }
+}
---
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/QuestionQuery/GetQuestionByIdQuery.cs
@@ -0,0 +1,8 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Course.Lms.Application.Queries.MockTestQuery
+{
+    public class GetQuestionByIdQuery
+    {
+    }
+}
