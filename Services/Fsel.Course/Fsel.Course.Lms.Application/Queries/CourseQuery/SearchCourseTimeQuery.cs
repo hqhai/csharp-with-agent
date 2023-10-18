9b618acfb3e4eb0e4f85fea9825402e159a1edf0
tunghung <tunghung@atlantic.edu.vn>
2023-10-18 11:23:00 +0700

    fix bug api

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/CourseQuery/SearchCourseTimeQuery.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/CourseQuery/SearchCourseTimeQuery.cs
@@ -62,12 +62,14 @@ namespace Fsel.Course.Lms.Application.Queries.CourseQuery
                     .AsNoTracking()
                     .ToListAsync(cancellationToken: cancellationToken)
                     .ConfigureAwait(false);
-            var courseTimeResult = await _systemService.GetCourseTimeConfigAsync(lists.Select(x => x.Id).ToList());
+            var courseTimeResult = await _systemService.GetCourseTimeConfigAsync();
             var courseTimes = courseTimeResult.Content?.Result;
             foreach (var item in lists)
             {
-                item.DurationMonth = courseTimes?.Select(x => x.DurationMonth).FirstOrDefault();
-                item.EnrollmentWeek = courseTimes?.Select(x => x.EnrollmentWeek).FirstOrDefault();
+                /*item.DurationMonth = courseTimes?.Where(x => x.CourseId == lists.Select(x => x.Id).FirstOrDefault()).Select(x => x.DurationMonth).FirstOrDefault();
+                item.EnrollmentWeek = courseTimes?.Select(x => x.EnrollmentWeek).FirstOrDefault();*/
+                item.DurationMonth = courseTimes?.Items?.FirstOrDefault(x => x.CourseId == item.Id)?.DurationMonth;
+                item.EnrollmentWeek = courseTimes?.Items?.FirstOrDefault(x => x.CourseId == item.Id)?.EnrollmentWeek;
             }
             methodResult.Result = new PagingItemsModel<CourseSearchModel>(lists, request, totalItem);
             methodResult.StatusCode = StatusCodes.Status200OK;
--- Services/Fsel.Course/Fsel.Course.Lms.Application/Services/SystemService/ISystemService.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Services/SystemService/ISystemService.cs
@@ -3,14 +3,15 @@
 namespace Fsel.Course.Lms.Application.Services.SystemService
 {
     using Fsel.Common.ActionResults;
+    using Fsel.Core.Base.BaseModels;
     using Fsel.Course.Lms.Application.Services.SystemService.Models;
     using Microsoft.AspNetCore.Mvc;
     using Refit;
 
     public interface ISystemService
     {
-        [Post("/course-time-config")]
-        Task<IApiResponse<MethodResult<IList<CourseTimeConfigModel>>>> GetCourseTimeConfigAsync([Body] IList<Guid> courseIds);
+        [Get("/course-time-config")]
+        Task<IApiResponse<MethodResult<PagingItemsModel<CourseTimeConfigModel>>>> GetCourseTimeConfigAsync();
 
         [Get("/log-action/{userId}")]
         Task<IApiResponse<MethodResult<LogActionDaysModel>>> GetLogActionsByUserId([FromRoute] Guid userId);
