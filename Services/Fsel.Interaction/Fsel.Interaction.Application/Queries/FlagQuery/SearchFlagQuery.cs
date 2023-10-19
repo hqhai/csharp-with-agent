1d03a18d9357b918f05ada8f016840c318f03e92
tunghung <tunghung@atlantic.edu.vn>
2023-10-19 18:02:26 +0700

    fix comment

--- Services/Fsel.Interaction/Fsel.Interaction.Application/Queries/FlagQuery/SearchFlagQuery.cs
+++ Services/Fsel.Interaction/Fsel.Interaction.Application/Queries/FlagQuery/SearchFlagQuery.cs
@@ -33,39 +33,8 @@ namespace Fsel.Interaction.Application.Queries.FlagQuery
         public async Task<MethodResult<PagingItemsModel<FlagModel>>> Handle(SearchFlagQuery request, CancellationToken cancellationToken)
         {
             ArgumentNullException.ThrowIfNull(request);
-            var methodResult = new MethodResult<PagingItemsModel<FlagModel>>();
-
-            if (request.PageSize > 100)
-            {
-                methodResult.StatusCode = StatusCodes.Status400BadRequest;
-                return methodResult;
-            }
-
-            var flagQuery = _flagRepository.Queryable
-                        .Where(x => x.Status == EnumFlagStatus.New)
-                        .Select(x => new FlagModel
-                        {
-                            Id = x.Id,
-                            CreatedDate = x.CreatedDate,
-                            CreatedFullName = x.CreatedFullName,
-                            FeedBack = x.FeedBack,
-                            FlagIssue = x.FlagIssue,
-                            ObjectId = x.ObjectId,
-                            Status = x.Status,
-                            Type = x.Type,
-                        });
-
-            int totalItem = await flagQuery.CountAsync(cancellationToken: cancellationToken).ConfigureAwait(false);
-
-            var lists = await flagQuery
-                    .ApplySortAndPaging(request)
-                    .AsNoTracking()
-                    .ToListAsync(cancellationToken: cancellationToken)
-                    .ConfigureAwait(false);
-
-            methodResult.Result = new PagingItemsModel<FlagModel>(lists, request, totalItem);
-            methodResult.StatusCode = StatusCodes.Status200OK;
-            return methodResult;
+            var flagQuery = _flagRepository.Queryable.Where(x => x.Status == EnumFlagStatus.New);
+            return await _flagRepository.GetListByPageResultAsync<FlagModel>(flagQuery, request, cancellationToken).ConfigureAwait(false);
         }
     }
 }
