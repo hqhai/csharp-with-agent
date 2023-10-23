ffcc1122a388ac0a01391d77472c5f4f5ceb706d
tunghung <tunghung@atlantic.edu.vn>
2023-10-23 17:21:27 +0700

    fix bug update current student darft

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/ClassForumQuery/GetClassForumQuery.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/ClassForumQuery/GetClassForumQuery.cs
@@ -112,7 +112,7 @@ namespace Fsel.Course.Lms.Application.Queries.ClassForumQuery
             var query = await _classForumResultRepository.Queryable
                 .Include(x => x.ClassForumResultFiles)
                 .Include(x => x.ClassForumScores)
-                .Where(x => x.ClassForumId == classForum.Id && classStudentIds!.Contains(x.StudentId) && x.Status != EnumClassForumResultStatus.Draft)
+                .Where(x => x.ClassForumId == classForum.Id && classStudentIds!.Contains(x.StudentId))
                 .OrderBy(x => x.CreatedDate)
                 .ToListAsync(cancellationToken);
 
@@ -152,7 +152,7 @@ namespace Fsel.Course.Lms.Application.Queries.ClassForumQuery
                 {
                     // Lấy bài post học sinh trong lớp
                     var classForumResultAllStudents = classForumResults.Where(x => x.ClassForumId == classForum.Id &&
-                                    x.Id != classForumResultCurrentStudent.Id && x.Status != EnumClassForumResultStatus.Pending
+                                    x.Id != classForumResultCurrentStudent.Id && x.Status != EnumClassForumResultStatus.Pending && x.Status != EnumClassForumResultStatus.Draft
                                     ).ToList();
                     classForumByStudentModel.ClassForumResultAllStudents = classForumResultAllStudents;
 
