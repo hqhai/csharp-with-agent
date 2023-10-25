c765303236b761c22965e3aec68b8de6664cf647
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2023-10-25 16:14:05 +0700

    update get list course teacher and create order

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/CourseQuery/GetListCourseTeacherQuery.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/CourseQuery/GetListCourseTeacherQuery.cs
@@ -48,7 +48,7 @@ namespace Fsel.Course.Lms.Application.Queries.CourseQuery
             MethodResult<IList<CourseModel>> methodResult = new MethodResult<IList<CourseModel>>();
             var courses = await _courseRepository.Queryable
                               .Include(course => course.CourseTeachers.Where(y => !y.IsDeleted))
-                              .Where(x => request.CourseLevel == null || x.CourseLevel == request.CourseLevel)
+                              .Where(x => request.CourseLevel != null && x.CourseLevel == request.CourseLevel)
                               .Where(x => x.Status == EnumCourseStatus.Active)
                               .AsNoTracking()
                               .Select(course => new CourseModel
