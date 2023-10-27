914e92358d0d5ddf4d43480bf36bb13e9574c130
tunghung <tunghung@atlantic.edu.vn>
2023-10-27 09:04:59 +0700

    update

--- Services/Fsel.Cms.PlanetDefender/Fsel.Cms.PlanetDefender.Domain/Models/EntityModels/SpaceShipModel.cs
+++ Services/Fsel.Cms.PlanetDefender/Fsel.Cms.PlanetDefender.Domain/Models/EntityModels/SpaceShipModel.cs
@@ -2,9 +2,6 @@
 
 namespace Fsel.Cms.PlanetDefender.Domain.Models.EntityModels
 {
-    using System;
-    using System.Threading;
-    using System.Threading.Tasks;
     using Fsel.Core.Base.BaseModels;
 
     public class SpaceShipModel : BaseModel
@@ -12,10 +9,5 @@ namespace Fsel.Cms.PlanetDefender.Domain.Models.EntityModels
         public bool IsDefault { get; set; }
         public string? Name { get; set; }
         public string? Code { get; set; }
-
-        public Task<int> CountAsync(CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
