599c95a2e2fafa0d7803c30e92c1f803b0aac0d2
tunghung <tunghung@atlantic.edu.vn>
2023-10-27 09:10:51 +0700

    fix comment

--- Services/Fsel.Cms.PlanetDefender/Fsel.Cms.PlanetDefender.Application/Queries/SpaceShipQuery/SearchSpaceShipQuery.cs
+++ Services/Fsel.Cms.PlanetDefender/Fsel.Cms.PlanetDefender.Application/Queries/SpaceShipQuery/SearchSpaceShipQuery.cs
@@ -8,10 +8,7 @@ namespace Fsel.Cms.PlanetDefender.Application.Queries.SpaceShipQuery
     using Fsel.Cms.PlanetDefender.Domain.Models.EntityModels;
     using Fsel.Common.ActionResults;
     using Fsel.Core.Base.BaseModels;
-    using Fsel.Core.Extensions;
     using MediatR;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.EntityFrameworkCore;
 
     public class SearchSpaceShipQuery : BaseQueryModel, IRequest<MethodResult<PagingItemsModel<SpaceShipModel>>>
     {
@@ -30,31 +27,8 @@ namespace Fsel.Cms.PlanetDefender.Application.Queries.SpaceShipQuery
         public async Task<MethodResult<PagingItemsModel<SpaceShipModel>>> Handle(SearchSpaceShipQuery request, CancellationToken cancellationToken)
         {
             ArgumentNullException.ThrowIfNull(request);
-            var methodResult = new MethodResult<PagingItemsModel<SpaceShipModel>>();
-
-            var spaceShip = _spaceShipRepository
-                        .Queryable
-                        .Where(x => x.IsDefault == request.IsDefault)
-                        .Select(x => new SpaceShipModel
-                        {
-                            Id = x.Id,
-                            Name = x.Name,
-                            Code = x.Code,
-                            IsDefault = x.IsDefault,
-                            CreatedDate = x.CreatedDate,
-                            CreatedFullName = x.CreatedFullName,
-                        });
-
-            int totalItem = await spaceShip.CountAsync(cancellationToken: cancellationToken).ConfigureAwait(false);
-            var lists = await spaceShip
-                    .ApplySortAndPaging(request)
-                    .AsNoTracking()
-                    .ToListAsync(cancellationToken: cancellationToken)
-                    .ConfigureAwait(false);
-
-            methodResult.Result = new PagingItemsModel<SpaceShipModel>(lists, request, totalItem);
-            methodResult.StatusCode = StatusCodes.Status200OK;
-            return methodResult;
+            var spaceShipsQuery = _spaceShipRepository.Queryable.Where(x => x.IsDefault == request.IsDefault);
+            return await _spaceShipRepository.GetListByPageResultAsync<SpaceShipModel>(spaceShipsQuery, request, cancellationToken).ConfigureAwait(false);
         }
     }
 }
