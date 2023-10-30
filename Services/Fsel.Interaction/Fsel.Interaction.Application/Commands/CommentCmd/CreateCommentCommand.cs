397ebd42d3af0562524b42b0c2cf05021012f6eb
hqhai <quanghai@atlantic.edu.vn>
2023-10-30 22:08:45 +0700

    Thêm đầu thông báo CSO xóa Comment, Xóa bài ClassForum

--- Services/Fsel.Interaction/Fsel.Interaction.Application/Commands/CommentCmd/CreateCommentCommand.cs
+++ Services/Fsel.Interaction/Fsel.Interaction.Application/Commands/CommentCmd/CreateCommentCommand.cs
@@ -115,7 +115,7 @@ namespace Fsel.Interaction.Application.Commands.CommentCmd
                             Content = EnumNotificationContent.Comment,
                             Type = EnumNotificationType.LinkComment,
                             SenderId = _authContext.CurrentUserId,
-                            ParamsLink = new List<object> { classForumResult?.UnitId ?? default, classForumResult?.CourseId ?? default, classForumResult?.UnitId ?? default, request.ObjectId, comment.Id },
+                            ParamsLink = new List<object> { classForumResult?.UnitId ?? default, classForumResult?.CourseId ?? default,  request.ObjectId, comment.Id },
                             PlatformCode = EnumPlatformCode.LMS
                         };
                         await _classForumCommentPublisher.Publish(model, cancellationToken).ConfigureAwait(false);
@@ -154,7 +154,7 @@ namespace Fsel.Interaction.Application.Commands.CommentCmd
                             Content = EnumNotificationContent.ReplyComment,
                             Type = EnumNotificationType.LinkComment,
                             SenderId = _authContext.CurrentUserId,
-                            ParamsLink = new List<object> { classForumResultOfCommentOwner?.UnitId ?? default, classForumResultOfCommentOwner?.CourseId ?? default, classForumResultOfCommentOwner?.UnitId ?? default, commentOwner?.ObjectId ?? default, request.ObjectId, comment.Id },
+                            ParamsLink = new List<object> { classForumResultOfCommentOwner?.UnitId ?? default, classForumResultOfCommentOwner?.CourseId ?? default,commentOwner?.ObjectId ?? default, request.ObjectId, comment.Id },
                             PlatformCode = EnumPlatformCode.LMS
                         };
                         await _classForumCommentPublisher.Publish(model, cancellationToken).ConfigureAwait(false);
--- Services/Fsel.Notification/Fsel.Notification.Infrastructure/Migrations/20231030141256_Update_NotificationTypeTable_Add_TemplateLink.cs
+++ Services/Fsel.Notification/Fsel.Notification.Infrastructure/Migrations/20231030141256_Update_NotificationTypeTable_Add_TemplateLink.cs
@@ -1,7 +1,9 @@
-using System;
+﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
 
 namespace Fsel.Notification.Infrastructure.Migrations
 {
@@ -16,7 +18,7 @@ namespace Fsel.Notification.Infrastructure.Migrations
                 keyColumn: "Id",
                 keyValue: new Guid("810c70ca-4f3c-4d02-a194-708fa45d42ea"),
                 column: "TemplateLink",
-                value: "/learn/{0}?courseId={1}&unitId={0}&type=classForum&classForumResultId={3}&commentId={4}");
+                value: "/learn/{0}?courseId={1}&unitId={0}&type=classForum&classForumResultId={2}&commentId={3}");
 
             migrationBuilder.UpdateData(
                 table: "NotificationTypes",
@@ -30,7 +32,7 @@ namespace Fsel.Notification.Infrastructure.Migrations
                 keyColumn: "Id",
                 keyValue: new Guid("9e983172-dba2-4ca7-b602-372c011ecb99"),
                 column: "TemplateLink",
-                value: "/learn/{0}?courseId={1}&unitId={2}&type=classForum&classForumResultId={3}&repCommentId={4}");
+                value: "/learn/{0}?courseId={1}&unitId={0}&type=classForum&classForumResultId={1}&commentId={2}&repCommentId={3}");
 
             migrationBuilder.UpdateData(
                 table: "NotificationTypes",
@@ -44,7 +46,7 @@ namespace Fsel.Notification.Infrastructure.Migrations
                 keyColumn: "Id",
                 keyValue: new Guid("d789788a-1ba5-405b-b280-ae92cd3b4fc2"),
                 column: "TemplateLink",
-                value: "/learn/{0}?courseId={1}&unitId={0}&type=classForum&classForumResultId={3}");
+                value: "/learn/{0}?courseId={1}&unitId={0}&type=classForum&classForumResultId={2}");
 
             migrationBuilder.UpdateData(
                 table: "NotificationTypes",
@@ -53,17 +55,29 @@ namespace Fsel.Notification.Infrastructure.Migrations
                 column: "TemplateLink",
                 value: "/class-forum-management");
 
-            migrationBuilder.UpdateData(
+            migrationBuilder.InsertData(
                 table: "NotificationTypes",
-                keyColumn: "Id",
-                keyValue: new Guid("efba1f99-8fad-47b5-af14-978f674f438e"),
-                column: "TemplateLink",
-                value: "/learn/{0}?courseId={1}&unitId={0}&type=classForum&classForumResultId={2}&commentId={3}");
+                columns: new[] { "Id", "Content", "CreatedDate", "CreatedFullName", "CreatedUserId", "DeletedDate", "DeletedFullName", "DeletedUserId", "Icon", "IsDeleted", "Priority", "TemplateLink", "TemplateMessage", "Type", "UpdatedDate", "UpdatedFullName", "UpdatedUserId" },
+                values: new object[,]
+                {
+                    { new Guid("17c0df26-f131-42bf-b315-0a8a85524f6f"), "DeleteClassForumResult", new DateTime(2023, 8, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), "", new Guid("00000000-0000-0000-0000-000000000000"), null, null, null, "", false, 1, "/learn/{0}?courseId={1}&unitId={0}&type=classForum&classForumResultId={2}", "Bài viết của bạn đã vi phạm Tiêu chuẩn cộng đồng của FSEL và đã bị xóa", "LinkPage", null, null, null },
+                    { new Guid("96c0df96-f131-42bf-b315-0a8a85524f6f"), "DeleteComment", new DateTime(2023, 8, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), "", new Guid("00000000-0000-0000-0000-000000000000"), null, null, null, "", false, 1, "/learn/{0}?courseId={1}&unitId={0}&type=classForum&classForumResultId={2}&commentId={3}", "Bình luận của bạn đã vi phạm Tiêu chuẩn cộng đồng của FSEL và đã bị xóa", "LinkComment", null, null, null }
+                });
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DeleteData(
+                table: "NotificationTypes",
+                keyColumn: "Id",
+                keyValue: new Guid("17c0df26-f131-42bf-b315-0a8a85524f6f"));
+
+            migrationBuilder.DeleteData(
+                table: "NotificationTypes",
+                keyColumn: "Id",
+                keyValue: new Guid("96c0df96-f131-42bf-b315-0a8a85524f6f"));
+
             migrationBuilder.UpdateData(
                 table: "NotificationTypes",
                 keyColumn: "Id",
@@ -103,13 +117,6 @@ namespace Fsel.Notification.Infrastructure.Migrations
                 table: "NotificationTypes",
                 keyColumn: "Id",
                 keyValue: new Guid("d789788a-1ba6-405b-f280-ae92cd3b4fc2"),
-                column: "TemplateLink",
-                value: "");
-
-            migrationBuilder.UpdateData(
-                table: "NotificationTypes",
-                keyColumn: "Id",
-                keyValue: new Guid("efba1f99-8fad-47b5-af14-978f674f438e"),
                 column: "TemplateLink",
                 value: "");
         }
