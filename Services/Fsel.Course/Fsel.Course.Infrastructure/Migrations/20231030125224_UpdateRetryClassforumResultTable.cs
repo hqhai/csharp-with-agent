b9080d15f5b23f8e2827d829a58f5829e35f9c47
tunghung <tunghung@atlantic.edu.vn>
2023-10-30 19:55:28 +0700

    Create api retry class forum result

---
+++ Services/Fsel.Course/Fsel.Course.Infrastructure/Migrations/20231030125224_UpdateRetryClassforumResultTable.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fsel.Course.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateRetryClassforumResultTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "ClassForums");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RetryContent",
+                table: "ClassForumResults",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RetryGradingAlFeedBack",
+                table: "ClassForumResults",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RetryWordContent",
+                table: "ClassForumResults",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RetryContent",
+                table: "ClassForumResults");
+
+            migrationBuilder.DropColumn(
+                name: "RetryGradingAlFeedBack",
+                table: "ClassForumResults");
+
+            migrationBuilder.DropColumn(
+                name: "RetryWordContent",
+                table: "ClassForumResults");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "ClassForums",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
--- Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/ClassForumResultController.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/ClassForumResultController.cs
@@ -61,5 +61,19 @@ namespace Fsel.Course.Lms.Api.Controllers
             var commandResult = await _mediator.Send(new GetClassForumResultQuery { ClassForumResultId = id }).ConfigureAwait(false);
             return commandResult.GetActionResult();
         }
+
+        /// <summary>
+        /// Update try again
+        /// </summary>
+        [HttpPut("Retry/{id}")]
+        [ProducesResponseType(typeof(MethodResult<ClassForumResultModel>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> UpdateStatus([FromRoute] Guid id, [FromBody] RetryClassForumResultCommand command)
+        {
+            ArgumentNullException.ThrowIfNull(command);
+            command.Id = id;
+            MethodResult<ClassForumResultModel> commandResult = await _mediator.Send(command).ConfigureAwait(false);
+            return commandResult.GetActionResult();
+        }
     }
 }
