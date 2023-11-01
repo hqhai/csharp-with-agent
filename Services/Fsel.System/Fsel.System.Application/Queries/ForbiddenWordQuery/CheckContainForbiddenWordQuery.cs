dc0ed1b299cd291fa1c9b0067482bbeffc91173b
dungdh2 <dungdhph25174@fpt.edu.vn>
2023-11-01 16:27:16 +0700

    Bug ForbiddenWord

--- Services/Fsel.System/Fsel.System.Application/Queries/ForbiddenWordQuery/CheckContainForbiddenWordQuery.cs
+++ Services/Fsel.System/Fsel.System.Application/Queries/ForbiddenWordQuery/CheckContainForbiddenWordQuery.cs
@@ -36,9 +36,18 @@ namespace Fsel.System.Application.Queries.ForbiddenWordQuery
                 return methodResult;
             }
 
+            if (request.Word == null)
+            {
+                methodResult.StatusCode = StatusCodes.Status400BadRequest;
+                return methodResult;
+            }
+
+            var inputWord = StringHelper.RemoveHTMLTags(request.Word.Trim('"'));
             var forbiddenWords = await _forbiddenWordRepository.Queryable
-             .Where(x => !string.IsNullOrEmpty(x.Word) && StringHelper.RemoveHTMLTags(request.Word).Trim().ToLower().Contains(x.Word.Trim().ToLower()))
-             .Select(x => x.Word ?? string.Empty).Distinct().ToListAsync(cancellationToken: cancellationToken);
+                .Where(x => (" " + inputWord.Trim().ToLower() + " ").Contains(" " + x.Word.ToLower() + " "))
+                .Select(x => x.Word.ToLower())
+                .Distinct()
+                .ToListAsync(cancellationToken: cancellationToken);
             methodResult.Result = forbiddenWords;
             methodResult.StatusCode = StatusCodes.Status200OK;
             return methodResult;
