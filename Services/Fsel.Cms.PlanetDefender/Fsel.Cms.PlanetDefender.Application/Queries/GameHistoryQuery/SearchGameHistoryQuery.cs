bc01cc5547d7c2b79a86eead5d916066d6b113e1
tunghung <tunghung@atlantic.edu.vn>
2023-11-01 15:29:26 +0700

    fix api

--- Services/Fsel.Cms.PlanetDefender/Fsel.Cms.PlanetDefender.Application/Queries/GameHistoryQuery/SearchGameHistoryQuery.cs
+++ Services/Fsel.Cms.PlanetDefender/Fsel.Cms.PlanetDefender.Application/Queries/GameHistoryQuery/SearchGameHistoryQuery.cs
@@ -5,9 +5,11 @@ namespace Fsel.Cms.PlanetDefender.Application.Queries.GameHistoryQuery
     using System;
     using System.Threading;
     using System.Threading.Tasks;
+    using Fsel.Cms.PlanetDefender.Application.Services.UserServices;
     using Fsel.Cms.PlanetDefender.Domain.IRepositories;
     using Fsel.Cms.PlanetDefender.Domain.Models.EntityModels;
     using Fsel.Common.ActionResults;
+    using Fsel.Core.Base;
     using Fsel.Core.Base.BaseModels;
     using MediatR;
 
@@ -18,16 +20,24 @@ namespace Fsel.Cms.PlanetDefender.Application.Queries.GameHistoryQuery
     public class SearchGameHistoryQueryHandler : IRequestHandler<SearchGameHistoryQuery, MethodResult<PagingItemsModel<GameHistoryModel>>>
     {
         private readonly IGameHistoryRepository _gameHistoryRepository;
+        private readonly AuthContext _authContext;
+        private readonly IUserService _userService;
 
-        public SearchGameHistoryQueryHandler(IGameHistoryRepository gameHistoryRepository)
+        public SearchGameHistoryQueryHandler(IGameHistoryRepository gameHistoryRepository, AuthContext authContext, IUserService userService)
         {
             _gameHistoryRepository = gameHistoryRepository;
+            _authContext = authContext;
+            _userService = userService;
         }
 
         public async Task<MethodResult<PagingItemsModel<GameHistoryModel>>> Handle(SearchGameHistoryQuery request, CancellationToken cancellationToken)
         {
             ArgumentNullException.ThrowIfNull(request);
-            var gameHistory = _gameHistoryRepository.Queryable;
+
+            var studentResult = await _userService.GetStudentByUserIdAsync(_authContext.CurrentUserId);
+            var studentId = studentResult.Content?.Result?.Id;
+
+            var gameHistory = _gameHistoryRepository.Queryable.Where(x => x.StudentId == studentId);
             return await _gameHistoryRepository.GetListByPageResultAsync<GameHistoryModel>(gameHistory, request, cancellationToken).ConfigureAwait(false);
         }
     }
