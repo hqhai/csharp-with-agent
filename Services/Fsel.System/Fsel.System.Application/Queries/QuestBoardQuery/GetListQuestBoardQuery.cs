67ccc0393730958293928736f84906a9d1a89fd5
hqhai <quanghai@atlantic.edu.vn>
2023-11-02 17:12:05 +0700

    query questboard

--- Services/Fsel.System/Fsel.System.Application/Queries/QuestBoardQuery/GetListQuestBoardQuery.cs
+++ Services/Fsel.System/Fsel.System.Application/Queries/QuestBoardQuery/GetListQuestBoardQuery.cs
@@ -5,22 +5,19 @@ namespace Fsel.System.Application.Queries.QuestBoardQuery
     using AutoMapper;
     using Fsel.Common.ActionResults;
     using Fsel.Shared.Enums;
-    using Fsel.System.Domain.Entities;
     using Fsel.System.Domain.IRepositories;
     using Fsel.System.Domain.Models.EntityModels;
     using global::System;
     using global::System.Collections.Generic;
     using global::System.Linq;
-    using global::System.Text;
     using global::System.Threading.Tasks;
     using MediatR;
     using Microsoft.AspNetCore.Http;
-    using Microsoft.EntityFrameworkCore;
 
     public class GetListQuestBoardQuery : IRequest<MethodResult<List<QuestBoardModel>>>
     {
         public Guid PackageId { get; set; }
-        public EnumQuestBoardCategory CategoryBoard { get; set; }
+        public IList<EnumQuestBoardCategory>? CategoryBoards { get; set; }
 
     }
 
@@ -37,9 +34,14 @@ namespace Fsel.System.Application.Queries.QuestBoardQuery
         {
             ArgumentNullException.ThrowIfNull(request);
             var methodResult = new MethodResult<List<QuestBoardModel>>();
-            var questBoard = _questBoardRepository.Queryable
-     .Where(x => x.Category == request.CategoryBoard )
-     .ToList();
+            var questBoardQuery = _questBoardRepository.Queryable
+                         .Where(x => x.PackageIdsStr!.Contains(request.PackageId.ToString())).ToList();
+
+            var questBoard = from board in questBoardQuery
+                             join category in request.CategoryBoards! on board.Category equals category
+                             select board;
+
+
             methodResult.Result = _mapper.Map<List<QuestBoardModel>>(questBoard);
             methodResult.StatusCode = StatusCodes.Status200OK;
             return methodResult;
