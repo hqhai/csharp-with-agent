f05279cd05b3235b7aecbca0069afb7753c5299d
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2023-11-20 15:36:05 +0700

    Update Get Lesson Overview

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/CourseQuery/GetCourseQuery.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/CourseQuery/GetCourseQuery.cs
@@ -167,7 +167,7 @@ namespace Fsel.Course.Lms.Application.Queries.CourseQuery
                 StudentId = studentId ?? default,
                 Status = EnumResultStatus.New
             });
-            var unitIds = course.CourseUnitMockTests.Where(x => x.UnitId != null).OrderBy(x => x.DisplayOrder).Select(x => x.UnitId).ToList();
+            var unitIds = course.CourseUnitMockTests.Where(x => x.UnitId != null).OrderBy(x => x.DisplayOrder).ThenBy(x => x.CreatedDate).Select(x => x.UnitId).ToList();
             var finalTestIds = course.CourseUnitMockTests.Where(x => x.FinalTestId != null).Select(x => x.FinalTestId).ToList();
             var mockTestIds = course.CourseUnitMockTests.Where(x => x.MockTestId != null).Select(x => x.MockTestId).ToList();
             if (unitIds.Count > 0)
