58485c17fc5e8db6a16a36e00f9f91b764c34f61
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2023-12-07 13:19:37 +0700

    Update Create PT

--- Services/Fsel.Course/Fsel.Course.Domain/Models/CommandModels/PlacementTestAnswers/CreatePlacementTestAnswerBySectionGroupCommandModel.cs
+++ Services/Fsel.Course/Fsel.Course.Domain/Models/CommandModels/PlacementTestAnswers/CreatePlacementTestAnswerBySectionGroupCommandModel.cs
@@ -6,13 +6,13 @@ namespace Fsel.Course.Domain.Models.CommandModels.PlacementTestAnswers
 
     public class CreatePlacementTestAnswerBySectionGroupCommandModel
     {
-        public double TotalQuestion { get; set; }
-        public double CountQuestion { get; set; }
         public Guid PlacementTestResultId { get; set; }
         public Guid SectionGroupId { get; set; }
         public bool IsSubmit { get; set; }
+
         [JsonIgnore]
         public Guid? StudentId { get; set; }
+
         public IList<PlacementTestAnswerQuestionModel>? Answers { get; set; }
     }
 }
