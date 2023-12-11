a01ca50f9e988bc9095dfd87525fbbf61bd89bb9
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2023-12-11 16:34:20 +0700

    fixbug api random question return lack value

---
+++ Services/Fsel.Cms.PlanetDefender/Fsel.Cms.PlanetDefender.Infrastructure/Migrations/20231211070313_Add_Field_IsDisable_To_GameAnswerTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fsel.Cms.PlanetDefender.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Field_IsDisable_To_GameAnswerTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDisable",
+                table: "GameAnswers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDisable",
+                table: "GameAnswers");
+        }
+    }
+}
