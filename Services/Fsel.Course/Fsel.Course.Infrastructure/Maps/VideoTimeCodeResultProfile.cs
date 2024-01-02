9e0791e5142dd998280fd294cdc7dc1fc22583c6
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2024-01-02 15:29:41 +0700

    Update commit code

--- Services/Fsel.Course/Fsel.Course.Infrastructure/Maps/VideoTimeCodeResultProfile.cs
+++ Services/Fsel.Course/Fsel.Course.Infrastructure/Maps/VideoTimeCodeResultProfile.cs
@@ -15,8 +15,7 @@ namespace Fsel.Course.Infrastructure.Maps
             CreateMap<VideoTimeCodeResult, TestResultReportModel>()
             .ForMember(x => x.Score, p => p.MapFrom(o => o.CorrectCount));
             CreateMap<VideoTimeCodeResult, VideoTimeCodeResultModel>()
-            .ForMember(x => x.CorrectCount, p => p.MapFrom(o => o.CorrectCount + o.CorrectCountUngraded))
-            .ForMember(x => x.CorrectTotal, p => p.MapFrom(o => o.CorrectTotal + o.CorrectTotalUngraded));
+            .ForMember(x => x.CurrentVideoTimeCodeId, p => p.MapFrom(o => o.VideoResult != null ? o.VideoResult.CurrentVideoTimeCodeId : null));
         }
     }
 }
