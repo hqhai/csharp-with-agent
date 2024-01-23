124c5219a5b4fb89ae03c84ad592321e62b2cfdc
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2024-01-23 15:35:36 +0700

    Update code Choose Level by PlacementTest and Report Lesson

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/LessonQuery/V1i1/GetLessonReportQuery.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/LessonQuery/V1i1/GetLessonReportQuery.cs
@@ -74,11 +74,10 @@ namespace Fsel.Course.Lms.Application.Queries.V1i1.LessonQuery
             var lessonReport = new LessonReportModel();
             var videoTimeCodes = video.VideoTimeCodes.Where(x => x.TimeCodeType == EnumTimeCodeType.Standalone);
             var questions = videoTimeCodes.SelectMany(x => x.TimeCodeExercises).Select(x => x.Exercise).SelectMany(x => x!.ExerciseQuestions).Select(x => x.Question);
-            var answers = questions.SelectMany(x => x!.VideoTimeCodeAnswers);
             lessonReport.AnswerTime = videoTimeCodes.SelectMany(x => x.VideoTimeCodeResults).Sum(x => x.WorkingTime + x.RetryWorkingTime);
-            lessonReport.Percent = NumberHelper.GetPercent(answers.Sum(x => x.CorrectCount), questions.Where(x => !x!.Ungraded).Sum(x => x!.CorrectTotal));
-            lessonReport.CorrectCount = answers.Sum(x => x.CorrectCount);
-            lessonReport.CorrectTotal = questions.Sum(x => x!.CorrectTotal);
+            lessonReport.CorrectCount = videoTimeCodes.SelectMany(x => x.VideoTimeCodeResults).Sum(x => x.CorrectCount);
+            lessonReport.CorrectTotal = questions.Where(x => !x!.Ungraded).Sum(x => x!.CorrectTotal);
+            lessonReport.Percent = NumberHelper.GetPercent(lessonReport.CorrectCount, lessonReport.CorrectTotal);
             lessonReport.HighestStreak = videoResult.HighestStreak;
             return lessonReport;
         }
