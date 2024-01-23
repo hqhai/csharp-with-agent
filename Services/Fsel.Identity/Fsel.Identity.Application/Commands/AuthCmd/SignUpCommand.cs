c02be7e4244f7157c2bfe91a50841a4bbe42e159
Minh Chiến Lê Xuân <lxmchien@gmail.com>
2024-01-23 11:34:31 +0700

    update-signup

--- Services/Fsel.Identity/Fsel.Identity.Application/Commands/AuthCmd/SignUpCommand.cs
+++ Services/Fsel.Identity/Fsel.Identity.Application/Commands/AuthCmd/SignUpCommand.cs
@@ -91,7 +91,7 @@ namespace Fsel.Identity.Application.Commands.AuthCmd
                     return methodResult;
                 }
                 user = await _userManager.FindByEmailAsync(request.Email);
-                if (user != null && user.EmailConfirmed && !user.IsDeleted)
+                if (user != null && user.EmailConfirmed)
                 {
                     methodResult.AddErrorBadRequest(nameof(EnumAuthUserErrorCode.DuplicateEmail), nameof(request.Email), request.Email);
                     return methodResult;
