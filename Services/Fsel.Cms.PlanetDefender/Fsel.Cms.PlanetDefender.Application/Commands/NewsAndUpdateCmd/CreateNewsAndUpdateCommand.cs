d2020c5c528ca93542174ebc8b95a0d74460a9d6
tunghung <tunghung@atlantic.edu.vn>
2024-01-24 14:27:36 +0700

    update

--- Services/Fsel.Cms.PlanetDefender/Fsel.Cms.PlanetDefender.Application/Commands/NewsAndUpdateCmd/CreateNewsAndUpdateCommand.cs
+++ Services/Fsel.Cms.PlanetDefender/Fsel.Cms.PlanetDefender.Application/Commands/NewsAndUpdateCmd/CreateNewsAndUpdateCommand.cs
@@ -5,14 +5,52 @@ namespace Fsel.Cms.PlanetDefender.Application.Commands.NewsAndUpdateCmd
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Reflection;
     using System.Text;
+    using System.Threading;
     using System.Threading.Tasks;
+    using AutoMapper;
+    using Fsel.Cms.PlanetDefender.Domain.Entities;
+    using Fsel.Cms.PlanetDefender.Domain.IRepositories;
     using Fsel.Cms.PlanetDefender.Domain.Models.CommandModel.NewsAndUpdates;
     using Fsel.Cms.PlanetDefender.Domain.Models.EntityModels;
     using Fsel.Common.ActionResults;
+    using Fsel.Common.Enums.ErrorCodes;
     using MediatR;
+    using Microsoft.AspNetCore.Http;
 
     public class CreateNewsAndUpdateCommand : CreateNewsAndUpdateCommandModel, IRequest<MethodResult<NewsAndUpdateModel>>
     {
+    }
+    public class CreateNewsAndUpdateCommandHandler : IRequestHandler<CreateNewsAndUpdateCommand, MethodResult<NewsAndUpdateModel>>
+    {
+        private readonly IMapper _mapper;
+        private readonly INewsAndUpdateRepository _newsAndUpdateRepository;
+
+        public CreateNewsAndUpdateCommandHandler(IMapper mapper, INewsAndUpdateRepository newsAndUpdateRepository)
+        {
+            _mapper = mapper;
+            _newsAndUpdateRepository = newsAndUpdateRepository;
+        }
+
+        public async Task<MethodResult<NewsAndUpdateModel>> Handle(CreateNewsAndUpdateCommand request, CancellationToken cancellationToken)
+        {
+            ArgumentNullException.ThrowIfNull(request);
+            MethodResult<NewsAndUpdateModel> methodResult = new MethodResult<NewsAndUpdateModel>();
+
+            NewsAndUpdate newsAndUpdate = _mapper.Map<NewsAndUpdate>(request);
+
+            await _newsAndUpdateRepository.ExecuteTransactionAsync(async () =>
+            {
+                newsAndUpdate = _newsAndUpdateRepository.Add(newsAndUpdate);
+                await _newsAndUpdateRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken).ConfigureAwait(false);
+
+                methodResult.StatusCode = StatusCodes.Status201Created;
+                methodResult.Result = _mapper.Map<NewsAndUpdateModel>(newsAndUpdate);
+                return methodResult;
+            });
+
+            return methodResult;
+        }
     }
 }
--- Services/Fsel.Cms.PlanetDefender/Fsel.Cms.PlanetDefender.Application/Commands/NewsAndUpdateCmd/DeleteNewsAndUpdateCommand.cs
+++ Services/Fsel.Cms.PlanetDefender/Fsel.Cms.PlanetDefender.Application/Commands/NewsAndUpdateCmd/DeleteNewsAndUpdateCommand.cs
@@ -3,12 +3,55 @@
 namespace Fsel.Cms.PlanetDefender.Application.Commands.NewsAndUpdateCmd
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
+    using System.Threading;
     using System.Threading.Tasks;
+    using Fsel.Cms.PlanetDefender.Domain.IRepositories;
+    using Fsel.Common.ActionResults;
+    using Fsel.Common.Enums.ErrorCodes;
+    using MediatR;
+    using Microsoft.AspNetCore.Http;
 
-    public class DeleteNewsAndUpdateCommand
+    public class DeleteNewsAndUpdateCommand : IRequest<MethodResult<bool>>
     {
+        public Guid Id { get; set; }
+    }
+
+    public class DeleteNewsAndUpdateCommandHandler : IRequestHandler<DeleteNewsAndUpdateCommand, MethodResult<bool>>
+    {
+        private readonly INewsAndUpdateRepository _newsAndUpdateRepository;
+
+        public DeleteNewsAndUpdateCommandHandler(INewsAndUpdateRepository newsAndUpdateRepository)
+        {
+            _newsAndUpdateRepository = newsAndUpdateRepository;
+        }
+
+        public async Task<MethodResult<bool>> Handle(DeleteNewsAndUpdateCommand request, CancellationToken cancellationToken)
+        {
+            ArgumentNullException.ThrowIfNull(request);
+            MethodResult<bool> methodResult = new MethodResult<bool>();
+
+            #region Validation
+
+            var newsAndUpdate = await _newsAndUpdateRepository.GetByIdAsync(request.Id);
+            if (newsAndUpdate == null)
+            {
+                methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist), nameof(newsAndUpdate));
+                return methodResult;
+            }
+
+            #endregion Validation
+
+            await _newsAndUpdateRepository.ExecuteTransactionAsync(async () =>
+            {
+                var result = await _newsAndUpdateRepository.DeleteAsync(newsAndUpdate);
+                await _newsAndUpdateRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken).ConfigureAwait(false);
+
+                methodResult.StatusCode = StatusCodes.Status200OK;
+                methodResult.Result = result;
+                return methodResult;
+            });
+
+            return methodResult;
+        }
     }
 }
