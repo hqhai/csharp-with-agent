ee017c2ec2326d0b78d2d811248062b2ca37a146
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2024-01-24 17:46:31 +0700

    Update Ranking Report To Time Code

---
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/VideoTimeCodeResultQuery/GetVideoTimeCodeTestQuery.cs
@@ -0,0 +1,64 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Course.Lms.Application.Queries.VideoTimeCodeResultQuery
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using AutoMapper;
+    using Fsel.Common.ActionResults;
+    using Fsel.Common.Enums.ErrorCodes;
+    using Fsel.Course.Domain.Enums;
+    using Fsel.Course.Domain.IRepositories;
+    using Fsel.Course.Domain.Models.EntityModels;
+    using MediatR;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.EntityFrameworkCore;
+
+    public class GetVideoTimeCodeTestQuery : IRequest<MethodResult<IList<VideoTimeCodeModel>>>
+    {
+        public Guid LessonResultId { get; set; }
+        public EnumTimeCodeType Type { get; set; }
+    }
+
+    public class GetVideoTimeCodeTestQueryHandler : IRequestHandler<GetVideoTimeCodeTestQuery, MethodResult<IList<VideoTimeCodeModel>>>
+    {
+        private readonly IVideoTimeCodeRepository _videoTimeCodeRepository;
+        private readonly IVideoResultRepository _videoResultRepository;
+        private readonly IMapper _mapper;
+
+        public GetVideoTimeCodeTestQueryHandler(
+            IVideoTimeCodeRepository videoTimeCodeRepository,
+            IVideoResultRepository videoResultRepository,
+            IMapper mapper)
+        {
+            _videoTimeCodeRepository = videoTimeCodeRepository;
+            _videoResultRepository = videoResultRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<MethodResult<IList<VideoTimeCodeModel>>> Handle(GetVideoTimeCodeTestQuery request, CancellationToken cancellationToken)
+        {
+            ArgumentNullException.ThrowIfNull(request);
+            MethodResult<IList<VideoTimeCodeModel>> methodResult = new MethodResult<IList<VideoTimeCodeModel>>();
+
+            var videoResult = await _videoResultRepository.Queryable.FirstOrDefaultAsync(x => x.LessonResultId == request.LessonResultId, cancellationToken);
+            if (videoResult == null)
+            {
+                methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist), nameof(videoResult));
+                return methodResult;
+            }
+
+            var videoTimeCodes = await _videoTimeCodeRepository.Queryable.Where(x => x.VideoId == videoResult.VideoId && x.TimeCodeType == request.Type).ToListAsync(cancellationToken: cancellationToken);
+            if (videoTimeCodes == null)
+            {
+                methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist), nameof(videoTimeCodes));
+                return methodResult;
+            }
+            methodResult.Result = _mapper.Map<IList<VideoTimeCodeModel>>(videoTimeCodes);
+            methodResult.StatusCode = StatusCodes.Status200OK;
+            return methodResult;
+        }
+    }
+}
