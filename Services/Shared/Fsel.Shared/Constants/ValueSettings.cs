57e135d1c4d096281c9bf978da43f395ae7e33ee
hqhai <quanghai@atlantic.edu.vn>
2024-02-01 17:56:32 +0700

    xóa constant thừa

--- Services/Shared/Fsel.Shared/Constants/ValueSettings.cs
+++ Services/Shared/Fsel.Shared/Constants/ValueSettings.cs
@@ -13,10 +13,5 @@ namespace Fsel.Shared.Constants
 
         public const int DelayOneMinute = 1;
         public const int DelayWorkerSecond = 60;
-        public static class CaculateNumber
-        {
-            public const double DecimalPartHalf = 0.5; // phần thập phân 0.5 dùng để làm tròn số
-
-        }
     }
 }
