f096b8fe08fbb83051814058dcad32c22fb78b8a
Đô Nguyễn Thành <thanhdo@atlantic.edu.vn>
2024-02-01 15:35:15 +0700

    Update Realtime, Add queue Publish

--- Services/Fsel.Realtime/Fsel.Realtime.Application/Queues/Consumers/LeaderBoardConsumer.cs
+++ Services/Fsel.Realtime/Fsel.Realtime.Application/Queues/Consumers/LeaderBoardConsumer.cs
@@ -1,3 +1,4 @@
+using Fsel.Core.Base.Interfaces;
 using Fsel.Core.Extensions;
 using Fsel.Realtime.Application.Hubs;
 using Fsel.Shared.Constants;
@@ -10,10 +11,12 @@ namespace Fsel.Realtime.Application.Queues.Consumers
     public class LeaderBoardConsumer : IConsumer<LeaderBoardQueueModel>
     {
         private readonly IHubContext<LeaderBoardHub> _leaderBoardHubContext;
+        private readonly IQueueProvider _queueProvider;
 
-        public LeaderBoardConsumer(IHubContext<LeaderBoardHub> leaderBoardHubContext)
+        public LeaderBoardConsumer(IHubContext<LeaderBoardHub> leaderBoardHubContext, IQueueProvider queueProvider)
         {
             _leaderBoardHubContext = leaderBoardHubContext;
+            _queueProvider = queueProvider;
         }
 
         public async Task Consume(ConsumeContext<LeaderBoardQueueModel> context)
@@ -22,6 +25,12 @@ namespace Fsel.Realtime.Application.Queues.Consumers
             {
                 var courseLevel = context.Message.CourseLevel.ToString();
                 await _leaderBoardHubContext.GetGroup(courseLevel!).SendAsync(RealtimeSettings.LeaderBoardHub.Methods.LeaderBoardMessage, context.Message);
+
+                try
+                {
+                    _queueProvider.Publish(RealtimeSettings.LeaderBoardHub.Methods.LeaderBoardMessage, courseLevel, context.Message);
+                }
+                catch { }
             }
         }
     }
