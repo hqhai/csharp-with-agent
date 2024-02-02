07242903e200a82538bfeb4b68419defeef04bc2
hqhai <quanghai@atlantic.edu.vn>
2024-02-02 12:12:54 +0700

    bổ sung thêm điều kiện gọi publisher

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/MockTestCmd/V1i1/CreateMockTestAnswerBySectionGroupCommand.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/MockTestCmd/V1i1/CreateMockTestAnswerBySectionGroupCommand.cs
@@ -154,7 +154,7 @@ namespace Fsel.Course.Lms.Application.Commands.MockTestCmd.V1i1
             });
 
 
-            if (sectionGroup.CourseSkill == EnumCourseSkill.Writing && sectionGroup.Sections.FirstOrDefault() != null)
+            if (sectionGroup.CourseSkill == EnumCourseSkill.Writing && sectionGroup.Sections.FirstOrDefault() != null && request.IsSubmit)
             {
                 var sectionGroupId = sectionGroup.Sections.FirstOrDefault()!.SectionGroupId;
                 foreach (var item in request.Answers!)
@@ -168,7 +168,7 @@ namespace Fsel.Course.Lms.Application.Commands.MockTestCmd.V1i1
                         SectionId = (Guid)item.SectionId,
                         SectionGroupId = sectionGroupId,
                         MockTestResultId = mockTestResult.Id,
-                        WordContent = item.Answer?.ToString() ?? string.Empty
+                        WordContent = item.Answer?.ToString() ?? string.Empty,
                     }, cancellationToken);
                 }
             }
