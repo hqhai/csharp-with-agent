742a209140575442474c6a5fbc5b53c7745733d1
hqhai <quanghai@atlantic.edu.vn>
2024-02-02 14:37:53 +0700

    Bổ sung điều kiện check SkillScore khác null trước khi Select

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/MockTestCmd/V1i1/CreateMockTestAnswerBySectionGroupCommand.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/MockTestCmd/V1i1/CreateMockTestAnswerBySectionGroupCommand.cs
@@ -208,7 +208,7 @@ namespace Fsel.Course.Lms.Application.Commands.MockTestCmd.V1i1
                 {
                     mockTestResult.WorkingTime = sectionGroupResults.Sum(x => x.WorkingTime);
                     mockTestResult.HighestStreak = sectionGroupResults.Max(x => x.HighestStreak);
-                    mockTestResult = await GetMockTestResult(sectionGroupResults.SelectMany(x => x.SkillScores!).OrderBy(x => x.Skill).ToList(), mockTestResult, isSkillTest);
+                    mockTestResult = await GetMockTestResult(sectionGroupResults.Where(x => x.SkillScores != null).SelectMany(x => x.SkillScores!).OrderBy(x => x.Skill).ToList(), mockTestResult, isSkillTest);
                     await UpdateUserToken(mockTestResult).ConfigureAwait(false);
                 }
             }
