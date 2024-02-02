4b515134665effdf749d41d944bcd722a5f903d3
hqhai <quanghai@atlantic.edu.vn>
2024-02-02 12:12:54 +0700

    bổ sung thêm điều kiện gọi publisher

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/AiCmd/SubmitMockTestAnswerAICommand.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/AiCmd/SubmitMockTestAnswerAICommand.cs
@@ -38,6 +38,7 @@ namespace Fsel.Course.Lms.Application.Commands.AiCmd
 
         public async Task<bool> Handle(SubmitMockTestAnswerAICommand request, CancellationToken cancellationToken)
         {
+
             ArgumentNullException.ThrowIfNull(request);
             var mockTestAnswer = _mockTestAnswerRepository.Queryable.FirstOrDefault(x => x.SectionId == request.SectionId && x.MockTestResultId == request.MockTestResultId);
 
