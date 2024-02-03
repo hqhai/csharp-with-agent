7ccd953a9d19863e4289e9dde704cd1d59a9de49
honghv <vanhhong@atlantic.edu.vn>
2024-02-03 13:16:35 +0700

    hotfix

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/WeeklyReportQuery/WeeklyReportQuery.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/WeeklyReportQuery/WeeklyReportQuery.cs
@@ -75,8 +75,8 @@ namespace Fsel.Course.Lms.Application.Queries.WeeklyReportQuery
                 return methodResult;
             }
             DateTime currentDate = DateTime.UtcNow;
-            // Trừ tạm 2 ngày
-            currentDate = currentDate.AddDays(-2);
+            // Trừ tạm 1 ngày
+            currentDate = currentDate.AddDays(-1);
 
             // Lấy ngày thứ 6 gần nhất lúc 13h
             DateTime lastFridayAt13 = currentDate.AddDays(-6);
