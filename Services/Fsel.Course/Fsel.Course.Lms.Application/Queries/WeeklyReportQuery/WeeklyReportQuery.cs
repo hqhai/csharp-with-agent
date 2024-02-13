434b87f964869cad673f5bdd110efb86cb9df306
honghv <vanhhong@atlantic.edu.vn>
2024-02-13 16:30:07 +0700

    update clear temp

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/WeeklyReportQuery/WeeklyReportQuery.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/WeeklyReportQuery/WeeklyReportQuery.cs
@@ -75,7 +75,6 @@ namespace Fsel.Course.Lms.Application.Queries.WeeklyReportQuery
                 return methodResult;
             }
             DateTime currentDate = DateTime.UtcNow;
-            currentDate = currentDate.AddDays(-1);
 
             // Lấy ngày thứ 6 gần nhất lúc 13h
             DateTime lastFridayAt13 = currentDate.AddDays(-6);
