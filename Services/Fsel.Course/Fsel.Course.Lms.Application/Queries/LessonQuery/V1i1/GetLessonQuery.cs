f425753149f7ee738dd62a76c4dd420da23f9fde
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2024-02-23 17:28:31 +0700

    Update Trigger Lesson

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/LessonQuery/V1i1/GetLessonQuery.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/LessonQuery/V1i1/GetLessonQuery.cs
@@ -79,7 +79,6 @@ namespace Fsel.Course.Lms.Application.Queries.LessonQuery.V1i1
                 {
                     lessonDto.IsClassForumLock = false;
                 }
-
                 if (lessonResult.HomeWorkResults.Any(x => x.Status != EnumResultStatus.Unfinished) || (classForumResult != null && classForumResult.Status != EnumClassForumResultStatus.Draft))
                 {
                     lessonDto.IsHomeWorkLock = false;
