9d0272d7c1a77d405008ccd34a87d040063c426f
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2024-02-27 10:58:56 +0700

    Update Add Field Audio Path To SectionGroup

--- Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/SectionGroupDtoModel.cs
+++ Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/SectionGroupDtoModel.cs
@@ -9,6 +9,7 @@ namespace Fsel.Course.Domain.Models.EntityModels
     {
         public double ExecutionTime { get; set; }
         public EnumCourseSkill CourseSkill { get; set; }
+        public string? AudioPath { get; set; }
         public long TotalQuestion { get; set; }
         public IList<SectionDtoModel>? Sections { get; set; }
         public SectionGroupResultModel? SectionGroupResult { get; set; }
