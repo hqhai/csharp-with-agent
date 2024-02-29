662469b1f3d6653084865559b06e06a4d6dd14dc
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2024-02-29 14:57:00 +0700

    update table order and optimize code

--- Services/Fsel.Ordering/Fsel.Ordering.Application/Commands/OrderCmds/v1i1/UpdateOrderCommand.cs
+++ Services/Fsel.Ordering/Fsel.Ordering.Application/Commands/OrderCmds/v1i1/UpdateOrderCommand.cs
@@ -7,13 +7,7 @@ namespace Fsel.Ordering.Application.Commands.OrderCmds.v1i1
     using System.Threading.Tasks;
     using AutoMapper;
     using Fsel.Common.ActionResults;
-    using Fsel.Common.Enums;
     using Fsel.Common.Enums.ErrorCodes;
-    using Fsel.Common.Models;
-    using Fsel.Core.Base;
-    using Fsel.Core.Base.BaseModels;
-    using Fsel.Ordering.Application.Queries.OrderQuery;
-    using Fsel.Ordering.Application.Services.CourseService;
     using Fsel.Ordering.Domain.Entities;
     using Fsel.Ordering.Domain.IRepositories;
     using Fsel.Ordering.Domain.Models.CommandModels.Orders.V1i1;
@@ -22,7 +16,6 @@ namespace Fsel.Ordering.Application.Commands.OrderCmds.v1i1
     using Fsel.Shared.Helpers;
     using MediatR;
     using Microsoft.AspNetCore.Http;
-    using Microsoft.EntityFrameworkCore;
 
     public class UpdateOrderCommand : UpdateOrderCommandModel, IRequest<MethodResult<OrderModel>>
     {
@@ -32,19 +25,11 @@ namespace Fsel.Ordering.Application.Commands.OrderCmds.v1i1
     {
         private readonly IMapper _mapper;
         private readonly IOrderRepository _orderRepository;
-        private readonly IPackageRepository _packageRepository;
-        private readonly IMediator _mediator;
-        private readonly AuthContext _authContext;
-        private readonly ILmsCourseService _courseService;
 
-        public UpdateOrderCommandHandler(IMapper mapper, IOrderRepository orderRepository, IPackageRepository packageRepository, MediatR.IMediator mediator, AuthContext authContext, ILmsCourseService courseService)
+        public UpdateOrderCommandHandler(IMapper mapper, IOrderRepository orderRepository)
         {
             _mapper = mapper;
             _orderRepository = orderRepository;
-            _packageRepository = packageRepository;
-            _mediator = mediator;
-            _authContext = authContext;
-            _courseService = courseService;
         }
 
         public async Task<MethodResult<OrderModel>> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
@@ -54,60 +39,24 @@ namespace Fsel.Ordering.Application.Commands.OrderCmds.v1i1
 
             if (request.Order == null)
             {
-                methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist));
+                methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist), nameof(request.Order));
                 return methodResult;
             }
 
-            var package = await _packageRepository.GetByIdAsync(request.PackageId);
-
-            if (package == null)
+            if (request.Package == null)
             {
-                methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist), nameof(package));
+                methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist), nameof(request.Package));
                 return methodResult;
             }
 
-            var codeSend = await _mediator.Send(new GenerateRamdomOrderQuery { CourseLevel = request.CourseLevel, PackageId = package.Id }, cancellationToken).ConfigureAwait(false);
-            var code = codeSend.Result?.Code;
-
-            if (await _orderRepository.Queryable.AnyAsync(x => x.Code == code, cancellationToken))
-            {
-                methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataAlreadyExist), nameof(code));
-                return methodResult;
-            }
-
-            var courseResult = await _courseService.GetCourseByLevel(new BaseQueryModel()
-            {
-                Filters = new List<GenericFilterModel>()
-                {
-                    new GenericFilterModel()
-                    {
-                        Property = "Status",
-                        Operator = EnumFilterOperator.Equal,
-                        Value = "Active"
-                    },
-                    new GenericFilterModel()
-                    {
-                        Property = "CourseLevel",
-                        Operator = EnumFilterOperator.Equal,
-                        Value = request.CourseLevel.ToString()
-                    }
-                }
-            });
-
-            if (!courseResult.IsSuccessStatusCode)
-            {
-                methodResult.AddError(courseResult.Error);
-                return methodResult;
-            }
-            var course = courseResult.Content?.Result;
-
-            AddDataIntoOrder(request.Order, code, package.Price, course!.Id, request);
+            AddDataIntoOrder(request.Order, request.Code, request.Package.Price, request.CourseId, request);
 
             if (!request.Order.IsValid())
             {
                 methodResult.AddErrorBadRequest(request.Order.ErrorMessages);
                 return methodResult;
             }
+
             await _orderRepository.ExecuteTransactionAsync(async () =>
             {
                 request.Order = _orderRepository.Update(request.Order);
@@ -116,6 +65,7 @@ namespace Fsel.Ordering.Application.Commands.OrderCmds.v1i1
                 methodResult.Result = _mapper.Map<OrderModel>(request.Order);
                 return methodResult;
             });
+
             return methodResult;
         }
 
@@ -132,6 +82,7 @@ namespace Fsel.Ordering.Application.Commands.OrderCmds.v1i1
             order.DiscountPrice = (decimal)NumberHelper.ConvertDoublePercent(Convert.ToDouble(order.Price * order.DiscountPercent));
             order.TotalPrice = order.Price - order.DiscountPrice;
             order.CourseId = courseId;
+            order.PackageId = request.Package?.Id;
         }
     }
 }
