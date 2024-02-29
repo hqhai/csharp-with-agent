c23713bac6ede673f21555ccf2efe1635f4c8d25
tunghung <tunghung@atlantic.edu.vn>
2024-02-29 18:12:37 +0700

    update model

--- Services/Fsel.System/Fsel.System.Domain/Models/EntityModels/SchoolModel.cs
+++ Services/Fsel.System/Fsel.System.Domain/Models/EntityModels/SchoolModel.cs
@@ -12,15 +12,27 @@ namespace Fsel.System.Domain.Models.EntityModels
         public EnumSchoolType SchoolType { get; set; }
         public string? Address { get; set; }
         public string? Phone { get; set; }
+
         public string? Description { get; set; }
+
         public string? PrincipalName { get; set; }
+
         public string? PrincipalPhone { get; set; }
+
         public string? PrincipalEmail { get; set; }
+
         public string? Website { get; set; }
+
         public string? IdPath { get; set; }
+
         public string? LocationName { get; set; }
+
         public string? LongPath { get; set; }
+
         public string? ShortPath { get; set; }
         public Guid? LocationId { get; set; }
+        public LocationModel? Location { get; set; }
+
+        public bool IsActive { get; set; }
     }
 }
