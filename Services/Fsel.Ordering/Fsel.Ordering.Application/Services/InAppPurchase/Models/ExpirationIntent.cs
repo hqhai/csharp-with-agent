4e1162f80d03d005c2a216a1d801566e5f2b3d1a
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2024-03-05 09:43:55 +0700

    update models

--- Services/Fsel.Ordering/Fsel.Ordering.Application/Services/InAppPurchase/Models/ExpirationIntent.cs
+++ Services/Fsel.Ordering/Fsel.Ordering.Application/Services/InAppPurchase/Models/ExpirationIntent.cs
@@ -2,12 +2,6 @@
 
 namespace Fsel.Ordering.Application.Services.InAppPurchase.Models
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
     public enum ExpirationIntent
     {
         Canceled = 1,
