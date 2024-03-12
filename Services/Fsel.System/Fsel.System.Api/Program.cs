6f0d6306f4a91e5a2d64c9604ba457916fffef77
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2024-03-12 11:43:07 +0700

    Update Create TokenHistory Consumer

--- Services/Fsel.System/Fsel.System.Api/Program.cs
+++ Services/Fsel.System/Fsel.System.Api/Program.cs
@@ -59,8 +59,8 @@ queues: new Dictionary<string, Type>
     { QueueSettings.LmsQueue.NameQueue.QuestBoardMainFinish, typeof(QuestBoardFinishConsumer) },
     { QueueSettings.SystemQueue.NameQueue.QuestBoard, typeof(QuestBoardConsumer) },
     { QueueSettings.SystemQueue.NameQueue.CompleteApprovalPostTimeOut, typeof(CompleteApprovalConsumer) },
-    { QueueSettings.LmsQueue.NameQueue.CreateTokenHistory, typeof(TokenHistoryConsumer) },
-    { QueueSettings.UserQueue.NameQueue.CreateTokenHistory, typeof(TokenHistoryConsumer) }
+    { QueueSettings.LmsQueue.NameQueue.CreateTokenHistory, typeof(CreateTokenHistoryConsumer) },
+    { QueueSettings.UserQueue.NameQueue.CreateTokenHistory, typeof(CreateTokenHistoryConsumer) }
 });
 
 var app = builder.Build();
---
+++ Services/Fsel.System/Fsel.System.Application/Queues/Consumers/CreateTokenHistoryConsumer.cs
@@ -0,0 +1,39 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.System.Application.Queues.Consumers
+{
+    using Fsel.Shared.Models.ShareModels;
+    using global::System.Threading.Tasks;
+    using MassTransit;
+    using MediatR;
+
+    public class CreateTokenHistoryConsumer : IConsumer<TokenHistoryQueueModel>
+    {
+        private readonly IMediator _mediator;
+
+        public CreateTokenHistoryConsumer(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task Consume(ConsumeContext<TokenHistoryQueueModel> context)
+        {
+            var message = context?.Message;
+            if (message == null)
+            {
+                return;
+            }
+            await _mediator.Send(new TokenHistoryQueueModel
+            {
+                ObjectId = message.ObjectId,
+                InitialToken = message.InitialToken,
+                RemainToken = message.RemainToken,
+                VolatileToken = message.VolatileToken,
+                Mission = message.Mission,
+                Feature = message.Feature,
+                Type = message.Type,
+                UserId = message.UserId
+            }).ConfigureAwait(false);
+        }
+    }
+}
