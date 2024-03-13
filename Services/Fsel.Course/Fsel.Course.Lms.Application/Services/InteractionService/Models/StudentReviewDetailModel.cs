0db147c8927cd6fb5474e21fde780e873659e7ed
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2024-03-13 14:52:48 +0700

    Update Name StudentReviewDetail

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Services/InteractionService/Models/StudentReviewDetailModel.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Services/InteractionService/Models/StudentReviewDetailModel.cs
@@ -7,7 +7,7 @@ namespace Fsel.Course.Lms.Application.Services.InteractionService.Models
     public class StudentReviewDetailModel
     {
         public Guid Id { get; set; }
-        public EnumReviewQuestionType ReviewType { get; set; }
+        public EnumReviewQuestionType ReviewQuestionType { get; set; }
         public int VoteStars { get; set; }
         public string? Content { get; set; }
     }
