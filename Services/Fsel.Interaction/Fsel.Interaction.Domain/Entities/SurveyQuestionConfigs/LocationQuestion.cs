f7391335e148c06f80bcd78f5bbdccbdffdc36d2
tunghung <tunghung@atlantic.edu.vn>
2024-03-14 17:00:22 +0700

    update Survey location question and mirgation

--- Services/Fsel.Interaction/Fsel.Interaction.Domain/Entities/SurveyQuestionConfigs/LocationQuestion.cs
+++ Services/Fsel.Interaction/Fsel.Interaction.Domain/Entities/SurveyQuestionConfigs/LocationQuestion.cs
@@ -9,8 +9,12 @@ namespace Fsel.Interaction.Domain.Entities.SurveyQuestionConfigs
     public class LocationAnswer
     {
         public Guid? CountryId { get; set; }
+        public string? CountryName { get; set; }
         public Guid? CityId { get; set; }
+        public string? CityName { get; set; }
         public Guid? DistrictId { get; set; }
+        public string? DistrictName { get; set; }
         public Guid? SchoolId { get; set; }
+        public string? SchoolName { get; set; }
     }
 }
