64d0218ac62102e84bfc6608c1bf0336a0177b28
Đô Nguyễn Thành <thanhdo@atlantic.edu.vn>
2024-03-15 14:32:21 +0700

    Edit BaseInternalEventHandler

--- Services/Fsel.Course/Fsel.Course.Lms.Application/InternalEvents/BaseInternalEventHandler.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/InternalEvents/BaseInternalEventHandler.cs
@@ -45,7 +45,6 @@ namespace Fsel.Course.Lms.Application.InternalEvents
         protected readonly ISystemService _systemService;
         protected readonly IOrderService _orderService;
         private readonly QuestBoardPublisher _questBoardPublisher;
-        private readonly IOrderService _orderService;
         private const int TotalScoreClassForum = 36;
         private const int PercentClassForumAcademic = 20;
         private const int PercentClassForumIELST = 32;
