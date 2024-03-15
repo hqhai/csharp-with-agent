329a540b1899dbc85229ed6041e296c8eee0d03d
tunghung <tunghung@atlantic.edu.vn>
2024-03-15 09:22:16 +0700

    update location question model

--- Services/Fsel.Interaction/Fsel.Interaction.Domain/Entities/SurveyQuestionConfigs/LocationQuestion.cs
+++ Services/Fsel.Interaction/Fsel.Interaction.Domain/Entities/SurveyQuestionConfigs/LocationQuestion.cs
@@ -10,8 +10,8 @@ namespace Fsel.Interaction.Domain.Entities.SurveyQuestionConfigs
     {
         public Guid? CountryId { get; set; }
         public string? CountryName { get; set; }
-        public Guid? CityId { get; set; }
-        public string? CityName { get; set; }
+        public Guid? ProvinceId { get; set; }
+        public string? ProvinceName { get; set; }
         public Guid? DistrictId { get; set; }
         public string? DistrictName { get; set; }
         public Guid? SchoolId { get; set; }
