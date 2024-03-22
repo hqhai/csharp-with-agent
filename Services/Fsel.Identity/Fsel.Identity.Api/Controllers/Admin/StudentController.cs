6fca4e966a9a41fcaa5792976b2ada2bea12acb0
hqhai <quanghai@atlantic.edu.vn>
2024-03-22 11:08:53 +0700

    update: chuyển api lấy token của user sang usercontroller

--- Services/Fsel.Identity/Fsel.Identity.Api/Controllers/Admin/StudentController.cs
+++ Services/Fsel.Identity/Fsel.Identity.Api/Controllers/Admin/StudentController.cs
@@ -119,13 +119,6 @@ namespace Fsel.Identity.Api.Controllers.Admin
             return queryResult.GetActionResult();
         }
 
-        [HttpGet("token/{id}")]
-        [ProducesResponseType(typeof(MethodResult<TokenModel>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
-        public async Task<IActionResult> GetJWT([FromRoute] Guid id)
-        {
-            MethodResult<TokenModel> queryResult = await _mediator.Send(new GenerateTokenCommand { Id = id }).ConfigureAwait(false);
-            return queryResult.GetActionResult();
-        }
+        
     }
 }
