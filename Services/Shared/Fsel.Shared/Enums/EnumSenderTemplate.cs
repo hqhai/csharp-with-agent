f45942b1c706ec24f18c85c329ba7cddf4784e04
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2024-04-03 14:47:23 +0700

    send mail mid course

--- Services/Shared/Fsel.Shared/Enums/EnumSenderTemplate.cs
+++ Services/Shared/Fsel.Shared/Enums/EnumSenderTemplate.cs
@@ -18,6 +18,8 @@ namespace Fsel.Shared.Enums
         WeeklyReport2,
         WeeklyReport3,
         WeeklyReport4,
-        StudentCompletePT
+        StudentCompletePT,
+        SendMailMidCourseAcademic,
+        SendMailMidCourseIELT
     }
 }
---
+++ Services/Shared/Fsel.Shared/Models/SenderTemplates/SendStudentCompleteMidCourseModel.cs
@@ -0,0 +1,30 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Shared.Models.SenderTemplates
+{
+    using Fsel.Shared.Enums;
+
+    public class SendStudentCompleteMidCourseModel
+    {
+        public string? FullName { get; set; }
+        public string? CourseLevel { get; set; }
+        public string? StartDate { get; set; }
+        public string? EndDate { get; set; }
+        public string? Percent { get; set; }
+        public string? Unit1Name { get; set; }
+        public string? UnitNowNumber { get; set; }
+        public string? UnitNowName { get; set; }
+        public string? TotalLesson { get; set; }
+        public string? TotalDay { get; set; }
+        public string? TotalNote { get; set; }
+        public string? Video { get; set; }
+        public string? Homework { get; set; }
+        public string? ClassForum { get; set; }
+        public string? SkillTest { get; set; }
+        public string? UnitTest { get; set; }
+        public string? BandScore { get; set; }
+        public string? Level { get; set; }
+        public string? Description { get; set; }
+        public EnumCourseType CourseType { get; set; }
+    }
+}
