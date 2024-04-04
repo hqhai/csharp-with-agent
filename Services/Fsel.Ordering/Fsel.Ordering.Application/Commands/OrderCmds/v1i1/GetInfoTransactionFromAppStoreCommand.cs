72bf9c1d2bcfcfea950f64daca5f35fed816ce12
Đô Nguyễn Thành <thanhdo@atlantic.edu.vn>
2024-04-04 13:39:05 +0700

    Add log in GetInfoTransactionFromAppStoreCommand

--- Services/Fsel.Ordering/Fsel.Ordering.Application/Commands/OrderCmds/v1i1/GetInfoTransactionFromAppStoreCommand.cs
+++ Services/Fsel.Ordering/Fsel.Ordering.Application/Commands/OrderCmds/v1i1/GetInfoTransactionFromAppStoreCommand.cs
@@ -61,7 +61,7 @@ namespace Fsel.Ordering.Application.Commands.OrderCmds.v1i1
             ArgumentNullException.ThrowIfNull(request);
             var methodResult = new MethodResult<bool>();
 
-            _logger.LogError(_appSetting.Services.AppStoreApiUrl);
+            _logger.LogError(_appSetting.Services.AppStoreApiUrl ?? "AppStoreApiUrl");
 
             if (string.IsNullOrEmpty(request.TransactionId))
             {
@@ -96,7 +96,7 @@ namespace Fsel.Ordering.Application.Commands.OrderCmds.v1i1
                 token = jwtBuilder.Encode();
             }
 
-            var purchaseSetting = _appSetting.PurchaseSettings.Serialize();
+            var purchaseSetting = _appSetting.PurchaseSettings.Serialize() ?? "PurchaseSettings";
             _logger.LogError(purchaseSetting);
 
             var signedTransactionInfoResult = await _appStoreService.GetInfoTransaction(token, request.TransactionId);
