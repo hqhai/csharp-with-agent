334deecc6acfda544d7e188cc5b6ff19a47f0905
hqhai <quanghai@atlantic.edu.vn>
2024-04-04 09:27:27 +0700

    update: bỏ đi model bị thừa, không dùng đến

--- Services/Fsel.Course/Fsel.Course.Infrastructure/Maps/MockTestAISettingProfile.cs
+++ Services/Fsel.Course/Fsel.Course.Infrastructure/Maps/MockTestAISettingProfile.cs
@@ -11,8 +11,8 @@ namespace Fsel.Course.Infrastructure.Maps
     {
         public MockTestAISettingProfile()
         {
-            CreateMap<SectionAiSettingModel, MockTestAISetting>().IgnoreAllNonExisting();
-            CreateMap<MockTestAISettingModel, SectionAiSettingModel>().IgnoreAllNonExisting();
+            //CreateMap<SectionAiSettingModel, MockTestAISetting>().IgnoreAllNonExisting();
+            //CreateMap<MockTestAISettingModel, SectionAiSettingModel>().IgnoreAllNonExisting();
             CreateMap<MockTestAISetting, MockTestAISettingModel>().IgnoreAllNonExisting();
             CreateMap<MockTestAICriteriaSetting, MockTestAICriteriaSettingModel>().IgnoreAllNonExisting();
             CreateMap<MockTestAICriteriaSettingModel, MockTestAICriteriaSetting>().IgnoreAllNonExisting();
