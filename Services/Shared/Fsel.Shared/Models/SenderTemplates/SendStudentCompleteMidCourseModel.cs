6e18b2deda3a3aba219ee2a9930652b8d953873f
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2024-04-05 15:23:52 +0700

    update template mail mid course

--- Services/Shared/Fsel.Shared/Models/SenderTemplates/SendStudentCompleteMidCourseModel.cs
+++ Services/Shared/Fsel.Shared/Models/SenderTemplates/SendStudentCompleteMidCourseModel.cs
@@ -25,6 +25,7 @@ namespace Fsel.Shared.Models.SenderTemplates
         public string? BandScore { get; set; }
         public string? Level { get; set; }
         public string? Description { get; set; }
+        public string? ColorCircle { get; set; }
         public EnumCourseType CourseType { get; set; }
     }
 }
