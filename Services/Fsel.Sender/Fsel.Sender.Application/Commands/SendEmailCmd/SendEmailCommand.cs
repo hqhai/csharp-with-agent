35c7c0bb41453138ce72f17e4740084251967ca1
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2024-04-09 15:19:52 +0700

    Update SendMail To SDK AWS

--- Services/Fsel.Sender/Fsel.Sender.Application/Commands/SendEmailCmd/SendEmailCommand.cs
+++ Services/Fsel.Sender/Fsel.Sender.Application/Commands/SendEmailCmd/SendEmailCommand.cs
@@ -1,14 +1,16 @@
 // Copyright (c) Atlantic. All rights reserved.
 
+using System.Drawing;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
 using Fsel.Common.ActionResults;
 using Fsel.Common.Helpers;
+using Fsel.Sender.Application.Services;
 using Fsel.Sender.Domain.Models.Commands;
 using Fsel.Sender.Domain.Models.Entities;
 using Fsel.Sender.Domain.ValueSettings;
-using Fsel.Shared.Constants;
 using MediatR;
 using Microsoft.AspNetCore.Http;
-using MimeKit;
 
 namespace Fsel.Sender.Application.Commands.SendEmailCmd
 {
@@ -19,10 +21,12 @@ namespace Fsel.Sender.Application.Commands.SendEmailCmd
     public class SendEmailCommandHandler : IRequestHandler<SendEmailCommand, MethodResult<bool>>
     {
         private readonly AppSetting _appSetting;
+        private readonly SESWrapper _wrapper;
 
         public SendEmailCommandHandler(AppSetting appSetting)
         {
             _appSetting = appSetting;
+            _wrapper = new SESWrapper(new AmazonSimpleEmailServiceClient(_appSetting?.Smtp?.Username, _appSetting?.Smtp?.Password, region: Amazon.RegionEndpoint.APSoutheast1));
         }
 
         public async Task<MethodResult<bool>> Handle(SendEmailCommand request, CancellationToken cancellationToken)
@@ -40,10 +44,8 @@ namespace Fsel.Sender.Application.Commands.SendEmailCmd
             sendEmail.Content = request.Content;
             try
             {
-                using (var emailMessage = CreateEmailMessage(sendEmail))
-                {
-                    await Send(emailMessage);
-                }
+                var emailMessage = CreateEmailMessage(sendEmail);
+                await SendEmail(emailMessage);
             }
             catch (Exception)
             {
@@ -57,16 +59,27 @@ namespace Fsel.Sender.Application.Commands.SendEmailCmd
             return methodResult;
         }
 
-        private MimeMessage CreateEmailMessage(SendEmailModel message)
+        private SendEmailRequest CreateEmailMessage(SendEmailModel message)
         {
-            var emailMessage = new MimeMessage();
-            emailMessage.From.Add(new MailboxAddress(SenderSettings.HostName, _appSetting?.Smtp?.From ?? string.Empty));
+            var emailRequest = new SendEmailRequest
+            {
+                Source = _appSetting?.Smtp?.From,
+                Destination = new Destination(),
+                Message = new Message
+                {
+                    Body = new Body
+                    {
+                        Text = new Content(message.Content)
+                    },
+                    Subject = new Content(message.Subject)
+                }
+            };
 
             if (message.ToEmails != null)
             {
                 foreach (var item in message.ToEmails.Where(x => x.IsValidEmail()))
                 {
-                    emailMessage.To.Add(new MailboxAddress(item, item));
+                    emailRequest.Destination.ToAddresses.Add(item);
                 }
             }
 
@@ -74,7 +87,7 @@ namespace Fsel.Sender.Application.Commands.SendEmailCmd
             {
                 foreach (var item in message.BccEmails.Where(x => x.IsValidEmail()))
                 {
-                    emailMessage.Bcc.Add(new MailboxAddress(item, item));
+                    emailRequest.Destination.BccAddresses.Add(item);
                 }
             }
 
@@ -82,38 +95,61 @@ namespace Fsel.Sender.Application.Commands.SendEmailCmd
             {
                 foreach (var item in message.CcEmails.Where(x => x.IsValidEmail()))
                 {
-                    emailMessage.Cc.Add(new MailboxAddress(item, item));
+                    emailRequest.Destination.CcAddresses.Add(item);
                 }
             }
-
-            emailMessage.Subject = message.Subject;
-            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = message.Content };
-
-            return emailMessage;
+            return emailRequest;
         }
 
-        private async Task Send(MimeMessage mailmessage)
+        //private async Task Send(MimeMessage mailmessage)
+        //{
+        //    using (var client = new MailKit.Net.Smtp.SmtpClient())
+        //    {
+        //        try
+        //        {
+        //            client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+        //            await client.ConnectAsync(_appSetting?.Smtp?.SmtpServer ?? string.Empty, _appSetting?.Smtp?.Port ?? 0, true);
+        //            client.AuthenticationMechanisms.Remove("XOAUTH2");
+        //            await client.AuthenticateAsync(_appSetting?.Smtp?.Username ?? string.Empty, _appSetting?.Smtp?.Password ?? string.Empty);
+        //            await client.SendAsync(mailmessage);
+        //        }
+        //        catch (Exception)
+        //        {
+        //            throw;
+        //        }
+        //        finally
+        //        {
+        //            await client.DisconnectAsync(true);
+        //            client.Dispose();
+        //        }
+        //    }
+        //}
+        //public void SendEmailAsync(SendEmailRequest request)
+        //{
+        //    //AWSCredentials aWSCredentials = new BasicAWSCredentials(_appSetting?.Smtp?.Username, _appSetting?.Smtp?.Password);
+        //    using (var client = AWSClientFactory.CreateAmazonSimpleEmailServiceClient(_appSetting?.Smtp?.Username, _appSetting?.Smtp?.Password))
+        //    {
+        //        try
+        //        {
+        //            var a = client.SendEmail(request);
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //        }
+        //    }
+        //}
+
+        public async Task SendEmail(SendEmailRequest emailRequest)
         {
-            using (var client = new MailKit.Net.Smtp.SmtpClient())
-            {
-                try
-                {
-                    client.ServerCertificateValidationCallback = (s, c, h, e) => true;
-                    await client.ConnectAsync(_appSetting?.Smtp?.SmtpServer ?? string.Empty, _appSetting?.Smtp?.Port ?? 0, true);
-                    client.AuthenticationMechanisms.Remove("XOAUTH2");
-                    await client.AuthenticateAsync(_appSetting?.Smtp?.Username ?? string.Empty, _appSetting?.Smtp?.Password ?? string.Empty);
-                    await client.SendAsync(mailmessage);
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
-                finally
-                {
-                    await client.DisconnectAsync(true);
-                    client.Dispose();
-                }
-            }
+            await _wrapper.SendEmailAsync(
+                 emailRequest.Destination.ToAddresses,
+                 emailRequest.Destination.CcAddresses,
+                 emailRequest.Destination.BccAddresses,
+                 emailRequest.Message.Body.Text.Data,
+                 string.Empty,
+                 emailRequest.Message.Subject.Data,
+                  _appSetting?.Smtp?.From
+                 );
         }
     }
 }
