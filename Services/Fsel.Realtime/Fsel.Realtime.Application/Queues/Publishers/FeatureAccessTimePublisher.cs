9c9d686d71801493d2ba604b8b5e29e6be82e334
hqhai <quanghai@atlantic.edu.vn>
2024-04-15 17:24:40 +0700

    feat: chuyển phần featureaccesstime sang websocket

---
+++ Services/Fsel.Realtime/Fsel.Realtime.Application/Queues/Publishers/FeatureAccessTimePublisher.cs
@@ -0,0 +1,29 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Realtime.Application.Queues.Publishers
+
+{
+    using Fsel.Core.Base.Interfaces;
+    using Fsel.Shared.Constants;
+    using Fsel.Shared.Models.ShareModels;
+
+    public class FeatureAccessTimePublisher
+    {
+        private readonly IQueueProvider _queueProvider;
+
+
+        public FeatureAccessTimePublisher(IQueueProvider queueProvider)
+        {
+            _queueProvider = queueProvider;
+        }
+
+        public async Task Publish(TrackingTimeModel? request, CancellationToken cancellationToken)
+        {
+            if (request == null)
+            {
+                return;
+            }
+            await _queueProvider.Publish(QueueSettings.RealtimeQueue.NameQueue.FeatureAccessTime, request, cancellationToken);
+        }
+    }
+}
