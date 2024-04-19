1c7fa2ca168596a1231448f57010d477eb9aaf20
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2024-04-19 17:46:11 +0700

    fix create user field dob wrong

--- Services/Fsel.Identity/Fsel.Identity.Application/Commands/StudentCmd/ImportStudentsIntoPlatformCommand.cs
+++ Services/Fsel.Identity/Fsel.Identity.Application/Commands/StudentCmd/ImportStudentsIntoPlatformCommand.cs
@@ -2,6 +2,7 @@
 
 namespace Fsel.Identity.Application.Commands.StudentCmd
 {
+    using System.Globalization;
     using System.Threading;
     using System.Threading.Tasks;
     using Fsel.Common.ActionResults;
@@ -122,7 +123,7 @@ namespace Fsel.Identity.Application.Commands.StudentCmd
                         Human = new Human()
                         {
                             FullName = student.FullName,
-                            Birthday = student.DateOfBirth.ConvertDateTimeFormat(),
+                            Birthday = Convert.ToDateTime(student.DateOfBirth, CultureInfo.CurrentCulture),
                             Email = student.Email,
                             Student = new Student()
                             {
