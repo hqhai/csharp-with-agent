5f2777950c99cc882386a8adcbeef39c52f6fc41
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2024-04-24 15:58:00 +0700

    Add Contact Info To Google Sheet File

--- Services/Fsel.System/Fsel.System.Infrastructure/ValueSettings/AppSetting.cs
+++ Services/Fsel.System/Fsel.System.Infrastructure/ValueSettings/AppSetting.cs
@@ -23,6 +23,8 @@ namespace Fsel.System.Infrastructure.ValueSettings
         public string? I18NSpreadSheetId { get; set; }
         public string? I18NSheetVN { get; set; }
         public string? I18NSheetEN { get; set; }
+        public string? LandingPageSpreadSheetId { get; set; }
+        public string? LandingPageSheet { get; set; }
     }
 
     public class Services : BaseServices
