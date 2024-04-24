75cd41bfb0b70754034ee39c9d521e84613d108b
hqhai <quanghai@atlantic.edu.vn>
2024-04-24 16:58:01 +0700

    feat: Bổ sung api Test Notification trên websocket

--- Services/Fsel.Realtime/Fsel.Realtime.Application/Queues/Consumers/NotificationConsumer.cs
+++ Services/Fsel.Realtime/Fsel.Realtime.Application/Queues/Consumers/NotificationConsumer.cs
@@ -1,5 +1,7 @@
+using Fsel.Core.Base;
 using Fsel.Core.Base.BaseModels;
 using Fsel.Core.Base.Interfaces;
+using Fsel.Core.Base.Managers;
 using Fsel.Core.Extensions;
 using Fsel.Realtime.Application.Hubs;
 using Fsel.Shared.Constants;
@@ -9,12 +11,12 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Fsel.Realtime.Application.Queues.Consumers
 {
-    public class NotificationConsumer : Core.Base.Interfaces.IBaseConsumer<NotificationQueueModel>
+    public class NotificationConsumer : BaseConsumer<NotificationQueueModel>, Core.Base.Interfaces.IBaseConsumer<NotificationQueueModel>
     {
         private readonly IHubContext<NotificationHub> _notificationHubContext;
         private readonly IQueueProvider _queueProvider;
 
-        public NotificationConsumer(IHubContext<NotificationHub> notificationHubContext, IQueueProvider queueProvider)
+        public NotificationConsumer(IHubContext<NotificationHub> notificationHubContext, IQueueProvider queueProvider, AuthContext authContext) : base(authContext) 
         {
             _notificationHubContext = notificationHubContext;
             _queueProvider = queueProvider;
@@ -25,16 +27,7 @@ namespace Fsel.Realtime.Application.Queues.Consumers
             if (context != null && context!.Message!.Data.UserIds != null)
             {
                 var userIds = context.Message.Data.UserIds;
-                await _notificationHubContext.GetGroups(userIds.Select(x => x.ToString()).ToList()).SendAsync(RealtimeSettings.NotificationHub.Methods.NotificationMessage, context.Message);
-
-                userIds.Select(x => x.ToString()).ForEach(x =>
-                {
-                    try
-                    {
-                        _queueProvider.Publish(RealtimeSettings.NotificationHub.Methods.NotificationMessage, x, context.Message);
-                    }
-                    catch { }
-                });
+                await _notificationHubContext.GetGroups(userIds.Select(x => x.ToString()).ToList()).SendAsync(RealtimeSettings.NotificationHub.Methods.NotificationMessage, context.Message.Data);
             }
         }
     }
