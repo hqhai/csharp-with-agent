731310d7f2fbd06b65aa1ad9f3ccb07ecbc7e41e
Đô Nguyễn Thành <thanhdo@atlantic.edu.vn>
2024-05-04 21:59:34 +0700

    Update Logout function

--- Services/Fsel.Identity/Fsel.Identity.Authentication/Quickstart/Account/AccountController.cs
+++ Services/Fsel.Identity/Fsel.Identity.Authentication/Quickstart/Account/AccountController.cs
@@ -666,16 +666,16 @@ namespace Fsel.Identity.Authentication.Quickstart.Account
         {
             // build a model so the logout page knows what to display
             var vm = await BuildLogoutViewModelAsync(logoutId);
-            return await Logout(vm);
+            //return await Logout(vm);
 
-            //if (vm.ShowLogoutPrompt == false)
-            //{
-            //    // if the request for logout was properly authenticated from IdentityServer, then
-            //    // we don't need to show the prompt and can just log the user out directly.
-            //    return await Logout(vm);
-            //}
+            if (vm.ShowLogoutPrompt == false)
+            {
+                // if the request for logout was properly authenticated from IdentityServer, then
+                // we don't need to show the prompt and can just log the user out directly.
+                return await Logout(vm);
+            }
 
-            //return View(vm);
+            return View(vm);
         }
 
         /// <summary>
