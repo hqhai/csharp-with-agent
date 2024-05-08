71b360794c38408ab583c757b73944cd11147f13
Đô Nguyễn Thành <thanhdo@atlantic.edu.vn>
2024-05-08 08:52:51 +0700

    Update Login in AccountController

--- Services/Fsel.Identity/Fsel.Identity.Authentication/Quickstart/Account/AccountController.cs
+++ Services/Fsel.Identity/Fsel.Identity.Authentication/Quickstart/Account/AccountController.cs
@@ -539,7 +539,7 @@ namespace Fsel.Identity.Authentication.Quickstart.Account
         /// </summary>
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login(LoginInputModel model, string button)
+        public async Task<IActionResult> Login(LoginInputModel model/*, string button*/)
         {
             ArgumentNullException.ThrowIfNull(model);
 
@@ -549,32 +549,32 @@ namespace Fsel.Identity.Authentication.Quickstart.Account
             // check if we are in the context of an authorization request
             var context = await _interaction.GetAuthorizationContextAsync(model.ReturnUrl);
 
-            // the user clicked the "cancel" button
-            if (button != "login")
-            {
-                if (context != null)
-                {
-                    // if the user cancels, send a result back into IdentityServer as if they 
-                    // denied the consent (even if this client does not require consent).
-                    // this will send back an access denied OIDC error response to the client.
-                    await _interaction.DenyAuthorizationAsync(context, AuthorizationError.AccessDenied);
+            //// the user clicked the "cancel" button
+            //if (button != "login")
+            //{
+            //    if (context != null)
+            //    {
+            //        // if the user cancels, send a result back into IdentityServer as if they 
+            //        // denied the consent (even if this client does not require consent).
+            //        // this will send back an access denied OIDC error response to the client.
+            //        await _interaction.DenyAuthorizationAsync(context, AuthorizationError.AccessDenied);
 
-                    // we can trust model.ReturnUrl since GetAuthorizationContextAsync returned non-null
-                    if (context.IsNativeClient())
-                    {
-                        // The client is native, so this change in how to
-                        // return the response is for better UX for the end user.
-                        return this.LoadingPage("Redirect", model.ReturnUrl ?? string.Empty);
-                    }
+            //        // we can trust model.ReturnUrl since GetAuthorizationContextAsync returned non-null
+            //        if (context.IsNativeClient())
+            //        {
+            //            // The client is native, so this change in how to
+            //            // return the response is for better UX for the end user.
+            //            return this.LoadingPage("Redirect", model.ReturnUrl ?? string.Empty);
+            //        }
 
-                    return Redirect(model.ReturnUrl ?? string.Empty);
-                }
-                else
-                {
-                    // since we don't have a valid context, then we just go back to the home page
-                    return Redirect("~/");
-                }
-            }
+            //        return Redirect(model.ReturnUrl ?? string.Empty);
+            //    }
+            //    else
+            //    {
+            //        // since we don't have a valid context, then we just go back to the home page
+            //        return Redirect("~/");
+            //    }
+            //}
 
             _logger.LogWarning("ModelState.IsValid: " + ModelState.IsValid);
 
