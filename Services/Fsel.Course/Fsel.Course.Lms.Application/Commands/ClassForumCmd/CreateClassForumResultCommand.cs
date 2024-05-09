b97b02c9ac9c5c64e8e52d0f8c2a3dc000d1e6ad
hqhai <quanghai@atlantic.edu.vn>
2024-05-09 16:14:40 +0700

    fixbug: truyền thẳng giá trị lần gửi để tránh lỗi , websocket không lấy được dữ liệu

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/ClassForumCmd/CreateClassForumResultCommand.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/ClassForumCmd/CreateClassForumResultCommand.cs
@@ -241,11 +241,13 @@ namespace Fsel.Course.Lms.Application.Commands.ClassForumCmd
 
                     bool hasFirstTime = await _classForumDetailResultRepository.Queryable.AnyAsync(x => x.ClassForumResultId == classForumResult!.Id, cancellationToken);
 
-                    int displayOrder = !hasFirstTime ? Display_Order_First : Display_Order_Second;
-
-                    if (request.IsSubmit)
+                    if (request.IsSubmit && hasFirstTime)
                     {
-                        await PublishAIClassForumResponseAsync(classForumDetailResult.Id, classForumResult.Id, classForum, request, displayOrder, cancellationToken);
+                        await PublishAIClassForumResponseAsync(classForumDetailResult.Id, classForumResult.Id, classForum, request, Display_Order_Second, cancellationToken);
+                    }
+                    else if (request.IsSubmit && !hasFirstTime)
+                    {
+                        await PublishAIClassForumResponseAsync(classForumDetailResult.Id, classForumResult.Id, classForum, request, Display_Order_First, cancellationToken);
                     }
 
                     if (classForumDetailResult.Status != EnumClassForumResultStatus.Draft && classForumResult.SubmissionCount == EnumSubmissionCount.FirstSubmit)
