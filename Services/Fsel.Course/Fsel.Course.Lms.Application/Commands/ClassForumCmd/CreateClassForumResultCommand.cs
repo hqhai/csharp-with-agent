151050a2427e38247d9e8ee7eaddbe26bc21c922
hqhai <quanghai@atlantic.edu.vn>
2024-05-09 16:03:53 +0700

    Fixbug: Sửa điều kiện push message class forum lần 2

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/ClassForumCmd/CreateClassForumResultCommand.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/ClassForumCmd/CreateClassForumResultCommand.cs
@@ -239,9 +239,9 @@ namespace Fsel.Course.Lms.Application.Commands.ClassForumCmd
                     classForumDetailResult = _classForumDetailResultRepository.Update(classForumDetailResult);
                     await _classForumDetailResultRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken).ConfigureAwait(false);
 
-                    bool isFirstTime = await _classForumDetailResultRepository.Queryable.AnyAsync(x => x.ClassForumResultId == classForumResult!.Id, cancellationToken);
+                    bool isExistsFirstTime = await _classForumDetailResultRepository.Queryable.AnyAsync(x => x.ClassForumResultId == classForumResult!.Id, cancellationToken);
 
-                    int displayOrder = isFirstTime ? Display_Order_First : Display_Order_Second;
+                    int displayOrder = !isExistsFirstTime ? Display_Order_First : Display_Order_Second;
 
                     if (request.IsSubmit)
                     {
