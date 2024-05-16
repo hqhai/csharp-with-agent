18b697391babec10bb78269aeefbe6d9d69656aa
tunghung <tunghung@atlantic.edu.vn>
2024-05-16 18:06:53 +0700

    update survey tranlation

--- Services/Fsel.Interaction/Fsel.Interaction.Domain/Models/EntityModels/SurveyQuestionModel.cs
+++ Services/Fsel.Interaction/Fsel.Interaction.Domain/Models/EntityModels/SurveyQuestionModel.cs
@@ -3,10 +3,26 @@
 namespace Fsel.Interaction.Domain.Models.EntityModels
 {
     using Fsel.Core.Base.BaseModels;
+    using Fsel.Core.Base.Interfaces;
     using Fsel.Shared.Enums;
 
     public class SurveyQuestionModel : BaseModel
     {
+        public string? Question { get; set; }
+        public string? Description { get; set; }
+        public string? Icon { get; set; }
+        public float DisplayOrder { get; set; }
+        public int DisplayLevel { get; set; }
+        public EnumSurveyQuestion Type { get; set; }
+        public object? Answers { get; set; }
+        public IList<CustomerSurveyModel>? CustomerSurveys { get; set; }
+        public IList<SurveyQuestionTranslationModel>? Translations { get; set; }
+    }
+
+    public class SurveyQuestionTranslationModel : BaseModel, ITranslationObject
+    {
+        public string? Language { get; set; }
+
         public string? Question { get; set; }
         public string? Description { get; set; }
         public string? Icon { get; set; }
