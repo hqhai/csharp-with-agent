bdbc117116b5566b41fdfd56eecc023967aa3a0c
Chiến Cò <lxmchien@gmail.com>
2024-05-22 16:52:13 +0700

    update api integration

--- Services/Fsel.Identity/Fsel.Identity.Domain/Models/EntityModels/IntegrationModel/LeadsIntegrationModel.cs
+++ Services/Fsel.Identity/Fsel.Identity.Domain/Models/EntityModels/IntegrationModel/LeadsIntegrationModel.cs
@@ -2,30 +2,16 @@
 
 namespace Fsel.Identity.Domain.Models.EntityModels.IntegrationModel
 {
-    using Fsel.Identity.Domain.Enums;
     using Fsel.Shared.Enums;
 
-    public class LeadsIntegrationModel
+    public class LeadsIntegrationModel : IntegrationModel
     {
-        public Guid UserId { get; set; }
-        public string? FullName { get; set; }
-        public string? UserName { get; set; }
-        public string? StudentEmail { get; set; }
-        public string? StudentPhone { get; set; }
-        public EnumGender? EnumGender { get; set; }
-        public DateTime? Birthday { get; set; }
-        public string? Address { get; set; }
-        public string? ParentName { get; set; }
-        public string? ParentPhone { get; set; }
-        public string? ParentEmail { get; set; }
-        public EnumGender? ParentGender { get; set; }
         public EnumIntegrationStatus? Status { get; set; }
+
         public string? CourseLevel { get; set; }
-        public string? PTLevel { get; set; }
+
         public DateTime? ExpireDate { get; set; }
+
         public string? StatusPT { get; set; }
-        public Guid? SchoolId { get; set; }
-        public Guid? LocationId { get; set; }
-        public DateTime? LastDate { get; set; }
     }
 }
