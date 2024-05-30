2b698dfa3aecc95cc893c3e78ebfed7973ee872e
hqhai <quanghai@atlantic.edu.vn>
2024-05-30 10:14:25 +0700

    feature: speaking ai mixed teacher and AI

--- Services/Fsel.Course/Fsel.Course.Lms.Api/Program.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Api/Program.cs
@@ -11,6 +11,8 @@ using Fsel.Course.Lms.Application.InternalEvents;
 using Fsel.Course.Lms.Application.Queues.Consumers;
 using Fsel.Course.Lms.Application.Queues.Publishers;
 using Fsel.Course.Lms.Application.Services.AiService;
+using Fsel.Course.Lms.Application.Services.AiService.SpeakingAIService;
+using Fsel.Course.Lms.Application.Services.AIService.SpeakingAIService;
 using Fsel.Course.Lms.Application.Services.InteractionService;
 using Fsel.Course.Lms.Application.Services.NotificationServices;
 using Fsel.Course.Lms.Application.Services.OrderServices;
@@ -101,6 +103,8 @@ builder.Services.AddScoped<IMockTestScoreRepository, MockTestScoreRepository>();
 builder.Services.AddScoped<IClassForumResultRandomRepository, ClassForumResultRandomRepository>();
 builder.Services.AddScoped<IVideoTimeCodeResultRepository, VideoTimeCodeResultRepository>();
 builder.Services.AddScoped<IMockTestAISettingRepository, MockTestAISettingRepository>();
+builder.Services.AddScoped<IClassForumDetailResultRepository, ClassForumDetailResultRepository>();
+builder.Services.AddScoped<ISpeakingAIService, SpeakingAIService>();
 
 builder.Services.AddScoped<QuestBoardPublisher>();
 builder.Services.AddScoped<SubmitMockTestAnswerPublisher>();
