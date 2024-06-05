dd2e5d4136477fd18cb23f1a7be67d7fe6c26bbc
Nguyễn Thành Đô <thanhdo@atlantic.edu.vn>
2024-06-05 23:04:46 +0700

    merge staging and testing

--- Services/Fsel.Realtime/Fsel.Realtime.Application/Hubs/MockTestWritingHub.cs
+++ Services/Fsel.Realtime/Fsel.Realtime.Application/Hubs/MockTestWritingHub.cs
@@ -4,11 +4,16 @@ namespace Fsel.Realtime.Application.Hubs
 {
     using Fsel.Core.Base;
     using Fsel.Core.Extensions;
+    using Fsel.Core.Services.IpApiServices;
     using Microsoft.AspNetCore.SignalR;
 
     public class MockTestWritingHub : BaseHub
     {
-        public override async Task OnConnectedAsync()
+        public MockTestWritingHub(AuthContext authContext, IIpApiService ipApiService) : base(authContext, ipApiService)
+        {
+        }
+
+        public override async Task OnConnectedHubAsync()
         {
             string mockTestCriteria = Context.GetHttpContext()?.Request.Query["MockTestResultId"].ToString()!;
             if (!string.IsNullOrEmpty(mockTestCriteria))
@@ -19,7 +24,7 @@ namespace Fsel.Realtime.Application.Hubs
             await base.OnConnectedAsync();
         }
 
-        public override async Task OnDisconnectedAsync(Exception? exception)
+        public override async Task OnDisconnectedHubAsync(Exception? exception)
         {
             string mockTestCriteria = Context.GetHttpContext()?.Request.Query["MockTestResultId"].ToString()!;
             if (!string.IsNullOrEmpty(mockTestCriteria))
