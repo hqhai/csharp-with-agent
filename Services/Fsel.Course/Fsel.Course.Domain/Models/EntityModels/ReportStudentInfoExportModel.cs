17bdcefdc21cfc98d1adbd63ea426e46ce92b647
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2024-06-14 08:48:54 +0700

    Export Report Progress Student

---
+++ Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/ReportStudentInfoExportModel.cs
@@ -0,0 +1,74 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Course.Domain.Models.EntityModels
+{
+    using Fsel.Course.Domain.Enums;
+    using Fsel.Shared.Enums;
+    using OfficeOpenXml.Attributes;
+
+    public class ReportStudentInfoExportModel
+    {
+        [EpplusTableColumn(Header = "FullName")]
+        public string? FullName { get; set; }
+
+        [EpplusTableColumn(Header = "Birthday", NumberFormat = "dd/MM/yyyy")]
+        public DateTime? Birthday { get; set; }
+
+        [EpplusTableColumn(Header = "Email")]
+        public string? Email { get; set; }
+
+        [EpplusTableColumn(Header = "PhoneNumber")]
+        public string? PhoneNumber { get; set; }
+
+        [EpplusTableColumn(Header = "CreatedDate", NumberFormat = "dd/MM/yyyy HH:mm:ss")]
+        public DateTime? CreatedDate { get; set; }
+
+        [EpplusTableColumn(Header = "School")]
+        public string? SchoolName { get; set; }
+
+        [EpplusTableColumn(Header = "District")]
+        public string? DistrictName { get; set; }
+
+        [EpplusTableColumn(Header = "Province/City")]
+        public string? Province { get; set; }
+
+        [EpplusTableColumn(Header = "PlacmentTest Status")]
+        public EnumResultStatus? PTStatus { get; set; }
+
+        [EpplusTableColumn(Header = "PT Level")]
+        public EnumCourseLevel? PTLevel { get; set; }
+
+        [EpplusTableColumn(Header = "Course Name")]
+        public string? CourseName { get; set; }
+
+        [EpplusTableColumn(Header = "Course Level")]
+        public EnumCourseLevel? CourseLevel { get; set; }
+
+        [EpplusTableColumn(Header = "Start Trial")]
+        public DateTime? StartTrial { get; set; }
+
+        [EpplusTableColumn(Header = "End Trial")]
+        public DateTime? EndTrial { get; set; }
+
+        [EpplusTableColumn(Header = "Unit")]
+        public string? UnitName { get; set; }
+
+        [EpplusTableColumn(Header = "Unit Average")]
+        public double? UnitAverage { get; set; }
+
+        [EpplusTableColumn(Header = "Lesson")]
+        public string? LessonName { get; set; }
+
+        [EpplusTableColumn(Header = "Lesson Completed")]
+        public string? LessonCompleted { get; set; }
+
+        [EpplusTableColumn(Header = "Total time")]
+        public long? TotalTime { get; set; }
+
+        [EpplusTableColumn(Header = "Total access")]
+        public int? TotalAccess { get; set; }
+
+        [EpplusTableColumn(Header = "Last entry", NumberFormat = "dd/MM/yyyy HH:mm:ss")]
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
--- Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/ProgressController.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/ProgressController.cs
@@ -271,5 +271,20 @@ namespace Fsel.Course.Lms.Api.Controllers
             }
             return File(commandResult.Result, Settings.Excels.ContentType, "report_student_export.xlsx");
         }
+
+        /// <summary>
+        /// Report Student
+        /// </summary>
+        [HttpPost("export-report-progress-student")]
+        [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> ExportProgressStudent([FromQuery] ImportEmailByReportProgressQuery query)
+        {
+            MethodResult<Stream> commandResult = await _mediator.Send(query).ConfigureAwait(false);
+            if (!commandResult.IsOK || commandResult.Result == null)
+            {
+                return commandResult.GetActionResult();
+            }
+            return File(commandResult.Result, Settings.Excels.ContentType, "report_progress_student_export.xlsx");
+        }
     }
 }
