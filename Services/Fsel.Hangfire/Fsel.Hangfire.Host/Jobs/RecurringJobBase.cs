20f1469037ad4d5c21a8066cb5a09036d391cb41
Nguyễn Thành Đô <thanhdo@atlantic.edu.vn>
2024-06-25 14:45:26 +0700

    Set time job WeeklyReportWorker, update logic WeeklyReportWorker

--- Services/Fsel.Hangfire/Fsel.Hangfire.Host/Jobs/RecurringJobBase.cs
+++ Services/Fsel.Hangfire/Fsel.Hangfire.Host/Jobs/RecurringJobBase.cs
@@ -20,7 +20,7 @@ namespace Fsel.Hangfire.Host.Jobs
             JobExtensions.SetRecurringJob<EndTrialRegistrationWorker>(WorkerSettings.JobName.EndTrialRegistrationJob, Cron.Daily());
             JobExtensions.SetRecurringJob<LeaderBoardWorker>(WorkerSettings.JobName.LeaderBoardJob, Cron.HourInterval(1));
             JobExtensions.SetRecurringJob<NoticeAccessTimeWorker>(WorkerSettings.JobName.NoticeAccessTime, Cron.Daily());
-            JobExtensions.SetRecurringJob<WeeklyReportWorker>(WorkerSettings.JobName.WeeklyReport, Cron.Daily(2, 0));
+            JobExtensions.SetRecurringJob<WeeklyReportWorker>(WorkerSettings.JobName.WeeklyReport, Cron.Daily(8, 0));
             //JobExtensions.SetRecurringJob<TestWorker>(WorkerSettings.JobName.TestWorkerJob, Cron.Daily);
         }
     }
