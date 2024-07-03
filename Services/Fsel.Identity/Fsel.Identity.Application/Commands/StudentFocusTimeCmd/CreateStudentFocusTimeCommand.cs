06879166ef56090389898a66ba085e80fee80ff2
hqhai <quanghai@atlantic.edu.vn>
2024-07-03 17:17:00 +0700

    fixbug/Fsel-2738: Bổ sung điều kiện số lần  cài đặt config focus time trong ngày

--- Services/Fsel.Identity/Fsel.Identity.Application/Commands/StudentFocusTimeCmd/CreateStudentFocusTimeCommand.cs
+++ Services/Fsel.Identity/Fsel.Identity.Application/Commands/StudentFocusTimeCmd/CreateStudentFocusTimeCommand.cs
@@ -83,7 +83,7 @@ namespace Fsel.Identity.Application.Commands.StudentFocusTimeCmd
                 else
                 {
                     // Set targetTime
-                    bool confitionChangeTarget = !studentFocusTime!.IsEstablished && request.TargetTime != 0;
+                    bool confitionChangeTarget = !studentFocusTime!.IsEstablished && request.TargetTime != studentFocusTime.TargetTime;
                     if (confitionChangeTarget)
                     {
                         studentFocusTime.TargetTime = request.TargetTime;
