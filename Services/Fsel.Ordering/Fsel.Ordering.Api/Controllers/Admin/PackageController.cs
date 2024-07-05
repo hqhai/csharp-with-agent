f952f1876628e83b16fda8aaa47278bff7f80841
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2024-07-05 13:25:00 +0700

    core: Update multiple api version

--- Services/Fsel.Ordering/Fsel.Ordering.Api/Controllers/Admin/PackageController.cs
+++ Services/Fsel.Ordering/Fsel.Ordering.Api/Controllers/Admin/PackageController.cs
@@ -3,16 +3,17 @@
 namespace Fsel.Ordering.Api.Controllers.Admin
 {
     using System.Net;
-    using Asp.Versioning;
     using Fsel.Common.ActionResults;
     using Fsel.Common.Constants;
+    using Fsel.Core.Base;
     using Fsel.Ordering.Application.Commands.PackageCmds;
-    using Fsel.Shared.Bases.V1;
+    using Fsel.Shared.Attributes;
     using Fsel.Shared.Constants;
     using Fsel.Shared.Enums;
     using MediatR;
     using Microsoft.AspNetCore.Mvc;
 
+    [ApiVersions(ApiSettings.APIVersion1)]
     [Route(Settings.APIDefaultRoute + "/admin/package")]
     [Common.Attributes.Permission(role: nameof(EnumRole.Admin))]
     [ApiController]
