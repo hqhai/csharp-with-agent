a0dee5f36a161c4d4ec32cce0f523dd4c40d47dc
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2024-07-08 10:57:03 +0700

    fsel-2719: fixbug job active event error

--- Services/Fsel.Ordering/Fsel.Ordering.Application/Queues/Consumers/JobActiveEventConsumer.cs
+++ Services/Fsel.Ordering/Fsel.Ordering.Application/Queues/Consumers/JobActiveEventConsumer.cs
@@ -6,19 +6,19 @@ namespace Fsel.Ordering.Application.Queues.Consumers
     using Fsel.Core.Base;
     using Fsel.Core.Base.BaseModels;
     using Fsel.Ordering.Application.Commands.Events;
-    using MassTransit;
     using MediatR;
+    using Microsoft.AspNetCore.Http;
 
     public class JobActiveEventConsumer : BaseConsumer<BaseQueueModel>
     {
         private readonly IMediator _mediator;
 
-        public JobActiveEventConsumer(IMediator mediator, AuthContext authContext, Microsoft.AspNetCore.Http.IHttpContextAccessor httpContextAccessor) : base(authContext, httpContextAccessor)
+        public JobActiveEventConsumer(IMediator mediator, AuthContext authContext, IHttpContextAccessor httpContextAccessor) : base(authContext, httpContextAccessor)
         {
             _mediator = mediator;
         }
 
-        public async Task ConsumeQueue(BaseQueueModel message)
+        public override async Task ConsumeQueue(BaseQueueModel? message)
         {
             if (message == null)
             {
