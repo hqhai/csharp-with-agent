593dad2c0bc96e6185de63e31e69cea6ed8baaee
hqhai <quanghai@atlantic.edu.vn>
2024-07-09 19:56:35 +0700

    feature/Fsel-2821_techie: bổ sung thêm một số dạng hoàn thành timecode để gửi cho techie

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/VideoTimeCodeAnswerCmd/CreateVideoTimeCodeResultCommand.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/VideoTimeCodeAnswerCmd/CreateVideoTimeCodeResultCommand.cs
@@ -2,6 +2,7 @@
 
 namespace Fsel.Course.Lms.Application.Commands.VideoTimeCodeAnswerCmd
 {
+    using System.Globalization;
     using AutoMapper;
     using Fsel.Common.ActionResults;
     using Fsel.Common.Enums.ErrorCodes;
@@ -9,7 +10,10 @@ namespace Fsel.Course.Lms.Application.Commands.VideoTimeCodeAnswerCmd
     using Fsel.Course.Domain.Enums;
     using Fsel.Course.Domain.IRepositories;
     using Fsel.Course.Domain.Models.EntityModels;
+    using Fsel.Course.Lms.Application.Queues.Publishers;
+    using Fsel.Shared.Enums;
     using Fsel.Shared.Helpers;
+    using Fsel.Shared.Models.ShareModels;
     using MediatR;
     using Microsoft.AspNetCore.Http;
     using Microsoft.EntityFrameworkCore;
@@ -30,14 +34,16 @@ namespace Fsel.Course.Lms.Application.Commands.VideoTimeCodeAnswerCmd
         private readonly IMapper _mapper;
         private readonly IVideoTimeCodeResultRepository _videoTimeCodeResultRepository;
         private readonly ILogger<object> _logger;
+        private readonly TechieActionPublisher _techieActionPublisher;
 
-        public CreateVideoTimeCodeResultCommandHandler(IVideoResultRepository videoResultRepository, IVideoTimeCodeRepository videoTimeCodeRepository, IMapper mapper, IVideoTimeCodeResultRepository videoTimeCodeResultRepository, ILogger<object> logger)
+        public CreateVideoTimeCodeResultCommandHandler(IVideoResultRepository videoResultRepository, IVideoTimeCodeRepository videoTimeCodeRepository, IMapper mapper, IVideoTimeCodeResultRepository videoTimeCodeResultRepository, ILogger<object> logger, TechieActionPublisher techieActionPublisher)
         {
             _videoResultRepository = videoResultRepository;
             _videoTimeCodeRepository = videoTimeCodeRepository;
             _mapper = mapper;
             _videoTimeCodeResultRepository = videoTimeCodeResultRepository;
             _logger = logger;
+            _techieActionPublisher = techieActionPublisher;
         }
 
         public async Task<MethodResult<VideoTimeCodeResultModel>> Handle(CreateVideoTimeCodeResultCommand request, CancellationToken cancellationToken)
@@ -57,6 +63,7 @@ namespace Fsel.Course.Lms.Application.Commands.VideoTimeCodeAnswerCmd
                 methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist), nameof(videoTimeCode));
                 return methodResult;
             }
+            await StreakTimeCodeCheer(request, cancellationToken);
             methodResult.Result = _mapper.Map<VideoTimeCodeResultModel>(await GetAndUpdateVideoTimeCodeResultAsync(request, videoTimeCode, videoResult));
             methodResult.StatusCode = StatusCodes.Status200OK;
             return methodResult;
@@ -85,11 +92,54 @@ namespace Fsel.Course.Lms.Application.Commands.VideoTimeCodeAnswerCmd
             return videoTimeCodeResult;
         }
 
+        private async Task StreakTimeCodeCheer(CreateVideoTimeCodeResultCommand request, CancellationToken cancellation)
+        {
+            var videoTimeCodeResults = _videoTimeCodeResultRepository.Queryable.OrderBy(x => x.CreatedDate).Where(x => x.VideoResultId == request.VideoResultId).ToList();
+
+            var currentVideoTimeCodeResults = await _videoTimeCodeResultRepository.Queryable.Where(x => x.VideoTimeCodeId == request.VideoTimeCodeId && x.VideoResultId == request.VideoResultId).FirstOrDefaultAsync(cancellation);
+
+            if (currentVideoTimeCodeResults != null && currentVideoTimeCodeResults.CorrectCount == currentVideoTimeCodeResults.CorrectTotal)
+            {
+                int indexOfCurrent = videoTimeCodeResults.IndexOf(currentVideoTimeCodeResults);
+
+
+                int count = videoTimeCodeResults.Take(indexOfCurrent + 1).Count(x => x.CorrectCount == x.CorrectTotal);
+
+                if (count >= 5)
+                {
+                    StudentTechieActionModel model = new StudentTechieActionModel()
+                    {
+                        Config = new TechieConfig
+                        {
+                            Value = count.ToString(CultureInfo.InvariantCulture)
+                        },
+                        Feature = EnumTechieFeature.Cheer,
+                        Action = NumberOfCorrectTimeCode(count)
+                    };
+
+                    await _techieActionPublisher.Publish(model, cancellation);
+                }
+            }
+        }
+
+
         private async Task UpdateVideoResult(VideoResult videoResult, Guid videoTimeCodeId)
         {
             videoResult.CurrentVideoTimeCodeId = videoTimeCodeId;
             _videoResultRepository.Update(videoResult);
             await _videoResultRepository.UnitOfWork.SaveChangesAsync().ConfigureAwait(false);
+        }
+
+        private static EnumTechieAction NumberOfCorrectTimeCode(int correctCount)
+        {
+            switch (correctCount)
+            {
+                case 5:
+                    return EnumTechieAction.StreakFiveTimeCode;
+                default:
+                    return EnumTechieAction.StreakFiveTimeCode;
+
+            }
         }
     }
 }
