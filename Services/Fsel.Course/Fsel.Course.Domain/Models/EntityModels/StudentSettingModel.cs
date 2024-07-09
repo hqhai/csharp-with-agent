35903108ac25052a31a8c428570327422a55b4a7
Nguyễn Thành Đô <thanhdo@atlantic.edu.vn>
2024-07-09 19:19:12 +0700

    fsel-2719: Add validate choose level befor done PT

--- Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/StudentSettingModel.cs
+++ Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/StudentSettingModel.cs
@@ -9,6 +9,7 @@ namespace Fsel.Course.Domain.Models.EntityModels
         public bool IsPlacementTest { get; set; }
         public Guid? ClassId { get; set; }
         public EnumCourseLevel? Level { get; set; }
+        public EnumCourseLevel? BaseCourseLevel { get; set; }
         public EnumPlacementTestLevel? PTLevel { get; set; }
         public EnumCourseLevel? StartPTLevel { get; set; }
         public int ModuleNumber { get; set; }
