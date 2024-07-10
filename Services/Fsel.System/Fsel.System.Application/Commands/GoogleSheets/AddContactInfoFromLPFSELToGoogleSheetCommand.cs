105e5b555f54aaf9e374b322a912c882b51535c2
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2024-07-10 15:11:27 +0700

    feature/fsel-2939: update currentDate

--- Services/Fsel.System/Fsel.System.Application/Commands/GoogleSheets/AddContactInfoFromLPFSELToGoogleSheetCommand.cs
+++ Services/Fsel.System/Fsel.System.Application/Commands/GoogleSheets/AddContactInfoFromLPFSELToGoogleSheetCommand.cs
@@ -45,7 +45,7 @@ namespace Fsel.System.Application.Commands.GoogleSheets
             var credentialsPath = ResourceSettings.I18NCredentialsFilePath;
 
             var birthday = request.Birthday.ToString("dd-MM-yyyy", CultureInfo.CurrentCulture);
-            var currentDate = DateTime.UtcNow.ConvertTimeFromUtc(EnumCountryKey.Vietnam).ToString("dd-MM-yyyy hh:mm", CultureInfo.CurrentCulture);
+            var currentDate = DateTime.UtcNow.ConvertTimeFromUtc(EnumCountryKey.Vietnam).ToString("dd-MM-yyyy HH:mm", CultureInfo.CurrentCulture);
 
             var data = new List<IList<object>>()
         {
