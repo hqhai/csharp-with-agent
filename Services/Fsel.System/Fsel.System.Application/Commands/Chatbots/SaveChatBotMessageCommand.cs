f9ec0a906e0362a173d6e803d25e906acb1c9a82
hqhai <quanghai@atlantic.edu.vn>
2024-07-11 11:01:01 +0700

    đặt log chabot web-socket

--- Services/Fsel.System/Fsel.System.Application/Commands/Chatbots/SaveChatBotMessageCommand.cs
+++ Services/Fsel.System/Fsel.System.Application/Commands/Chatbots/SaveChatBotMessageCommand.cs
@@ -217,7 +217,7 @@ namespace Fsel.System.Application.Commands.Chatbots
                 FilePath = filePath,
                 TokenRatio = tokenRation
             };
-
+            _logger.LogInformation($"Feature Send to ChatbotPublisher:{ConvertHelper.Serialize(model)}");
             await _chatBotPublisher.Publish(model, cancellationToken);
         }
 
