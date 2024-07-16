f5cd2e7da440538b4d1d56fb6557358e09b7ecdb
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2024-07-16 14:51:23 +0700

    feature/FSEL-2984: Update Migrations

--- Services/Fsel.Course/Fsel.Course.Infrastructure/Migrations/20240716074336_Update_VideoResult_TimeCodeHighestStreak.Designer.cs
+++ Services/Fsel.Course/Fsel.Course.Infrastructure/Migrations/20240716074336_Update_VideoResult_TimeCodeHighestStreak.Designer.cs
@@ -4,6 +4,7 @@ using Fsel.Course.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fsel.Course.Infrastructure.Migrations
 {
     [DbContext(typeof(CourseDbContext))]
-    partial class CourseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240716074336_Update_VideoResult_TimeCodeHighestStreak")]
+    partial class Update_VideoResult_TimeCodeHighestStreak
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
