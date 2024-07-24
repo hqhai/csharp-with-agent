62579cb6e9c480aaf162e37852bae93cafead484
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2024-07-24 19:00:31 +0700

    fixbug/FSEL-2180: Update Progress Admin Lesson

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/StudentProgressQuery/GetStudentProgressLessonsQuery.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/StudentProgressQuery/GetStudentProgressLessonsQuery.cs
@@ -151,7 +151,7 @@ namespace Fsel.Course.Lms.Application.Queries.StudentProgressQuery
             {
                 var lesson = lessonResult.Lesson;
                 counts.Add(lessonResult.VideoResult?.Status == EnumResultStatus.Done ? 1 : 0);
-                counts.Add(lessonResult.ClassForumResults.Where(x => x != null && (x.Status == EnumClassForumResultStatus.Denied || x.Status == EnumClassForumResultStatus.Graded) && x.StudentId == request.StudentId).Count());
+                counts.Add(lessonResult.ClassForumResults.Where(x => x != null && x.Status.HasValue && x.StudentId == request.StudentId).Count());
                 counts.Add(lessonResult.HomeWorkResults.Any() && lessonResult.HomeWorkResults.All(x => x != null && x.Status == EnumResultStatus.Done && x.StudentId == request.StudentId) ? 1 : 0);
                 if (lesson != null)
                 {
