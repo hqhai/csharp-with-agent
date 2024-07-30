18219a469778efdff8156159c3310db9c50e844c
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2024-07-30 16:56:38 +0700

    feature/change mail bcc

--- Services/Fsel.Sender/Fsel.Sender.Application/Commands/SendEmailCmd/SendEmailCommand.cs
+++ Services/Fsel.Sender/Fsel.Sender.Application/Commands/SendEmailCmd/SendEmailCommand.cs
@@ -35,10 +35,10 @@ namespace Fsel.Sender.Application.Commands.SendEmailCmd
             ArgumentNullException.ThrowIfNull(request);
             MethodResult<bool> methodResult = new MethodResult<bool>();
 
-            var ccEmailDefault = _appSetting.EmailConfig?.BCCEmail;
-            if (ccEmailDefault != null && ccEmailDefault.Count > 0)
+            var bccEmail = _appSetting.EmailConfig?.BCCEmail;
+            if (bccEmail != null && bccEmail.Count > 0)
             {
-                ccEmailDefault.ForEach(request.CcEmails.Add);
+                bccEmail.ForEach(request.BccEmails.Add);
             }
 
             #region Get CC Email
