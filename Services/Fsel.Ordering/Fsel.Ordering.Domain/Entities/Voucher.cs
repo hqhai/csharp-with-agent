551433073fd13f6dd7fb43bc8aa1558b10220717
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2024-07-31 09:51:06 +0700

    feature/fsel-3144: update entity

--- Services/Fsel.Ordering/Fsel.Ordering.Domain/Entities/Voucher.cs
+++ Services/Fsel.Ordering/Fsel.Ordering.Domain/Entities/Voucher.cs
@@ -5,49 +5,55 @@ namespace Fsel.Ordering.Domain.Entities
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
     using Fsel.Common.Enums.ErrorCodes;
-    using Fsel.Common.Helpers;
     using Fsel.Core.Entities;
-    using Fsel.Ordering.Domain.Enums;
     using Fsel.Shared.Enums;
 
     public class Voucher : Entity
     {
         /// <summary>
-        /// Tên khóa học
+        /// Mã voucher
         /// </summary>
         [Required(ErrorMessage = nameof(EnumSystemErrorCode.Required))]
-        [MaxLength(250, ErrorMessage = nameof(EnumSystemErrorCode.MaxLength))]
+        [MaxLength(8, ErrorMessage = nameof(EnumSystemErrorCode.MaxLength))]
+        public string? Code { get; set; }
+
+        /// <summary>
+        /// Tên voucher
+        /// </summary>
+        [Required(ErrorMessage = nameof(EnumSystemErrorCode.Required))]
+        [MaxLength(30, ErrorMessage = nameof(EnumSystemErrorCode.MaxLength))]
         public string? Name { get; set; }
 
-        public DateTime? StartDate { get; set; }
+        /// <summary>
+        /// Phần trăm giảm
+        /// </summary>
+        public int Percent { get; set; }
 
+        /// <summary>
+        /// Số lượng tối đa
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Ngày bắt đầu
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// Ngày kết thúc
+        /// </summary>
         public DateTime? EndDate { get; set; }
 
-        public string? ContentFilePath { get; set; }
+        /// <summary>
+        /// Loại voucher
+        /// </summary>
+        public EnumVoucherType VoucherType { get; set; }
 
-        public bool IsGlobal { get; set; }
-
+        /// <summary>
+        /// Trạng thái
+        /// </summary>
         public bool IsActive { get; set; }
-
-        public string? CustomerTypesStr { get; set; }
-
-        [NotMapped]
-        public IList<EnumCustomerType>? CustomerTypes
-        {
-            get { return ConvertHelper.Deserialize<IList<EnumCustomerType>>(CustomerTypesStr); }
-            set { CustomerTypesStr = ConvertHelper.Serialize(value); }
-        }
-
-        public string? CourseLevelsStr { get; set; }
-
-        [NotMapped]
-        public IList<EnumCourseLevel>? CourseLevels
-        {
-            get { return ConvertHelper.Deserialize<IList<EnumCourseLevel>>(CourseLevelsStr); }
-            set { CourseLevelsStr = ConvertHelper.Serialize(value); }
-        }
 
         public ICollection<VoucherPackage> VoucherPackages { get; set; } = new List<VoucherPackage>();
 
