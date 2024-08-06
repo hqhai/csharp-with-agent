9d9b23d667cca148926cfcd5322651f49e4f188c
Nguyễn Thành Đô <thanhdo@atlantic.edu.vn>
2024-08-06 13:38:52 +0700

    openid: update appsettings

--- Services/Fsel.Identity/Fsel.Identity.Infrastructure/Migrations/20240603025110_Update_Add_Field_Student.cs
+++ Services/Fsel.Identity/Fsel.Identity.Infrastructure/Migrations/20240603025110_Update_Add_Field_Student.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -58,9 +58,9 @@ namespace Fsel.Identity.Infrastructure.Migrations
                 {
                     table.PrimaryKey("PK_UserCourseSettings", x => x.Id);
                     table.ForeignKey(
-                        name: "FK_UserCourseSettings_AspNetUsers_UserId",
+                        name: "FK_UserCourseSettings_Users_UserId",
                         column: x => x.UserId,
-                        principalTable: "AspNetUsers",
+                        principalTable: "Users",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
                 });
