0edecf6f75f4db7c507778d0422cbbd0508ee3b9
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2024-08-06 14:25:52 +0700

    fixbug/SendMail: Update Send Email unit report

--- Services/Fsel.Course/Fsel.Course.Lms.Application/InternalEvents/BaseInternalUnitResultEventHandler.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/InternalEvents/BaseInternalUnitResultEventHandler.cs
@@ -148,7 +148,7 @@ namespace Fsel.Course.Lms.Application.InternalEvents
 
             var listClassForumResult = await _classForumResultRepository.Queryable.Where(p => lessonResultIds.Contains(p.LessonResultId)).ToListAsync(cancellationToken);
 
-            var isSendEmail = lessonResultIds.Count == listClassForumResult.Count && !listClassForumResult.Any(p => p.Status != EnumClassForumResultStatus.Graded);
+            var isSendEmail = lessonResultIds.Count == listClassForumResult.Count && listClassForumResult.All(p => p.Status.HasValue);
             if (!isSendEmail)
                 return;
             MockTestResult? mockTestResult = default;
