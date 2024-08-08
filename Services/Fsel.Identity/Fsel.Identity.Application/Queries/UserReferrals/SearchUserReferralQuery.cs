2bcc75ae620c1e66eaf350fb2c7f972eed151a64
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2024-08-08 15:33:18 +0700

    feature/fsel-3077: update user referral

---
+++ Services/Fsel.Identity/Fsel.Identity.Application/Queries/UserReferrals/SearchUserReferralQuery.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Identity.Application.Queries.UserReferrals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SearchUserReferralQuery
+    {
+    }
+}
