91d540feab04dcd9e4c9921bdfaede40884497ee
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2024-08-08 17:17:24 +0700

    feature/fsel-3077: manage referral code

--- Services/Fsel.Identity/Fsel.Identity.Domain/Models/EntityModels/SearchReferralCodeModel.cs
+++ Services/Fsel.Identity/Fsel.Identity.Domain/Models/EntityModels/SearchReferralCodeModel.cs
@@ -4,5 +4,13 @@ namespace Fsel.Identity.Domain.Models.EntityModels
 {
     public class SearchReferralCodeModel
     {
+        public Guid SenderId { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? UserName { get; set; }
+        public string? Code { get; set; }
+        public string? Link { get; set; }
+        public int NumberUser { get; set; }
+        public int TotalToken { get; set; }
     }
 }
