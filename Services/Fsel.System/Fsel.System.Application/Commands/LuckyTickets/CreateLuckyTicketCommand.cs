6f13029c19c2cd7715c2c0197f426d505be46a04
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2024-08-12 15:49:31 +0700

    fixbug/create lucky ticket for user

--- Services/Fsel.System/Fsel.System.Application/Commands/LuckyTickets/CreateLuckyTicketCommand.cs
+++ Services/Fsel.System/Fsel.System.Application/Commands/LuckyTickets/CreateLuckyTicketCommand.cs
@@ -78,7 +78,7 @@ namespace Fsel.System.Application.Commands.LuckyTickets
             }
 
             var checkLuckySpin = checkLuckySpinResults.Content?.Result;
-            if (checkLuckySpin != null && checkLuckySpin.Count > 0)
+            if (checkLuckySpin == null || checkLuckySpin.Count == 0)
             {
                 methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist));
                 return methodResult;
