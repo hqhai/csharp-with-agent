2e06dd1949e4421c78d9c6770a50ba3eaa7d4db3
hqhai <quanghai@atlantic.edu.vn>
2024-08-20 14:26:55 +0700

    update/fsel-3208: Bổ sung dấu ,

--- Services/Fsel.Identity/Fsel.Identity.Api/Program.cs
+++ Services/Fsel.Identity/Fsel.Identity.Api/Program.cs
@@ -81,7 +81,8 @@ queues: new Dictionary<string, Type>
     { QueueSettings.LmsQueue.NameQueue.SaveUserCourseSetting, typeof(SaveUserCourseSettingConsumer) },
     { QueueSettings.TrainingQueue.NameQueue.SaveUserCourseSetting, typeof(SaveUserCourseSettingConsumer) },
     { QueueSettings.UserQueue.NameQueue.AddExpiredDateForStudent, typeof(AddExpiredDateForStudentConsumer) },
-    { QueueSettings.LmsQueue.NameQueue.StudentRankingEvent, typeof(StudentRankingEventsConsumer) }
+    { QueueSettings.LmsQueue.NameQueue.StudentRankingEvent, typeof(StudentRankingEventsConsumer) },
+    { QueueSettings.UserQueue.NameQueue.AddFeatureMission, typeof(AddFeatureMissionConsumer) }
 });
 var app = builder.Build();
 app.UseServices();
