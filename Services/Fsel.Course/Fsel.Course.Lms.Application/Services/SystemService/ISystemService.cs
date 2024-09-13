35c2a2a760f3cec7b94d7b5391e1630f8c9f7d75
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2024-09-13 16:52:19 +0700

    feature/FSEL-3372: Update QuestionExplanationErrorReport

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Services/SystemService/ISystemService.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Services/SystemService/ISystemService.cs
@@ -44,5 +44,8 @@ namespace Fsel.Course.Lms.Application.Services.SystemService
 
         [Get("/v1/feature-access-time/execute-list-query")]
         Task<IApiResponse<MethodResult<IList<FeatureAccessTimeModel>>>> GetListFeatureAccessTime([FromQuery] BaseQueryModel model);
+
+        [Post("/v1/google-sheet/add-error-report-explanation-question")]
+        Task<IApiResponse<MethodResult<IList<FeatureAccessTimeModel>>>> AddErrorReportExplanationQuestionToGoogleSheet([FromBody] AddErrorReportExplanationQuestionModel model);
     }
 }
--- Services/Fsel.System/Fsel.System.Api/Controllers/GoogleSheetController.cs
+++ Services/Fsel.System/Fsel.System.Api/Controllers/GoogleSheetController.cs
@@ -97,5 +97,17 @@ namespace Fsel.System.Api.Controllers
             var commandResult = await _mediator.Send(command).ConfigureAwait(false);
             return commandResult.GetActionResult();
         }
+
+        /// <summary>
+        /// Add Error Report Explanation Question  To GoogleSheet
+        /// </summary>
+        [HttpPost("add-error-report-explanation-question")]
+        [ProducesResponseType(typeof(MethodResult<bool>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> AddErrorReportExplanationQuestionToGoogleSheet([FromBody] AddErrorReportExplanationQuestionToGoogleSheetCommand command)
+        {
+            var commandResult = await _mediator.Send(command).ConfigureAwait(false);
+            return commandResult.GetActionResult();
+        }
     }
 }
