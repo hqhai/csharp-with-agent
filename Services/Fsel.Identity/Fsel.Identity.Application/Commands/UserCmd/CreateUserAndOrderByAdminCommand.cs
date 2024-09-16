a3c0d7c7b23146b4d906717791fea7401e6d62f7
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2024-09-16 13:39:27 +0700

    feature-testing/fsel-3395: api create user and order by admin

---
+++ Services/Fsel.Identity/Fsel.Identity.Application/Commands/UserCmd/CreateUserAndOrderByAdminCommand.cs
@@ -0,0 +1,86 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Identity.Application.Commands.UserCmd
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Fsel.Common.ActionResults;
+    using Fsel.Common.Enums.ErrorCodes;
+    using Fsel.Core.Base.Managers;
+    using Fsel.Identity.Application.Services.OrderService;
+    using Fsel.Identity.Application.Services.OrderService.Model;
+    using Fsel.Identity.Domain.Entities;
+    using Fsel.Identity.Domain.Models.CommandModels.Users;
+    using MediatR;
+    using Microsoft.EntityFrameworkCore;
+
+    public class CreateUserAndOrderByAdminCommand : CreateUserAndOrderByAdminCommandModel, IRequest<MethodResult<bool>>
+    {
+    }
+
+    public class CreateUserAndOrderByAdminCommandHandler : IRequestHandler<CreateUserAndOrderByAdminCommand, MethodResult<bool>>
+    {
+        private readonly IMediator _mediator;
+        private readonly UserManager<User> _userManager;
+        private readonly IOrderService _orderService;
+
+        public CreateUserAndOrderByAdminCommandHandler(IMediator mediator, UserManager<User> userManager, IOrderService orderService)
+        {
+            _mediator = mediator;
+            _userManager = userManager;
+            _orderService = orderService;
+        }
+
+        public async Task<MethodResult<bool>> Handle(CreateUserAndOrderByAdminCommand request, CancellationToken cancellationToken)
+        {
+            ArgumentNullException.ThrowIfNull(request);
+            var methodResult = new MethodResult<bool>();
+
+            var user = await _userManager.Users.FirstOrDefaultAsync(p => p.Email.Trim().ToLower() == request.Email.Trim().ToLower() || p.UserName.Trim().ToLower() == request.Email.Trim().ToLower(), cancellationToken);
+
+            if (user == null)
+            {
+                var result = await _mediator.Send(new CreateUserByAdminCommand()
+                {
+                    FullName = request.FullName,
+                    Email = request.Email,
+                    School = request.School,
+                    DateOfBirth = request.DateOfBirth,
+                    ReferralCode = request.ReferralCode,
+                }, cancellationToken);
+                if (!result.IsOK)
+                {
+                    methodResult.AddError(result.ErrorMessages);
+                    return methodResult;
+                }
+
+                user = result.Result;
+            }
+
+            if (user == null)
+            {
+                methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist));
+                return methodResult;
+            }
+
+            var createOrderResult = await _orderService.CreateOrderPayment(new CreateOrderPaymentCommandModel()
+            {
+                UserId = user.Id,
+                FullName = request.FullName,
+                Email = request.Email,
+                MonthNumber = request.MonthNumber,
+                IsRevenue = request.IsRevenue,
+                IsSendMail = false,
+                VoucherCode = request.VoucherCode,
+                EventCode = request.EventCode,
+            });
+
+            if (!createOrderResult.IsSuccessStatusCode)
+            {
+                methodResult.AddError(createOrderResult.Error);
+                return methodResult;
+            }
+            return methodResult;
+        }
+    }
+}
---
+++ Services/Fsel.Identity/Fsel.Identity.Domain/Models/CommandModels/Users/CreateUserByAdminCommandModel.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Identity.Domain.Models.CommandModels.Users
+{
+    using System;
+
+    public class CreateUserByAdminCommandModel
+    {
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? School { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? ReferralCode { get; set; }
+    }
+}
