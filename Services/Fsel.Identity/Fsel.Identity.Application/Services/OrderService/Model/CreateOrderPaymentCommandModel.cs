59fa2e9e3c58ec09aad3cae0aa85ca2e64f1978a
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2024-09-16 13:39:27 +0700

    feature-testing/fsel-3395: api create user and order by admin

---
+++ Services/Fsel.Identity/Fsel.Identity.Application/Services/OrderService/Model/CreateOrderPaymentCommandModel.cs
@@ -0,0 +1,18 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Identity.Application.Services.OrderService.Model
+{
+    using System;
+
+    public class CreateOrderPaymentCommandModel
+    {
+        public Guid UserId { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public int MonthNumber { get; set; }
+        public bool IsRevenue { get; set; }
+        public bool IsSendMail { get; set; }
+        public string? VoucherCode { get; set; }
+        public string? EventCode { get; set; }
+    }
+}
--- Services/Fsel.Ordering/Fsel.Ordering.Api/Controllers/V1i2/Admin/AdminController.cs
+++ Services/Fsel.Ordering/Fsel.Ordering.Api/Controllers/V1i2/Admin/AdminController.cs
@@ -65,5 +65,17 @@ namespace Fsel.Ordering.Api.Controllers.V1i2.Admin
             var queryResult = await _mediator.Send(new GetOrderByIdQuery() { Id = id }).ConfigureAwait(false);
             return queryResult.GetActionResult();
         }
+
+        /// <summary>
+        /// create order payment
+        /// </summary>
+        [HttpPost("create-order-payment")]
+        [ProducesResponseType(typeof(MethodResult<PagingItemsModel<SearchOrderModel>>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> CreateOrderPayment([FromBody] CreateOrderPaymentCommand command)
+        {
+            var queryResult = await _mediator.Send(command).ConfigureAwait(false);
+            return queryResult.GetActionResult();
+        }
     }
 }
