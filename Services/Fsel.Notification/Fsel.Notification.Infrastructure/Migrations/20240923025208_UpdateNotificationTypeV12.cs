54b9be7787560e5bf93192615ad3debf5ef57047
hqhai <quanghai@atlantic.edu.vn>
2024-09-23 09:52:34 +0700

    fixbug/fsel-3232: Bỏ đi đường link bị thừa, không dùng đến

---
+++ Services/Fsel.Notification/Fsel.Notification.Infrastructure/Migrations/20240923025208_UpdateNotificationTypeV12.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fsel.Notification.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateNotificationTypeV12 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "NotificationTypes",
+                keyColumn: "Id",
+                keyValue: new Guid("cd8a1edb-029f-4b2d-9725-c796e6fd7e5b"),
+                column: "TemplateLink",
+                value: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "NotificationTypes",
+                keyColumn: "Id",
+                keyValue: new Guid("cd8a1edb-029f-4b2d-9725-c796e6fd7e5b"),
+                column: "TemplateLink",
+                value: "/learn");
+        }
+    }
+}
