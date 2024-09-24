957edb55e645c08d7a6f860054f48f40cb24b2b5
Minh Chiến Lê Xuân <minhchien@atlantic.edu.vn>
2024-09-24 16:21:31 +0700

    feature/fsel-1916: course-target

---
+++ Services/Fsel.System/Fsel.System.Domain/Entities/CourseTargetConfig.cs
@@ -0,0 +1,18 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.System.Domain.Entities
+{
+    using Fsel.Core.Entities;
+    using Fsel.Shared.Enums;
+
+    public class CourseTargetConfig : Entity
+    {
+        public string? Title { get; set; }
+
+        public EnumCourseType CourseType { get; set; }
+
+        public int LessonNumberPerWeek { get; set; }
+
+        public int MaxHoursPerLesson { get; set; }
+    }
+}
--- Services/Fsel.System/Fsel.System.Infrastructure/SystemDbContext.cs
+++ Services/Fsel.System/Fsel.System.Infrastructure/SystemDbContext.cs
@@ -56,6 +56,7 @@ namespace Fsel.System.Infrastructure
             modelBuilder.ApplyConfiguration(new TechieActionEntityTypeConfiguration());
             modelBuilder.ApplyConfiguration(new StudentTechieEntityTypeConfiguration());
             modelBuilder.ApplyConfiguration(new LuckyTicketEntityTypeConfigConfiguration());
+            modelBuilder.ApplyConfiguration(new CourseTargetConfigEntityTypeConfigConfiguration());
             base.OnModelCreating(modelBuilder);
         }
 
@@ -90,6 +91,7 @@ namespace Fsel.System.Infrastructure
         public DbSet<StudentTechie> StudentTechies { get; set; }
         public DbSet<UserConfig> UserConfigs { get; set; }
         public DbSet<LuckyTicket> LuckyTickets { get; set; }
+        public DbSet<CourseTargetConfig> CourseTargetConfigs { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
