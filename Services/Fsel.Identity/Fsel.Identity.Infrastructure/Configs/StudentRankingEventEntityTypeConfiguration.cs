2b10970161ea947ae1b19d07b8ae5dc89a4f23f7
hqhai <quanghai@atlantic.edu.vn>
2024-10-01 16:13:40 +0700

    update/fsel-3478: Bổ sung thêm course-type cho sự kiện

--- Services/Fsel.Identity/Fsel.Identity.Infrastructure/Configs/StudentRankingEventEntityTypeConfiguration.cs
+++ Services/Fsel.Identity/Fsel.Identity.Infrastructure/Configs/StudentRankingEventEntityTypeConfiguration.cs
@@ -3,7 +3,9 @@
 namespace Fsel.Identity.Infrastructure.Configs
 {
     using System;
+    using Fsel.Common.Helpers;
     using Fsel.Identity.Domain.Entities;
+    using Fsel.Shared.Enums;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -12,6 +14,12 @@ namespace Fsel.Identity.Infrastructure.Configs
         public void Configure(EntityTypeBuilder<StudentRankingEvent> builder)
         {
             ArgumentNullException.ThrowIfNull(builder);
+
+            builder.Property(e => e.CourseType)
+                   .HasMaxLength(100)
+                   .HasConversion(
+                        v => v.ToString(),
+                        v => v.EnumParse<EnumCourseType>());
         }
     }
 }
