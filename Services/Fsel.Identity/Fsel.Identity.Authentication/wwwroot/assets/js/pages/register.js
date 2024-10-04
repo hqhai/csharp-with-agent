582a65224620415131d72e5f2b9580169d65ba5c
Nguyễn Thành Đô <thanhdo@atlantic.edu.vn>
2024-10-04 18:43:55 +0700

    openid: fixbug email trim()

--- Services/Fsel.Identity/Fsel.Identity.Authentication/wwwroot/assets/js/pages/register.js
+++ Services/Fsel.Identity/Fsel.Identity.Authentication/wwwroot/assets/js/pages/register.js
@@ -39,7 +39,7 @@
 
   // Validation functions
   function validateEmail() {
-    const email = $emailInput.val().trim();
+    const email = $emailInput.val();
     const emailPattern = /^(?=.{1,64}@)(?=.{1,255}$)[a-zA-Z0-9!#$%&'*+/=?^_{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$/;
     if (email !== "" && emailPattern.test(email)) {
       $emailInput.removeClass("content-border-danger");
