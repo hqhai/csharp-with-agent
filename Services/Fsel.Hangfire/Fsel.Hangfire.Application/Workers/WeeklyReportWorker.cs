47a93aa99f32c51b8a71e47a1c5581695b2952d0
Nguyễn Thành Đô <thanhdo@atlantic.edu.vn>
2024-10-07 14:50:35 +0700

    fixbug: disable job weekly report, enable api weekly report

--- Services/Fsel.Hangfire/Fsel.Hangfire.Application/Workers/WeeklyReportWorker.cs
+++ Services/Fsel.Hangfire/Fsel.Hangfire.Application/Workers/WeeklyReportWorker.cs
@@ -5,19 +5,31 @@ namespace Fsel.Hangfire.Application.Workers
     using System.Threading.Tasks;
     using Fsel.Core.Base.Interfaces;
     using Fsel.Hangfire.Application.Queues.Publishers;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
 
-    public class WeeklySnapShotLeaderBoardWorker : IWorker
+    public class WeeklyReportWorker : IWorker
     {
-        private readonly WeeklySnapShotLeaderBoardPublisher _weeklySnapShotPublisher;
+        //private readonly WeeklyReportPublisher _weeklyReportPublisher;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly ILogger<WeeklyReportWorker> _logger;
 
-        public WeeklySnapShotLeaderBoardWorker(WeeklySnapShotLeaderBoardPublisher weeklySnapShotPublisher)
+        public WeeklyReportWorker(/*WeeklyReportPublisher weeklyReportPublisher, */IWebHostEnvironment webHostEnvironment, ILogger<WeeklyReportWorker> logger)
         {
-            _weeklySnapShotPublisher = weeklySnapShotPublisher;
+            //_weeklyReportPublisher = weeklyReportPublisher;
+            _webHostEnvironment = webHostEnvironment;
+            _logger = logger;
         }
 
         public async Task RunAsync()
         {
-            await _weeklySnapShotPublisher.Publish(CancellationToken.None);
+            if (_webHostEnvironment.IsProduction() || _webHostEnvironment.IsStaging())
+            {
+                _logger.LogWarning($"Call WeeklyReportWorker - {DateTime.UtcNow}");
+                _logger.LogWarning($"Call WeeklyReportWorker, Disable WeeklyReportPublisher - {DateTime.UtcNow}");
+                //await _weeklyReportPublisher.Publish(CancellationToken.None);
+            }
         }
     }
 }
