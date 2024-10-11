4d4638be367cc8f0e1457343c02c5d03b7b6305c
hqhai <quanghai@atlantic.edu.vn>
2024-10-11 18:17:12 +0700

    feature/fsel-3719: bổ sung migration

--- Services/Fsel.Identity/Fsel.Identity.Infrastructure/Migrations/UserDbContextModelSnapshot.cs
+++ Services/Fsel.Identity/Fsel.Identity.Infrastructure/Migrations/UserDbContextModelSnapshot.cs
@@ -263,6 +263,8 @@ namespace Fsel.Identity.Infrastructure.Migrations
 
                     b.HasKey("Id");
 
+                    b.HasIndex("ParentEventId");
+
                     b.ToTable("CompetitionEvents");
                 });
 
@@ -2415,6 +2417,16 @@ namespace Fsel.Identity.Infrastructure.Migrations
                     b.Navigation("Human");
                 });
 
+            modelBuilder.Entity("Fsel.Identity.Domain.Entities.CompetitionEvent", b =>
+                {
+                    b.HasOne("Fsel.Identity.Domain.Entities.CompetitionEvent", "CompetitionEventParent")
+                        .WithMany("CompetitionEvents")
+                        .HasForeignKey("ParentEventId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.Navigation("CompetitionEventParent");
+                });
+
             modelBuilder.Entity("Fsel.Identity.Domain.Entities.Human", b =>
                 {
                     b.HasOne("Fsel.Identity.Domain.Entities.User", "User")
@@ -2595,6 +2607,8 @@ namespace Fsel.Identity.Infrastructure.Migrations
 
             modelBuilder.Entity("Fsel.Identity.Domain.Entities.CompetitionEvent", b =>
                 {
+                    b.Navigation("CompetitionEvents");
+
                     b.Navigation("StudentCompetitionEvents");
                 });
 
