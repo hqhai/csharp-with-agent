608039db8dec0797f5cce5fa07a038717d1063eb
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2024-11-04 11:09:10 +0700

    feature/fsel-3797: Update decentralization AdminSchool

--- Services/Fsel.Identity/Fsel.Identity.Api/Controllers/Admin/UserController.cs
+++ Services/Fsel.Identity/Fsel.Identity.Api/Controllers/Admin/UserController.cs
@@ -3,6 +3,7 @@
 using System.Net;
 using Asp.Versioning;
 using Fsel.Common.ActionResults;
+using Fsel.Common.Attributes;
 using Fsel.Common.Constants;
 using Fsel.Core.Base.BaseModels;
 using Fsel.Identity.Application.Commands.AdminCmd;
@@ -21,7 +22,7 @@ namespace Fsel.Identity.Api.Controllers.Admin
     [ApiVersion(ApiSettings.APIVersion1i1)]
     [Route(Settings.APIDefaultRoute + "/admin/user")]
     [ApiController]
-    [Common.Attributes.Permission(role: nameof(EnumRole.Admin))]
+    [Permission]
     public class UserController : ControllerBase
     {
         private readonly IMediator _mediator;
@@ -34,10 +35,10 @@ namespace Fsel.Identity.Api.Controllers.Admin
         /// <summary>
         /// Create User
         /// </summary>
-
         [HttpPost("create-user")]
         [ProducesResponseType(typeof(MethodResult<UserModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(role: nameof(EnumRole.Admin))]
         public async Task<IActionResult> Create([FromBody] CreateUserCommand command)
         {
             MethodResult<UserModel> commandResult = await _mediator.Send(command).ConfigureAwait(false);
@@ -50,6 +51,7 @@ namespace Fsel.Identity.Api.Controllers.Admin
         [HttpPut("{id}")]
         [ProducesResponseType(typeof(MethodResult<UserModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(role: nameof(EnumRole.Admin))]
         public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateUserCommand command)
         {
             ArgumentNullException.ThrowIfNull(command);
@@ -64,6 +66,7 @@ namespace Fsel.Identity.Api.Controllers.Admin
         [HttpDelete("{id}")]
         [ProducesResponseType(typeof(MethodResult<bool>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(role: nameof(EnumRole.Admin))]
         public async Task<IActionResult> Delete([FromRoute] Guid id)
         {
             MethodResult<bool> commandResult = await _mediator.Send(new DeleteUserCommand { Id = id }).ConfigureAwait(false);
@@ -76,6 +79,7 @@ namespace Fsel.Identity.Api.Controllers.Admin
         [HttpGet("search-user")]
         [ProducesResponseType(typeof(MethodResult<PagingItemsModel<UserSearchModel>>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(role: nameof(EnumRole.Admin))]
         public async Task<IActionResult> SearchUser([FromQuery] SearchUserQuery query)
         {
             MethodResult<PagingItemsModel<UserSearchModel>> commandResult = await _mediator.Send(query).ConfigureAwait(false);
@@ -88,6 +92,7 @@ namespace Fsel.Identity.Api.Controllers.Admin
         [HttpGet("get-user/{id}")]
         [ProducesResponseType(typeof(MethodResult<UserProfileModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(role: nameof(EnumRole.Admin))]
         public async Task<IActionResult> Get([FromRoute] Guid id)
         {
             MethodResult<UserProfileModel> commandResult = await _mediator.Send(new GetUserQuery { UserId = id }).ConfigureAwait(false);
@@ -100,6 +105,7 @@ namespace Fsel.Identity.Api.Controllers.Admin
         [HttpPost("active-user")]
         [ProducesResponseType(typeof(MethodResult<UserModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(role: nameof(EnumRole.Admin))]
         public async Task<IActionResult> ActiveUser([FromBody] UpdateStatusUserCommand command)
         {
             MethodResult<UserModel> commandResult = await _mediator.Send(command).ConfigureAwait(false);
@@ -112,6 +118,7 @@ namespace Fsel.Identity.Api.Controllers.Admin
         [HttpGet("request-teacher-bank")]
         [ProducesResponseType(typeof(MethodResult<UserModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(role: nameof(EnumRole.Admin))]
         public async Task<IActionResult> RequestUpdateTeacherBank([FromQuery] ApproveTeacherBankCommand command)
         {
             MethodResult<UserModel> commandResult = await _mediator.Send(command).ConfigureAwait(false);
@@ -121,6 +128,7 @@ namespace Fsel.Identity.Api.Controllers.Admin
         [HttpGet("token/{id}")]
         [ProducesResponseType(typeof(MethodResult<TokenModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(roles: new string[] { nameof(EnumRole.Admin), nameof(EnumRole.AdminSchool) })]
         public async Task<IActionResult> GetJWT([FromRoute] Guid id)
         {
             MethodResult<TokenModel> queryResult = await _mediator.Send(new GenerateTokenCommand { Id = id }).ConfigureAwait(false);
@@ -133,6 +141,7 @@ namespace Fsel.Identity.Api.Controllers.Admin
         [HttpPost("create-student")]
         [ProducesResponseType(typeof(MethodResult<UserModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(role: nameof(EnumRole.Admin))]
         public async Task<IActionResult> CreateStudent([FromBody] CreateUserStudentToAdminCommand command)
         {
             MethodResult<UserModel> commandResult = await _mediator.Send(command).ConfigureAwait(false);
@@ -145,6 +154,7 @@ namespace Fsel.Identity.Api.Controllers.Admin
         [HttpPost("create-students")]
         [ProducesResponseType(typeof(MethodResult<Stream>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(role: nameof(EnumRole.Admin))]
         public async Task<IActionResult> CreateStudents([FromQuery] CreateUserStudentsToAdminCommand command)
         {
             MethodResult<Stream> commandResult = await _mediator.Send(command).ConfigureAwait(false);
@@ -161,6 +171,7 @@ namespace Fsel.Identity.Api.Controllers.Admin
         [HttpPost("export-template-create-students")]
         [ProducesResponseType(typeof(MethodResult<Stream>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(role: nameof(EnumRole.Admin))]
         public async Task<IActionResult> ExportTemplate()
         {
             MethodResult<Stream> commandResult = await _mediator.Send(new ExportTemplateCreateStudentCommand()).ConfigureAwait(false);
@@ -177,6 +188,7 @@ namespace Fsel.Identity.Api.Controllers.Admin
         [HttpGet("search-referral-code")]
         [ProducesResponseType(typeof(MethodResult<PagingItemsModel<SearchReferralCodeModel>>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(role: nameof(EnumRole.Admin))]
         public async Task<IActionResult> SearchUserReferral([FromQuery] SearchReferralCodeQuery query)
         {
             var commandResult = await _mediator.Send(query).ConfigureAwait(false);
@@ -189,6 +201,7 @@ namespace Fsel.Identity.Api.Controllers.Admin
         [HttpGet("search-detail-referral-code")]
         [ProducesResponseType(typeof(MethodResult<PagingItemsModel<SearchDetailReferralCodeModel>>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(role: nameof(EnumRole.Admin))]
         public async Task<IActionResult> SearchDetailUserReferral([FromQuery] SearchDetailReferralCodeQuery query)
         {
             var commandResult = await _mediator.Send(query).ConfigureAwait(false);
@@ -201,6 +214,7 @@ namespace Fsel.Identity.Api.Controllers.Admin
         [HttpPost("export-template-admin-school")]
         [ProducesResponseType(typeof(MethodResult<Stream>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(role: nameof(EnumRole.Admin))]
         public async Task<IActionResult> Export([FromForm] ExportTemplateCreateAdminSchoolCommand command)
         {
             MethodResult<Stream> commandResult = await _mediator.Send(command).ConfigureAwait(false);
@@ -217,6 +231,7 @@ namespace Fsel.Identity.Api.Controllers.Admin
         [HttpPost("import-admin-school")]
         [ProducesResponseType(typeof(MethodResult<Stream>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(role: nameof(EnumRole.Admin))]
         public async Task<IActionResult> Import([FromForm] ImportFileAdminSchoolsCommand command)
         {
             MethodResult<Stream> commandResult = await _mediator.Send(command).ConfigureAwait(false);
