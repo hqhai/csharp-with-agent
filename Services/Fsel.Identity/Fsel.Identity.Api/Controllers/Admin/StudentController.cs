e7c9e64207ad6b3697a2cda865c8646d6468062e
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2024-11-20 10:12:27 +0700

    feature/fsel-3974: Update Role StudentController

--- Services/Fsel.Identity/Fsel.Identity.Api/Controllers/Admin/StudentController.cs
+++ Services/Fsel.Identity/Fsel.Identity.Api/Controllers/Admin/StudentController.cs
@@ -22,7 +22,7 @@ namespace Fsel.Identity.Api.Controllers.Admin
     [ApiVersion(ApiSettings.APIVersion1)]
     [ApiVersion(ApiSettings.APIVersion1i1)]
     [Route(Settings.APIDefaultRoute + "/admin/student")]
-    //[Common.Attributes.Permission(roles: new string[] { nameof(EnumRole.Admin), nameof(EnumRole.AdminSchool) })]
+    [Common.Attributes.Permission(roles: new string[] { nameof(EnumRole.Admin), nameof(EnumRole.AdminSchool) })]
     [ApiController]
     public class StudentController : ControllerBase
     {
