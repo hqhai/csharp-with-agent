e3b4a2846ac5070ffa8d0227167df7cce77c16c2
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2024-11-26 13:35:39 +0700

    feature/fsel-3741: fixbug change GetUserByStudentId to GetStudentByUserIdWithCacheAsync

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/MockTestCmd/V1i1/CreateMockTestAnswerBySectionGroupCommand.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/MockTestCmd/V1i1/CreateMockTestAnswerBySectionGroupCommand.cs
@@ -112,7 +112,7 @@ namespace Fsel.Course.Lms.Application.Commands.MockTestCmd.V1i1
             StudentModel? student;
             if (request.StudentId.HasValue)
             {
-                var studentResult = await _userService.GetStudentByUserIdWithCacheAsync(request.StudentId.Value);
+                var studentResult = await _userService.GetUserByStudentId(request.StudentId.Value);
                 if (!studentResult.IsSuccessStatusCode)
                 {
                     methodResult.AddErrorBadRequest(nameof(EnumServicesErrorCode.CallUserServiceError), nameof(studentResult));
@@ -122,7 +122,7 @@ namespace Fsel.Course.Lms.Application.Commands.MockTestCmd.V1i1
             }
             else
             {
-                var studentResult = await _userService.GetStudentByUserIdAsync(_authContext.CurrentUserId);
+                var studentResult = await _userService.GetStudentByUserIdWithCacheAsync(_authContext.CurrentUserId);
                 if (!studentResult.IsSuccessStatusCode)
                 {
                     methodResult.AddErrorBadRequest(nameof(EnumServicesErrorCode.CallUserServiceError), nameof(studentResult));
