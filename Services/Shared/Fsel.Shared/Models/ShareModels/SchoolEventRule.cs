60bd22c10a4fe0c32b20acf35ffe7af446c4cee2
Minh Chiến Lê Xuân <minhchien@atlantic.edu.vn>
2024-12-11 18:26:41 +0700

    feature-testing/fsel-3768: sửa model SchoolEventRule

--- Services/Shared/Fsel.Shared/Models/ShareModels/SchoolEventRule.cs
+++ Services/Shared/Fsel.Shared/Models/ShareModels/SchoolEventRule.cs
@@ -139,12 +139,12 @@ namespace Fsel.Shared.Models.ShareModels
     {
         public string? Title { get; set; }
         public IList<WeekRule>? Rules { get; set; }
-        public IList<WeekRule>? ChildrenRules { get; set; }
         public int PrizeCount { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public int WeekNumber { get; set; }
         public string? Rule { get; set; }
+        public IList<CollectiveAward>? CollectiveAwards { get; set; }
     }
 
     public class WeekRule
@@ -154,6 +154,12 @@ namespace Fsel.Shared.Models.ShareModels
         public string? Detail { get; set; }
         public string? RewardImagine { get; set; }
         public int Rank { get; set; }
+    }
+
+    public class CollectiveAward
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
     }
 
     public class LearningNotice
