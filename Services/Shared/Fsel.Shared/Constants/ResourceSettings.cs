832cc539b7954f20213ff01a63d64ba36bf5101a
hqhai <quanghai@atlantic.edu.vn>
2024-12-19 15:19:25 +0700

    feat/fsel-4320: add new tool for auto approval class forum post

--- Services/Shared/Fsel.Shared/Constants/ResourceSettings.cs
+++ Services/Shared/Fsel.Shared/Constants/ResourceSettings.cs
@@ -72,6 +72,10 @@ namespace Fsel.Shared.Constants
         public const string SpeakingFluency = "Resources//SpeakingAiConfig//FluencyConfig.txt";
         public const string SpeakingLexical = "Resources//SpeakingAiConfig//LexicalResourceConfig.txt";
 
+
+        public const string AIClassForumRole = "Resources//AIPrompt//Role//ApprovalClassForumRole.txt";
+        public const string AIClassForumInstruction = "Resources//AIPrompt//Instruction//ApprovalClassForumInstruction.txt";
+
         public const string IeltsStudentResult = "Resources//IeltsStudentsResult.json";
         public const string AcademicStudentsResult = "Resources//AcademicStudentsResult.json";
         public const string SchoolEventRules = "Resources//SchoolEventRules.json";
