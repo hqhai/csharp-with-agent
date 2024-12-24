8d75131018fe1d80d655510bd7b09eb74d828423
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2024-12-24 13:33:23 +0700

    feature-staging/fsel-4256: update CheckVoucherCommand

--- Services/Fsel.Ordering/Fsel.Ordering.Application/Commands/VoucherCmds/CheckVoucherCommand.cs
+++ Services/Fsel.Ordering/Fsel.Ordering.Application/Commands/VoucherCmds/CheckVoucherCommand.cs
@@ -118,10 +118,13 @@ namespace Fsel.Ordering.Application.Commands.VoucherCmds
 
             if (voucher.Source == EnumVoucherSource.Auto)
             {
-                var vouchersAuto = _voucherRepository.Queryable.Where(p => p.CodePrefix == voucher.CodePrefix).ToList();
+                var vouchersAuto = await _voucherRepository.Queryable.Where(p => p.CodePrefix == voucher.CodePrefix).ToListAsync(cancellationToken);
+
                 var voucherIds = vouchersAuto.Select(p => p.Id).ToList();
-                var orders = _orderRepository.Queryable.Where(p => p.UserId == _authContext.CurrentUserId && p.VoucherId.HasValue && voucherIds.Contains(p.VoucherId ?? default)).ToList();
-                if (voucher.NumberOfChanges >= orders.Count)
+
+                var orders = await _orderRepository.Queryable.Where(p => p.UserId == _authContext.CurrentUserId && p.VoucherId.HasValue && voucherIds.Contains(p.VoucherId.Value) && p.Status == EnumOrderStatus.Payment).ToListAsync(cancellationToken);
+
+                if (orders.Count >= voucher.NumberOfChanges)
                 {
                     methodResult.AddErrorBadRequest(nameof(EnumVoucherErrorCode.TheNumberOfUsesHasExpired));
                     return methodResult;
