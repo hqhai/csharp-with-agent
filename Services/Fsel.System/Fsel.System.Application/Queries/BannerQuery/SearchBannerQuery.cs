0a7e62b0c2735bd65d4805d0aafb628ef34b4c35
Minh Chiến Lê Xuân <minhchien@atlantic.edu.vn>
2025-01-03 13:30:09 +0700

    feature/fsel-4032: update seach contains

--- Services/Fsel.System/Fsel.System.Application/Queries/BannerQuery/SearchBannerQuery.cs
+++ Services/Fsel.System/Fsel.System.Application/Queries/BannerQuery/SearchBannerQuery.cs
@@ -40,51 +40,51 @@ namespace Fsel.System.Application.Queries.BannerQuery
                 return methodResult;
             }
 
-            var query = await _bannerRepository.Queryable
-                                               .Include(x => x.BannerScopes)
-                                               .Select(x => new BannerModel
-                                               {
-                                                   Id = x.Id,
-                                                   Name = x.Name,
-                                                   Code = x.Code,
-                                                   CreatedDate = x.CreatedDate,
-                                                   CreatedFullName = x.CreatedFullName,
-                                                   CreatedUserId = x.CreatedUserId,
-                                                   Content = x.Content,
-                                                   FilePath = x.FilePath,
-                                                   StartDate = x.StartDate.ConvertTimeFromUtc(EnumCountryKey.Vietnam),
-                                                   EndDate = x.EndDate.ConvertTimeFromUtc(EnumCountryKey.Vietnam),
-                                                   Type = x.Type,
-                                                   UpdatedDate = x.UpdatedDate,
-                                                   UpdatedFullName = x.UpdatedFullName,
-                                                   UpdatedUserId = x.UpdatedUserId,
-                                                   Url = x.Url,
-                                                   BannerFrequency = x.BannerFrequency,
-                                                   DisplayStartDate = x.DisplayStartDate,
-                                                   DisplayEndDate = x.DisplayEndDate,
-                                                   DisplayStartTime = x.DisplayStartTime,
-                                                   DisplayEndTime = x.DisplayEndTime,
-                                                   Status = x.Status,
-                                                   BannerScopes = _mapper.Map<IList<BannerScopeModel>>(x.BannerScopes)
-                                               }).ToListAsync(cancellationToken);
+            var query = _bannerRepository.Queryable
+                                         .Include(x => x.BannerScopes)
+                                         .Select(x => new BannerModel
+                                         {
+                                             Id = x.Id,
+                                             Name = x.Name,
+                                             Code = x.Code,
+                                             CreatedDate = x.CreatedDate,
+                                             CreatedFullName = x.CreatedFullName,
+                                             CreatedUserId = x.CreatedUserId,
+                                             Content = x.Content,
+                                             FilePath = x.FilePath,
+                                             StartDate = x.StartDate.ConvertTimeFromUtc(EnumCountryKey.Vietnam),
+                                             EndDate = x.EndDate.ConvertTimeFromUtc(EnumCountryKey.Vietnam),
+                                             Type = x.Type,
+                                             UpdatedDate = x.UpdatedDate,
+                                             UpdatedFullName = x.UpdatedFullName,
+                                             UpdatedUserId = x.UpdatedUserId,
+                                             Url = x.Url,
+                                             BannerFrequency = x.BannerFrequency,
+                                             DisplayStartDate = x.DisplayStartDate,
+                                             DisplayEndDate = x.DisplayEndDate,
+                                             DisplayStartTime = x.DisplayStartTime,
+                                             DisplayEndTime = x.DisplayEndTime,
+                                             Status = x.Status,
+                                             BannerScopes = _mapper.Map<IList<BannerScopeModel>>(x.BannerScopes)
+                                         });
 
             if (!string.IsNullOrEmpty(request.Keyword))
             {
-                query = query.Where(x => (x.Name != null && x.Name.ToLower().Trim() == request.Keyword.ToLower().Trim()) || (x.Code != null && x.Code.ToLower().Trim() == request.Keyword.ToLower().Trim())).ToList();
+                query = query.Where(x => (x.Name != null && x.Name.ToLower().Trim().Contains(request.Keyword.ToLower().Trim())) || (x.Code != null && x.Code.ToLower().Trim().Contains(request.Keyword.ToLower().Trim())));
             }
 
             if (request.ListCourseLevels != null && request.ListCourseLevels.Any())
             {
-                query = query.Where(x => x.BannerScopes != null && x.BannerScopes.Any(c => request.ListCourseLevels.Contains(c.CourseLevel))).ToList();
+                query = query.Where(x => x.BannerScopes != null && x.BannerScopes.Any(c => request.ListCourseLevels.Contains(c.CourseLevel)));
             }
 
             if (request.StartDate.HasValue && request.EndDate.HasValue)
             {
-                query = query.Where(x => request.StartDate.Value.Date <= x.EndDate.Date && request.EndDate.Value.Date >= x.StartDate.Date).ToList();
+                query = query.Where(x => request.StartDate.Value.Date <= x.EndDate.Date && request.EndDate.Value.Date >= x.StartDate.Date);
             }
 
-            int totalItem = query.Count;
-            var lists = query.ApplySortAndPaging(request).ToList();
+            int totalItem = await query.CountAsync(cancellationToken);
+            var lists = await query.ApplySortAndPaging(request).ToListAsync(cancellationToken);
 
             methodResult.Result = new PagingItemsModel<BannerModel>(lists, request, totalItem);
             methodResult.StatusCode = StatusCodes.Status200OK;
