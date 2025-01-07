ac32eec166b88b88458e48abd1434133ab7c7b90
Khouse169 <89691794+Khouse169@users.noreply.github.com>
2025-01-07 18:01:04 +0700

    feature/fsel-4069: tool update video time code

--- Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/ClassForumToolController.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/ClassForumToolController.cs
@@ -12,6 +12,7 @@ namespace Fsel.Course.Lms.Api.Controllers
     using Fsel.Shared.Constants;
     using Fsel.Shared.Attributes;
     using Fsel.Course.Lms.Application.Commands.AiCmd;
+    using Fsel.Course.Lms.Application.Commands.VideoTimeCodeAnswerCmd;
 
     [ApiVersions(ApiSettings.APIVersion1)]
     [Route(Settings.APIDefaultRoute + "/class-forum-tool")]
@@ -33,6 +34,32 @@ namespace Fsel.Course.Lms.Api.Controllers
         [ProducesResponseType(typeof(MethodResult<bool>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
         public async Task<IActionResult> Retry([FromBody] AutoSubmitClassForumCommand command)
+        {
+            ArgumentNullException.ThrowIfNull(command);
+            MethodResult<bool> commandResult = await _mediator.Send(command).ConfigureAwait(false);
+            return commandResult.GetActionResult();
+        }
+
+        /// <summary>
+        /// Update video time code
+        /// </summary>
+        [HttpPut("tool-update-video-time-code")]
+        [ProducesResponseType(typeof(MethodResult<bool>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> ToolUpdateVideoTimeCode(ToolUpdateVideoTimeCodeCommand command)
+        {
+            ArgumentNullException.ThrowIfNull(command);
+            MethodResult<bool> commandResult = await _mediator.Send(command).ConfigureAwait(false);
+            return commandResult.GetActionResult();
+        }
+
+        /// <summary>
+        /// Update video time code
+        /// </summary>
+        [HttpPut("tool-update-video-result")]
+        [ProducesResponseType(typeof(MethodResult<bool>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> ToolUpdateVideoResultCommand(ToolUpdateVideoResultCommand command)
         {
             ArgumentNullException.ThrowIfNull(command);
             MethodResult<bool> commandResult = await _mediator.Send(command).ConfigureAwait(false);
