71eeb455a4a7cba5f3051b3e4e461c3c6289f168
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2025-01-17 03:07:01 +0700

    feature/fsel-4475: Update CourseResult Repository

--- Services/Fsel.Course/Fsel.Course.Infrastructure/Repositories/CourseResultRepository.cs
+++ Services/Fsel.Course/Fsel.Course.Infrastructure/Repositories/CourseResultRepository.cs
@@ -2,15 +2,9 @@
 
 namespace Fsel.Course.Infrastructure.Repositories
 {
-    using System.Collections.Generic;
-    using System.Linq;
     using Fsel.Core.Base;
     using Fsel.Course.Domain.Entities;
-    using Fsel.Course.Domain.Enums;
     using Fsel.Course.Domain.IRepositories;
-    using Fsel.Course.Domain.Models.EntityModels;
-    using Fsel.Shared.Enums;
-    using Microsoft.EntityFrameworkCore;
 
     public class CourseResultRepository : BaseRepository<CourseResult>, ICourseResultRepository
     {
