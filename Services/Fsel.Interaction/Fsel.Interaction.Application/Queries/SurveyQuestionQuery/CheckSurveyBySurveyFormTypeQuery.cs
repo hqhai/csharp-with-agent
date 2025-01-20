59eff21c0b78137d72b1ac094b8ef3d1bfaeaed4
hqhai <quanghai@atlantic.edu.vn>
2025-01-20 19:15:43 +0700

    update/fsel-4441: Cập nhật thông tin lấy dữ liệu parent event id theo child id

--- Services/Fsel.Interaction/Fsel.Interaction.Application/Queries/SurveyQuestionQuery/CheckSurveyBySurveyFormTypeQuery.cs
+++ Services/Fsel.Interaction/Fsel.Interaction.Application/Queries/SurveyQuestionQuery/CheckSurveyBySurveyFormTypeQuery.cs
@@ -6,6 +6,7 @@ namespace Fsel.Interaction.Application.Queries.SurveyQuestionQuery
     using System.Threading.Tasks;
     using Fsel.Common.ActionResults;
     using Fsel.Core.Base;
+    using Fsel.Interaction.Application.Services.UserServices;
     using Fsel.Interaction.Domain.IRepositories;
     using Fsel.Shared.Enums;
     using MediatR;
@@ -22,15 +23,18 @@ namespace Fsel.Interaction.Application.Queries.SurveyQuestionQuery
     {
         private readonly ICustomerSurveyGroupRepository _customerSurveyGroupRepository;
         private readonly ISurveyQuestionRepository _surveyQuestionRepository;
+        private readonly IUserService _userService;
         private readonly AuthContext _authContext;
 
         public CheckSurveyBySurveyFormTypeQueryHandler(ICustomerSurveyGroupRepository customerSurveyGroupRepository,
                                                        ISurveyQuestionRepository surveyQuestionRepository,
-                                                       AuthContext authContext)
+                                                       AuthContext authContext,
+                                                       IUserService userService)
         {
             _customerSurveyGroupRepository = customerSurveyGroupRepository;
             _surveyQuestionRepository = surveyQuestionRepository;
             _authContext = authContext;
+            _userService = userService;
         }
 
         public async Task<MethodResult<bool>> Handle(CheckSurveyBySurveyFormTypeQuery request, CancellationToken cancellationToken)
@@ -38,10 +42,15 @@ namespace Fsel.Interaction.Application.Queries.SurveyQuestionQuery
             ArgumentNullException.ThrowIfNull(request);
             MethodResult<bool> methodResult = new MethodResult<bool>();
 
+
+            var parentEventResult = await _userService.GetParentEventId(request.CompetitionEventId);
+            var eventId = parentEventResult?.Content?.Result;
+
+
             var surveyQuestions = await _surveyQuestionRepository.Queryable.Where(x => x.IsPilot == false && x.SurveyFormType == request.SurveyFormType).ToListAsync(cancellationToken);
-            if (request.SurveyFormType == EnumSurveyFormType.Event && request.CompetitionEventId.HasValue)
+            if (request.SurveyFormType == EnumSurveyFormType.Event && eventId.HasValue)
             {
-                surveyQuestions = surveyQuestions.Where(x => x.CompetitionEventId == request.CompetitionEventId).ToList();
+                surveyQuestions = surveyQuestions.Where(x => x.CompetitionEventId == eventId).ToList();
             }
 
             if (!surveyQuestions.Any())
@@ -50,7 +59,7 @@ namespace Fsel.Interaction.Application.Queries.SurveyQuestionQuery
                 return methodResult;
             }
 
-            if (request.SurveyFormType == EnumSurveyFormType.Event && !request.CompetitionEventId.HasValue)
+            if (request.SurveyFormType == EnumSurveyFormType.Event && !eventId.HasValue)
             {
                 methodResult.Result = false;
                 return methodResult;
@@ -60,9 +69,9 @@ namespace Fsel.Interaction.Application.Queries.SurveyQuestionQuery
                                                                            .Where(x => x.SurveyFormType == request.SurveyFormType && x.UserId == _authContext.CurrentUserId)
                                                                            .ToListAsync(cancellationToken);
 
-            if (request.SurveyFormType == EnumSurveyFormType.Event && request.CompetitionEventId.HasValue)
+            if (request.SurveyFormType == EnumSurveyFormType.Event && eventId.HasValue)
             {
-                customerSurveyGroups = customerSurveyGroups.Where(x => x.CompetitionEventId == request.CompetitionEventId).ToList();
+                customerSurveyGroups = customerSurveyGroups.Where(x => x.CompetitionEventId == eventId).ToList();
             }
 
             if (customerSurveyGroups == null || !customerSurveyGroups.Any() || customerSurveyGroups.Any(x => x.Status == EnumSurveyGroupStatus.Process))
@@ -73,5 +82,13 @@ namespace Fsel.Interaction.Application.Queries.SurveyQuestionQuery
 
             return methodResult;
         }
+
+        public async Task<Guid> CheckParent(Guid? competitionEventId)
+        {
+
+
+            return Guid.Empty;
+        }
+
     }
 }
--- Services/Fsel.Interaction/Fsel.Interaction.Application/Services/UserServices/IUserService.cs
+++ Services/Fsel.Interaction/Fsel.Interaction.Application/Services/UserServices/IUserService.cs
@@ -36,5 +36,8 @@ namespace Fsel.Interaction.Application.Services.UserServices
         [Get("/v1/student-ranking/get-events-by-user-id")]
         Task<IApiResponse<MethodResult<IList<CompetitionEventsModel>>>> GetEventByUserId([Query] Guid? userId);
 
+        [Get("/v1/event/get-event-parent/{id}")]
+        Task<IApiResponse<MethodResult<Guid>>> GetParentEventId([FromRoute] Guid? id);
+
     }
 }
