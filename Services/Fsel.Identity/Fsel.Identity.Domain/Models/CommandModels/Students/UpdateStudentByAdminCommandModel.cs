343d77d6703504fbd56bbf71d89d8535fdc8b50e
Khouse169 <89691794+Khouse169@users.noreply.github.com>
2025-02-10 11:26:54 +0700

    feature/FSEL-4614: update soft delete student for admin

--- Services/Fsel.Identity/Fsel.Identity.Domain/Models/CommandModels/Students/UpdateStudentByAdminCommandModel.cs
+++ Services/Fsel.Identity/Fsel.Identity.Domain/Models/CommandModels/Students/UpdateStudentByAdminCommandModel.cs
@@ -14,6 +14,8 @@ namespace Fsel.Identity.Domain.Models.CommandModels.Students
         public string? Occupation { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
+        public string? SchoolGrade { get; set; }
+        public string? SchoolClass { get; set; }
         public UpdateParentCommandModel? Parent { get; set; }
     }
 }
