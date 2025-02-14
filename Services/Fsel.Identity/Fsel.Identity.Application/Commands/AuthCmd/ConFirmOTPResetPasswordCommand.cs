695a8703e830b9f84890e2e7cf0160e3b69f963b
Minh Chiến Lê Xuân <minhchien@atlantic.edu.vn>
2025-02-14 16:22:42 +0700

    fixbug/fsel-4654: check cofirm phone number

--- Services/Fsel.Identity/Fsel.Identity.Application/Commands/AuthCmd/ConFirmOTPResetPasswordCommand.cs
+++ Services/Fsel.Identity/Fsel.Identity.Application/Commands/AuthCmd/ConFirmOTPResetPasswordCommand.cs
@@ -72,7 +72,14 @@ namespace Fsel.Identity.Application.Commands.AuthCmd
 
             var hashPassword = _userManager.PasswordHasher.HashPassword(user, request.NewPassword);
             user.PasswordHash = hashPassword;
-            user.EmailConfirmed = true;
+            if (!string.IsNullOrEmpty(request.PhoneNumber))
+            {
+                user.PhoneNumberConfirmed = true;
+            }
+            else
+            {
+                user.EmailConfirmed = true;
+            }
             await _userManager.UpdateAsync(user);
 
             methodResult.StatusCode = StatusCodes.Status200OK;
