b0b5325bf3a215ebb0ece23151799cc8f4909c28
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2025-02-14 20:42:00 +0700

    feature/fsel-4439: update validate send otp for user ha noi

--- Services/Fsel.Identity/Fsel.Identity.Application/Commands/UserOtpCodeCmd/SaveOTPForUserEventHaNoiCommand.cs
+++ Services/Fsel.Identity/Fsel.Identity.Application/Commands/UserOtpCodeCmd/SaveOTPForUserEventHaNoiCommand.cs
@@ -87,11 +87,11 @@ namespace Fsel.Identity.Application.Commands.UserOtpCodeCmd
                 return methodResult;
             }
 
-            if (lastOTP != null && IsValidTime(lastOTP.UpdatedDate ?? lastOTP.CreatedDate, DateTime.UtcNow))
-            {
-                methodResult.AddErrorBadRequest(nameof(EnumOTPCodeErrorCode.SentWithin30Seconds));
-                return methodResult;
-            }
+            //if (lastOTP != null && IsValidTime(lastOTP.UpdatedDate ?? lastOTP.CreatedDate, DateTime.UtcNow))
+            //{
+            //    methodResult.AddErrorBadRequest(nameof(EnumOTPCodeErrorCode.SentWithin30Seconds));
+            //    return methodResult;
+            //}
 
             var studentId = user.Human?.Student?.Id;
 
@@ -165,11 +165,11 @@ namespace Fsel.Identity.Application.Commands.UserOtpCodeCmd
             return methodResult;
         }
 
-        public bool IsValidTime(DateTime dateTime1, DateTime dateTime2)
-        {
-            TimeSpan difference = dateTime2 - dateTime1;
+        //public bool IsValidTime(DateTime dateTime1, DateTime dateTime2)
+        //{
+        //    TimeSpan difference = dateTime2 - dateTime1;
 
-            return difference.TotalSeconds < 30;
-        }
+        //    return difference.TotalSeconds < 30;
+        //}
     }
 }
