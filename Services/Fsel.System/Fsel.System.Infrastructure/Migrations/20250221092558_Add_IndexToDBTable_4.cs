d3dbb56b64b831dc96e13044f176185880fcb142
Nguyễn Thành Đô <thanhdo@atlantic.edu.vn>
2025-02-21 16:26:02 +0700

    feature/fsel-3742: add migration index

---
+++ Services/Fsel.System/Fsel.System.Infrastructure/Migrations/20250221092558_Add_IndexToDBTable_4.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fsel.System.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_IndexToDBTable_4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TokenHistories_IsDeleted_UserId_CreatedDate",
+                table: "TokenHistories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TokenHistories_IsDeleted_UserId_Type",
+                table: "TokenHistories");
+
+            migrationBuilder.DropColumn(
+                name: "CourseLevel",
+                table: "CourseSuggestConfigs");
+
+            migrationBuilder.DropColumn(
+                name: "CourseType",
+                table: "CourseSuggestConfigs");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CourseLevelStr",
+                table: "CourseSuggestConfigs",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TokenHistories_IsDeleted_UserId",
+                table: "TokenHistories",
+                columns: new[] { "IsDeleted", "UserId" })
+                .Annotation("SqlServer:Include", new[] { "VolatileToken", "Type" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TokenHistories_IsDeleted_UserId_CreatedDate",
+                table: "TokenHistories",
+                columns: new[] { "IsDeleted", "UserId", "CreatedDate" })
+                .Annotation("SqlServer:Include", new[] { "ConfigDataStr", "ConfigStr", "CourseResultId", "CreatedFullName", "CreatedUserId", "DeletedDate", "DeletedFullName", "DeletedUserId", "Feature", "InitialToken", "Mission", "ObjectId", "RemainToken", "TokenConfigId", "Type", "UpdatedDate", "UpdatedFullName", "UpdatedUserId", "VolatileToken" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TokenHistories_IsDeleted_UserId_Type",
+                table: "TokenHistories",
+                columns: new[] { "IsDeleted", "UserId", "Type" })
+                .Annotation("SqlServer:Include", new[] { "ConfigDataStr", "ConfigStr", "CourseResultId", "CreatedDate", "CreatedFullName", "CreatedUserId", "DeletedDate", "DeletedFullName", "DeletedUserId", "Feature", "InitialToken", "Mission", "ObjectId", "RemainToken", "TokenConfigId", "UpdatedDate", "UpdatedFullName", "UpdatedUserId", "VolatileToken" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BannerStudents_IsDeleted_StudentId_CreatedDate",
+                table: "BannerStudents",
+                columns: new[] { "IsDeleted", "StudentId", "CreatedDate" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TokenHistories_IsDeleted_UserId",
+                table: "TokenHistories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TokenHistories_IsDeleted_UserId_CreatedDate",
+                table: "TokenHistories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TokenHistories_IsDeleted_UserId_Type",
+                table: "TokenHistories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BannerStudents_IsDeleted_StudentId_CreatedDate",
+                table: "BannerStudents");
+
+            migrationBuilder.DropColumn(
+                name: "CourseLevelStr",
+                table: "CourseSuggestConfigs");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CourseLevel",
+                table: "CourseSuggestConfigs",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CourseType",
+                table: "CourseSuggestConfigs",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TokenHistories_IsDeleted_UserId_CreatedDate",
+                table: "TokenHistories",
+                columns: new[] { "IsDeleted", "UserId", "CreatedDate" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TokenHistories_IsDeleted_UserId_Type",
+                table: "TokenHistories",
+                columns: new[] { "IsDeleted", "UserId", "Type" });
+        }
+    }
+}
