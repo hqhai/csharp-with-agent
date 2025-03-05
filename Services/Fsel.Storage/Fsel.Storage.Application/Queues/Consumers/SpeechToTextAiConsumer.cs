789f93a0445578f384d04a23a8e96745956c24b2
hqhai <quanghai@atlantic.edu.vn>
2025-03-05 17:34:07 +0700

    update/fsel-4687: Bố sung Timout với open ai

--- Services/Fsel.Storage/Fsel.Storage.Application/Queues/Consumers/SpeechToTextAiConsumer.cs
+++ Services/Fsel.Storage/Fsel.Storage.Application/Queues/Consumers/SpeechToTextAiConsumer.cs
@@ -15,22 +15,22 @@ namespace Fsel.Storage.Application.Queues.Consumers
         private readonly IMediator _mediator;
         private readonly IServiceScopeFactory _serviceScopeFactory;
         private readonly ILogger<SpeechToTextAiConsumer> _logger;
-
+        private readonly AppSetting _appSetting;
         public SpeechToTextAiConsumer(IMediator mediator, AuthContext authContext, Microsoft.AspNetCore.Http.IHttpContextAccessor httpContextAccessor, IServiceScopeFactory serviceScopeFactory, ILogger<SpeechToTextAiConsumer> logger) : base(authContext, httpContextAccessor)
         {
             _mediator = mediator;
             _serviceScopeFactory = serviceScopeFactory;
             _logger = logger;
         }
-        public async override Task ConsumeQueue(SpeechToTextAiConsumerModel? message)
+                                      IOpenAIService openAIService,
+                                      IAmazonS3Service amazonS3Service,
+                                      SpeechToTextPublisher speechToTextPublisher,
+                                      AppSetting appSetting,
+                                      ILogger<SpeechToTextAiConsumer> logger,
+                                      ICognitiveProvider cognitiveProvider) : base(authContext, httpContextAccessor)
         {
-            if (message == null)
-            {
-                return;
-            }
-
             _ = Task.Run(async () =>
-            {
+            _amazonS3Service = amazonS3Service;
                 using var scope = _serviceScopeFactory.CreateScope();
                 var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
 
@@ -46,6 +46,86 @@ namespace Fsel.Storage.Application.Queues.Consumers
 
             _logger.LogCritical("Processing Out Of Task Run");
 
+                return;
+            }
+
+            IFormFile formFile = ConvertToIFormFile(message.FileData, message.FileName, message.ContentType);
+            if (formFile == null)
+            {
+                return;
+            }
+
+            int countRetry = 0;
+
+            var retryAI = Policy.HandleResult<UserAiModel>(result => !result.CheckSubAI)
+                                .WaitAndRetryAsync(Max_Time_Retry, retryAttempt => TimeSpan.FromSeconds(5));
+
+            var retryResult = await retryAI.ExecuteAsync(async () =>
+            {
+                _logger.LogError($"CountRetry: {message.UserId} count: {countRetry += 1}");
+
+                var stream = formFile.OpenReadStream();
+                var streamPart = new StreamPart(stream, formFile.FileName, formFile.ContentType);
+
+                var startDate = DateTime.UtcNow;
+
+                var content = await _openAIService.SpeechToTextByAIAsync(streamPart, _appSetting.OpenAiConfig?.ApprovalAIModel);
+
+                var endDate = DateTime.UtcNow;
+
+                _logger.LogError($"CountTimeResponseAI: {(endDate - startDate).TotalSeconds}");
+                _logger.LogError($"LogContentAI: {content.Content}");
+
+                if (!content.IsSuccessStatusCode)
+                {
+                    var deepGramContent = await _cognitiveProvider.GetTranscriptionAsync(formFile);
+
+                    _logger.LogError($"LogContentDeepGramAI: {deepGramContent}");
+
+                    if (string.IsNullOrEmpty(deepGramContent))
+                    {
+                        return new UserAiModel { CheckSubAI = false };
+                    }
+                    var fileInfomationDeepGram = await UpLoadFileAsync(formFile);
+                    await PublishTextToSocket(message, deepGramContent, fileInfomationDeepGram.Result);
+                }
+
+                // var fileInfomation = await _amazonS3Service.UploadFileAsync(EnumBucketType.FselPublic, formFile, EnumFolderType.Videos, false, false);
+                var fileInfomation = await UpLoadFileAsync(formFile);
+
+                var convertContent = !string.IsNullOrEmpty(content.Content) ? JsonConvert.DeserializeObject<ContentModel>(content.Content)?.Text : string.Empty;
+                if (string.IsNullOrEmpty(convertContent))
+                {
+                    return new UserAiModel { CheckSubAI = false };
+                }
+
+                // publisher real time
+                //await _speechToTextPublisher.Publish(new SpeechToTextConsumerModel { UserId = message.UserId, TranscriptFile = new Shared.Models.ShareModels.TranscriptFileModel { Content = convertContent, FilePath = fileInfomation.Result } }, CancellationToken.None);
+
+                await PublishTextToSocket(message, convertContent, fileInfomation.Result);
+                return new UserAiModel { CheckSubAI = true };
+            });
+        }
+
+        private static IFormFile ConvertToIFormFile(byte[] fileData, string fileName, string contentType)
+        {
+            var stream = new MemoryStream(fileData);
+            return new FormFile(stream, 0, fileData.Length, "file", fileName)
+            {
+                Headers = new HeaderDictionary(),
+                ContentType = contentType
+            };
+        }
+
+        public async Task<MethodResult<string?>> UpLoadFileAsync(IFormFile formFile)
+        {
+            return await _amazonS3Service.UploadFileAsync(EnumBucketType.FselPublic, formFile, EnumFolderType.Videos, false, false);
+        }
+
+
+        private async Task PublishTextToSocket(SpeechToTextAiConsumerModel message, string? convertContent, string? filePath)
+        {
+            await _speechToTextPublisher.Publish(new SpeechToTextConsumerModel { UserId = message.UserId, TranscriptFile = new Shared.Models.ShareModels.TranscriptFileModel { Content = convertContent, FilePath = filePath } }, CancellationToken.None);
         }
 
     }
