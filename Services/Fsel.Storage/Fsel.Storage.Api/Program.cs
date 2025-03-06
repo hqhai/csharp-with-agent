237ee1a58b958b3b9ace220f8906b5c0c45e7672
hqhai <quanghai@atlantic.edu.vn>
2025-03-06 14:15:21 +0700

    update/fsel-4687: cập nhật logic về phần queue

--- Services/Fsel.Storage/Fsel.Storage.Api/Program.cs
+++ Services/Fsel.Storage/Fsel.Storage.Api/Program.cs
@@ -27,6 +27,7 @@ builder.Services.AddRefitClient<IOpenAIService>().ConfigureHttpClient(delegate (
     {
         var randomApiKey = appSetting.OpenAiConfig.ApiKeys[Random.Shared.Next(appSetting.OpenAiConfig.ApiKeys.Count)];
         httpClient.DefaultRequestHeaders.Add("Authorization", $"{Settings.Bearer} {randomApiKey}");
+        httpClient.Timeout = TimeSpan.FromSeconds(2); // Thiết lập timeout 1 phút 40 giây
     }
 });
 
