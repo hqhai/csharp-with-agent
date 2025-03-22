6647291da4c012ce6949fb61ddf9e9018cd4f2c3
honghv <vanhhong@atlantic.edu.vn>
2025-03-22 08:36:03 +0700

    update log chatgpt

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/AiCmd/SubmitAICommand.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/AiCmd/SubmitAICommand.cs
@@ -57,19 +57,22 @@ namespace Fsel.Course.Lms.Application.Commands.AiCmd
             var result = response.Content?.Choices?.Select(x => x.Message?.Content).FirstOrDefault();
             string requestLog = ConvertHelper.Serialize(request);
             string responseLog = "";
-            if (!response.IsSuccessStatusCode)
+            try
             {
-                try
+                if (response.RequestMessage != null)
                 {
-                    responseLog = ConvertHelper.Serialize(response.RequestMessage);
+                    responseLog = responseLog + " RequestMessage: " + ConvertHelper.Serialize(response.RequestMessage);
                 }
-                catch (Exception ex)
+                if (response.Content != null)
                 {
-                    responseLog = result + " - Error convert json: " + ex.Message;
-
+                    responseLog = responseLog + " Content: " + ConvertHelper.Serialize(response.Content);
                 }
-                _logger.LogCritical($"ChatGPT response : {responseLog}, RequestLog : {requestLog}");
             }
+            catch (Exception ex)
+            {
+                responseLog = result + " - Error convert json: " + ex.Message;
+            }
+            _logger.LogCritical($"ChatGPT response : {responseLog}, RequestLog : {requestLog}");
             return result;
         }
     }
