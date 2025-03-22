dd736895c1cc650f616bea31c477e0b0e4c571f6
honghv <vanhhong@atlantic.edu.vn>
2025-03-22 08:37:21 +0700

    add status code

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/AiCmd/SubmitAICommand.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/AiCmd/SubmitAICommand.cs
@@ -59,6 +59,7 @@ namespace Fsel.Course.Lms.Application.Commands.AiCmd
             string responseLog = "";
             try
             {
+                responseLog = responseLog + " StatusCode: " + ConvertHelper.Serialize(response.StatusCode);
                 if (response.RequestMessage != null)
                 {
                     responseLog = responseLog + " RequestMessage: " + ConvertHelper.Serialize(response.RequestMessage);
