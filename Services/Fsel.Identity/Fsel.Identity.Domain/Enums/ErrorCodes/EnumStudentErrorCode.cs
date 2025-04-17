44df7a673288d64a49d09e2c86d5cda9c204493a
hqhai <quanghai@atlantic.edu.vn>
2025-04-17 10:20:21 +0700

    feature/fsel-5334: change expireddate student

--- Services/Fsel.Identity/Fsel.Identity.Domain/Enums/ErrorCodes/EnumStudentErrorCode.cs
+++ Services/Fsel.Identity/Fsel.Identity.Domain/Enums/ErrorCodes/EnumStudentErrorCode.cs
@@ -42,6 +42,11 @@ namespace Fsel.Identity.Domain.Enums.ErrorCodes
         /// <summary>
         /// Student hasn't not expired Date
         /// </summary>
+        StudentAlreadyAnotherEvent,
+
+        /// <summary>
+        /// Student hasn't not expired Date
+        /// </summary>
         StudentHasNotExpiredDate
     }
 }
