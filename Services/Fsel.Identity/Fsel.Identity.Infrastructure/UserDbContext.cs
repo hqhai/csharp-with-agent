c29dc4e968eca9b0e4dd8f3c8c37dc8c2d60418f
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2025-04-23 11:11:32 +0700

    feature/fsel-5242: update user db context

--- Services/Fsel.Identity/Fsel.Identity.Infrastructure/UserDbContext.cs
+++ Services/Fsel.Identity/Fsel.Identity.Infrastructure/UserDbContext.cs
@@ -26,9 +26,9 @@ namespace Fsel.Identity.Infrastructure
             ArgumentNullException.ThrowIfNull(builder);
 
             //Dùng khi tạo migration, comment lại sau khi tạo xong
-            builder.Ignore<OverallStudentModel>();
-            builder.Ignore<NumberStudentLearnOnSystemModel>();
-            builder.Ignore<SummaryDataOnCityModel>();
+            //builder.Ignore<OverallStudentModel>();
+            //builder.Ignore<NumberStudentLearnOnSystemModel>();
+            //builder.Ignore<SummaryDataOnCityModel>();
 
             builder.Entity<Role>().HasQueryFilter(e => !e.IsDeleted);
             builder.Entity<User>().HasQueryFilter(e => !e.IsDeleted);
@@ -100,11 +100,11 @@ namespace Fsel.Identity.Infrastructure
         public DbSet<SchoolImportHistory> SchoolImportHistorys { get; set; }
         public DbSet<StudentEventLearningRecord> StudentEventLearningRecords { get; set; }
 
-        public DbSet<OverallStudentModel> OverallStudentResults { get; set; }
+        //public DbSet<OverallStudentModel> OverallStudentResults { get; set; }
 
-        public DbSet<NumberStudentLearnOnSystemModel> NumberStudentLearnOnSystemResults { get; set; }
+        //public DbSet<NumberStudentLearnOnSystemModel> NumberStudentLearnOnSystemResults { get; set; }
 
-        public DbSet<SummaryDataOnCityModel> SummaryDataOnCityResults { get; set; }
+        //public DbSet<SummaryDataOnCityModel> SummaryDataOnCityResults { get; set; }
 
         #endregion Db Set
 
