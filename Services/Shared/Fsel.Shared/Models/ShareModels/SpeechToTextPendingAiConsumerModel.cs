2ca2cc9434b05ea84894202153776b3fdd24ab36
Minh Chiến Lê Xuân <minhchien@atlantic.edu.vn>
2025-04-23 14:30:15 +0700

    feature/fsel-5090-5042: change name model

--- Services/Shared/Fsel.Shared/Models/ShareModels/SpeechToTextPendingAiConsumerModel.cs
+++ Services/Shared/Fsel.Shared/Models/ShareModels/SpeechToTextPendingAiConsumerModel.cs
@@ -2,9 +2,9 @@
 
 namespace Fsel.Shared.Models.ShareModels
 {
-    public class SpeechToTextAiConsumerModel
+    public class SpeechToTextPendingAiConsumerModel
     {
-        public Guid UserId { get; set; }
+        public Guid ClassForumDetailResultId { get; set; }
 
         public string FileName { get; set; } = string.Empty;
 
