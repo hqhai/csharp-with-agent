947d76099c3c0d2829cacc50c4acdfad8c8edb23
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2025-04-25 13:18:52 +0700

    feature/fsel-5275: update GetRemainingTimeQuery

--- Services/Fsel.System/Fsel.System.Application/Queries/DailyQuiz/GetRemainingTimeQuery.cs
+++ Services/Fsel.System/Fsel.System.Application/Queries/DailyQuiz/GetRemainingTimeQuery.cs
@@ -1,5 +1,7 @@
 using Fsel.Common.ActionResults;
 using Fsel.Common.Helpers;
+using Fsel.System.Domain.Enums;
+using Fsel.System.Infrastructure.ValueSettings;
 using MediatR;
 
 namespace Fsel.System.Application.Queries.DailyQuiz
@@ -10,16 +12,25 @@ namespace Fsel.System.Application.Queries.DailyQuiz
 
     public class GetRemainingTimeQueryHandler : IRequestHandler<GetRemainingTimeQuery, MethodResult<int>>
     {
-        public GetRemainingTimeQueryHandler()
+        private readonly AppSetting _appSetting;
+
+        public GetRemainingTimeQueryHandler(AppSetting appSetting)
         {
+            _appSetting = appSetting;
         }
 
         public async Task<MethodResult<int>> Handle(GetRemainingTimeQuery request, CancellationToken cancellationToken)
         {
             ArgumentNullException.ThrowIfNull(request);
             var methodResult = new MethodResult<int>();
+            var endHour = _appSetting.DailyQuizConfig?.EndHour;
+            if (!endHour.HasValue)
+            {
+                methodResult.AddErrorBadRequest(nameof(EnumDailyQuizErrorCode.MissingEventConfiguration), nameof(EnumDailyQuizErrorCode.MissingEventConfiguration), EnumDailyQuizErrorCode.MissingEventConfiguration.GetDescription());
+                return methodResult;
+            }
             var currentDate = DateTime.UtcNow.ConvertTimeFromUtc(EnumCountryKey.Vietnam);
-            var targetTime = new DateTime(currentDate.Year, currentDate.Month, currentDate.Day, 21, 0, 0);
+            var targetTime = new DateTime(currentDate.Year, currentDate.Month, currentDate.Day, endHour.Value, 0, 0);
             double remainingSeconds = (targetTime - currentDate).TotalSeconds;
             int secondsLeft = (int)Math.Max(0, Math.Floor(remainingSeconds));
             methodResult.Result = secondsLeft;
