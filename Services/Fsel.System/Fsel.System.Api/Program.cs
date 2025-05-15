2614d9cb3bc52335106eb0f7dde3de74329e8da1
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2025-05-15 11:06:39 +0700

    feature/fsel-4793: Update Program to Comment Code

--- Services/Fsel.System/Fsel.System.Api/Program.cs
+++ Services/Fsel.System/Fsel.System.Api/Program.cs
@@ -122,23 +122,23 @@ builder.Services.AddRefitClient<IOpenAIService>().ConfigureHttpClient(delegate (
 builder.AddMassTransit(appSetting,
 queues: new Dictionary<string, Type>
 {
-    //{ QueueSettings.SystemQueue.NameQueue.CompleteApprovalPostTimeOut, typeof(CompleteApprovalConsumer) },
-    //{ QueueSettings.LmsQueue.NameQueue.CreateTokenHistory, typeof(CreateTokenHistoryConsumer) },
-    //{ QueueSettings.UserQueue.NameQueue.CreateTokenHistory, typeof(CreateTokenHistoryConsumer) },
-    //{ QueueSettings.RealtimeQueue.NameQueue.FeatureAccessTime, typeof(FeatureAccessTimeConsumer) },
-    //{ QueueSettings.RealtimeQueue.NameQueue.ChatBot, typeof(ChatBotConsumer) },
-    //{ QueueSettings.LmsQueue.NameQueue.DoQuestBoard, typeof(CreateTokenHistoryConsumer) },
-    //{ QueueSettings.SystemQueue.NameQueue.QuestBoard, typeof(DoQuestBoardConsumer) },
-    //{ QueueSettings.RealtimeQueue.NameQueue.TechieAction, typeof(TechieConsumer) },
-    //{ QueueSettings.LmsQueue.NameQueue.Techie, typeof(TechieConsumer) },
-    //{ QueueSettings.SystemQueue.NameQueue.CreateLuckyTicket, typeof(CreateLuckyTicketConsumer) },
-    //{ QueueSettings.SystemQueue.NameQueue.NoticeAccessTime, typeof(NoticeAccessFeatureConsumer) },
-    //{ QueueSettings.InteractionQueue.NameQueue.CreateTokenHistory, typeof(CreateTokenHistoryConsumer) },
-    //{ QueueSettings.RealtimeQueue.NameQueue.Banner, typeof(BannerConsumer) },
-    //{ QueueSettings.SystemQueue.NameQueue.BuyBlindBox, typeof(BuyBlindBoxConsumer) },
-    //{ QueueSettings.SystemQueue.NameQueue.ChooseDailyQuizWinners, typeof(ChooseDailyQuizWinnersConsumer) },
-    //{ QueueSettings.RealtimeQueue.NameQueue.DictionaryRealTime, typeof(DictionaryConsumer) },
-    //{ QueueSettings.SystemQueue.NameQueue.CrawDictionaryData, typeof(CrawDictionaryDataConsumer) }
+    { QueueSettings.SystemQueue.NameQueue.CompleteApprovalPostTimeOut, typeof(CompleteApprovalConsumer) },
+    { QueueSettings.LmsQueue.NameQueue.CreateTokenHistory, typeof(CreateTokenHistoryConsumer) },
+    { QueueSettings.UserQueue.NameQueue.CreateTokenHistory, typeof(CreateTokenHistoryConsumer) },
+    { QueueSettings.RealtimeQueue.NameQueue.FeatureAccessTime, typeof(FeatureAccessTimeConsumer) },
+    { QueueSettings.RealtimeQueue.NameQueue.ChatBot, typeof(ChatBotConsumer) },
+    { QueueSettings.LmsQueue.NameQueue.DoQuestBoard, typeof(CreateTokenHistoryConsumer) },
+    { QueueSettings.SystemQueue.NameQueue.QuestBoard, typeof(DoQuestBoardConsumer) },
+    { QueueSettings.RealtimeQueue.NameQueue.TechieAction, typeof(TechieConsumer) },
+    { QueueSettings.LmsQueue.NameQueue.Techie, typeof(TechieConsumer) },
+    { QueueSettings.SystemQueue.NameQueue.CreateLuckyTicket, typeof(CreateLuckyTicketConsumer) },
+    { QueueSettings.SystemQueue.NameQueue.NoticeAccessTime, typeof(NoticeAccessFeatureConsumer) },
+    { QueueSettings.InteractionQueue.NameQueue.CreateTokenHistory, typeof(CreateTokenHistoryConsumer) },
+    { QueueSettings.RealtimeQueue.NameQueue.Banner, typeof(BannerConsumer) },
+    { QueueSettings.SystemQueue.NameQueue.BuyBlindBox, typeof(BuyBlindBoxConsumer) },
+    { QueueSettings.SystemQueue.NameQueue.ChooseDailyQuizWinners, typeof(ChooseDailyQuizWinnersConsumer) },
+    { QueueSettings.RealtimeQueue.NameQueue.DictionaryRealTime, typeof(DictionaryConsumer) },
+    { QueueSettings.SystemQueue.NameQueue.CrawDictionaryData, typeof(CrawDictionaryDataConsumer) }
 });
 
 var app = builder.Build();
