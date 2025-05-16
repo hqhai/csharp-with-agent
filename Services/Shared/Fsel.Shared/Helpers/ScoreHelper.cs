89e1433a61fa90fa7f496413e3c8fa738fe3a320
Minh Chiến Lê Xuân <minhchien@atlantic.edu.vn>
2025-05-16 18:03:43 +0700

    feature/fsel-5301: update tính điểm

--- Services/Shared/Fsel.Shared/Helpers/ScoreHelper.cs
+++ Services/Shared/Fsel.Shared/Helpers/ScoreHelper.cs
@@ -11,17 +11,17 @@ namespace Fsel.Shared.Helpers
             // thiếu fluency
             if (!fluency.HasValue)
             {
-                score = (0.65 * accuracy) * (0.35 * (prosody ?? 0));
+                score = (0.65 * accuracy) + (0.35 * (prosody ?? 0));
             }
             // thiếu prosody
             else if (!prosody.HasValue)
             {
-                score = (0.60 * accuracy) * (0.40 * (fluency ?? 0));
+                score = (0.60 * accuracy) + (0.40 * (fluency ?? 0));
             }
             // đủ cả 3
             else
             {
-                score = (0.50 * accuracy) * (0.30 * fluency.Value) * (0.20 * prosody.Value);
+                score = (0.50 * accuracy) + (0.30 * fluency.Value) + (0.20 * prosody.Value);
             }
 
             return ConvertToFivePointScale((int)Math.Round(score));
