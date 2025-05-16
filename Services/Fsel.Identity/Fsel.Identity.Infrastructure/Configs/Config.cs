8ff93bc7bcc2542221369ae0840aa445dd5b7d26
Nguyễn Thành Đô <thanhdo@atlantic.edu.vn>
2025-05-16 09:21:32 +0700

    openid: update app.fsel.angular PostLogoutRedirectUris

--- Services/Fsel.Identity/Fsel.Identity.Infrastructure/Configs/Config.cs
+++ Services/Fsel.Identity/Fsel.Identity.Infrastructure/Configs/Config.cs
@@ -64,15 +64,19 @@ namespace Fsel.Authentication.Infrastructure.Configs
                     {
                         "https://localhost:4400",
                         "http://localhost:4400",
-                        "http://lms-dev.fsel.edu.vn",
-                        "https://lms-dev.fsel.edu.vn"
+                        "https://lms-dev.fsel.edu.vn",
+                        "http://lms-dev.fsel.edu.vn"
                     },
                     PostLogoutRedirectUris =
                     {
+                        "https://localhost:4400",
+                        "http://localhost:4400",
+                        "https://lms-dev.fsel.edu.vn",
+                        "http://lms-dev.fsel.edu.vn",
                         "https://localhost:4400/auth/login",
                         "http://localhost:4400/auth/login",
-                        "http://lms-dev.fsel.edu.vn/auth/login",
-                        "https://lms-dev.fsel.edu.vn/auth/login"
+                        "https://lms-dev.fsel.edu.vn/auth/login",
+                        "http://lms-dev.fsel.edu.vn/auth/login"
                     },
 
                     AllowOfflineAccess = true,
