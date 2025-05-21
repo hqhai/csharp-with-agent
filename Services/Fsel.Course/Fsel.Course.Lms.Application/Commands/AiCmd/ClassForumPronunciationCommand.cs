5b44e4c1664c4c0cbf23d8bf5f19ce082226f98b
Minh Chiến Lê Xuân <minhchien@atlantic.edu.vn>
2025-05-21 15:57:49 +0700

    feature/fsel-5301: fix update bị đè field

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/AiCmd/ClassForumPronunciationCommand.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/AiCmd/ClassForumPronunciationCommand.cs
@@ -81,7 +81,15 @@ namespace Fsel.Course.Lms.Application.Commands.AiCmd
                     await _classForumDetailResultRepository.ExecuteTransactionAsync(async () =>
                     {
                         classForumDetailResult.PronunciationAlFeedback = ConvertHelper.Serialize(response);
-                        _classForumDetailResultRepository.Update(classForumDetailResult);
+                        _classForumDetailResultRepository.Update(classForumDetailResult, false
+                        , x => x.WordContent, x => x.Content
+                        , x => x.WordCount, x => x.SubmissionCount
+                        , x => x.ProcessDate, x => x.CompletionDate
+                        , x => x.Status
+                        // task 5307 chưa lên prod
+                        //, x => x.IsForbiddenImage
+                        //, x => x.IsForbiddenWork, x => x.GradingAiForbidden
+                        , x => x.GradingAlFeedback);
                         await _classForumDetailResultRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
                         methodResult.Result = true;
                         return methodResult;
