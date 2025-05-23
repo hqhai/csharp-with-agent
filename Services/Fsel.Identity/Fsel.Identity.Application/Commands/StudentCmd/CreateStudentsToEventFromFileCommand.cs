19b5e1300ceb37a308bc3a96cfc86cbf1f452a03
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2025-05-23 19:32:59 +0700

    feature/fsel-5679: update validate import

--- Services/Fsel.Identity/Fsel.Identity.Application/Commands/StudentCmd/CreateStudentsToEventFromFileCommand.cs
+++ Services/Fsel.Identity/Fsel.Identity.Application/Commands/StudentCmd/CreateStudentsToEventFromFileCommand.cs
@@ -400,6 +400,10 @@ namespace Fsel.Identity.Application.Commands.StudentCmd
                             {
                                 errors.Add(new ValidateExcelModel { RowIndex = rowIndex, ColumnName = nameof(x.DateOfBirth), Message = ErrorMassageSetting.InvalidBirthDayVN });
                             }
+                            else if (x.DateOfBirth.HasValue && x.DateOfBirth.Value.Year > (DateTime.UtcNow.ConvertTimeFromUtc(EnumCountryKey.Vietnam).Year - 1))
+                            {
+                                errors.Add(new ValidateExcelModel { RowIndex = rowIndex, ColumnName = nameof(x.DateOfBirth), Message = ErrorMassageSetting.InvalidBirthDayVN });
+                            }
                             else if (x.DateOfBirth.HasValue && x.DateOfBirth.Value.Year > DateTime.UtcNow.ConvertTimeFromUtc(EnumCountryKey.Vietnam).Year)
                             {
                                 errors.Add(new ValidateExcelModel { RowIndex = rowIndex, ColumnName = nameof(x.DateOfBirth), Message = ErrorMassageSetting.InvalidBirthDayVN });
