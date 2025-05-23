858691071293646a07c269574ff9ba24f59f7e6f
hqhai <quanghai@atlantic.edu.vn>
2025-05-23 14:15:41 +0700

    feature/fsel-5722: remove special label

--- Services/Fsel.Identity/Fsel.Identity.Application/Commands/StudentCmd/CreateStudentsToEventFromFileCommand.cs
+++ Services/Fsel.Identity/Fsel.Identity.Application/Commands/StudentCmd/CreateStudentsToEventFromFileCommand.cs
@@ -476,8 +476,8 @@ namespace Fsel.Identity.Application.Commands.StudentCmd
                 throw new ArgumentException("Full name and phone number cannot be empty.");
             }
 
-            string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            string digits = "0123456789";
+            string letters = "ABCDEFGHJKMNPQRSTUVWXYZ";
+            string digits = "123456789";
 
             CultureInfo cultureInfo = CultureInfo.InvariantCulture;
 
