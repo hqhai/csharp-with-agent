e5f135e5ff2c185bd8071c53f486adba89eb48d6
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2025-05-28 17:26:05 +0700

    feature/fsel-5276: update api get students choose level in 7 day

---
+++ Services/Shared/Fsel.Shared/Models/ShareModels/StudentsIn7DayChooseLevelModel.cs
@@ -0,0 +1,8 @@
+namespace Fsel.Shared.Models.ShareModels
+{
+    public class StudentsIn7DayChooseLevelModel
+    {
+        public Guid StudentId { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
