657dbd6857eb2b9a13bedb8888aab6923a3600bd
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2025-06-03 14:20:20 +0700

    feature/fsel-5817: Update Model Config SchoolEventRule

--- Services/Shared/Fsel.Shared/Models/ShareModels/SchoolEventRule.cs
+++ Services/Shared/Fsel.Shared/Models/ShareModels/SchoolEventRule.cs
@@ -61,9 +61,7 @@ namespace Fsel.Shared.Models.ShareModels
         public IList<CollectiveAward>? CollectiveAwards { get; set; }
         public FormConfig? FormConfig { get; set; }
         public Guid? LocationId { get; set; }
-
         public string? BackGroundImage { get; set; }
-
         public IList<string>? TabNames { get; set; }
         public ButtonNavSettings? ButtonNavSettings { get; set; }
         public bool? AutoGenAge { get; set; }
@@ -83,7 +81,9 @@ namespace Fsel.Shared.Models.ShareModels
         ExportAccount,
         EventGuideScreen,
         WarningScreen,
-        DailyQuiz
+        DailyQuiz,
+        EventRegistrationSuccess,
+        EventGiftDisplay
     }
 
     public enum EnumByPassPaymentType
