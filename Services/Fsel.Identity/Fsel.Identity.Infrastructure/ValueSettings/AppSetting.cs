baaa0b3088b77ecce2d7b0ca8b65caeb7e182891
Minh Chiến Lê Xuân <minhchien@atlantic.edu.vn>
2025-06-10 10:51:23 +0700

    feature/fsel-5981: update key gg sheet

--- Services/Fsel.Identity/Fsel.Identity.Infrastructure/ValueSettings/AppSetting.cs
+++ Services/Fsel.Identity/Fsel.Identity.Infrastructure/ValueSettings/AppSetting.cs
@@ -70,6 +70,7 @@ namespace Fsel.Identity.Infrastructure.ValueSettings
         public string? CreateUsersAndOrders { get; set; }
         public string? AddCoinSpreadSheetId { get; set; }
         public string? AddCoinBuyCourseSpreadSheetId { get; set; }
+        public string? AddCoinFselEventSpreadSheetId { get; set; }
     }
 
     public class UserDeletionConfig
