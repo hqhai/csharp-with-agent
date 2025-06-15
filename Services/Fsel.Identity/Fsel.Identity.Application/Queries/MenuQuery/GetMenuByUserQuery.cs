3e89ee7d6b49dd83f7a91abd01159528a220acda
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2025-06-15 16:29:53 +0700

    feature-staging/fsel-5942: update menu json

--- Services/Fsel.Identity/Fsel.Identity.Application/Queries/MenuQuery/GetMenuByUserQuery.cs
+++ Services/Fsel.Identity/Fsel.Identity.Application/Queries/MenuQuery/GetMenuByUserQuery.cs
@@ -49,6 +49,8 @@ namespace Fsel.Identity.Application.Queries.MenuQuery
                                where r.Name == role && pg.MenuId.HasValue
                                select pg.MenuId).ToListAsync(cancellationToken);
 
+            query = query.Distinct().ToList();
+
             var menus = await _menuRepository.Queryable.WhereBulkContains(query, p => p.Id).OrderBy(p => p.Index).ToListAsync(cancellationToken);
 
             methodResult.Result = menus.Where(p => p.Config != null).Select(p => p.Config!).ToList();
