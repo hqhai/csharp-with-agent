0e141fec5e6227c19317a58a292f11bb38c66509
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2025-06-16 15:41:17 +0700

    feature/fsel-5921: update images

--- Services/Fsel.Ordering/Fsel.Ordering.Infrastructure/Migrations/20250616083844_Update_ProductJson.Designer.cs
+++ Services/Fsel.Ordering/Fsel.Ordering.Infrastructure/Migrations/20250616083844_Update_ProductJson.Designer.cs
@@ -4,6 +4,7 @@ using Fsel.Ordering.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fsel.Ordering.Infrastructure.Migrations
 {
     [DbContext(typeof(OrderingDbContext))]
-    partial class OrderingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250616083844_Update_ProductJson")]
+    partial class Update_ProductJson
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
