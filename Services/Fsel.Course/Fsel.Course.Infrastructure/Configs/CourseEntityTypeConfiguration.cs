4ef0d14bfe1089c60050c677473f32345ebf295e
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2025-06-19 21:42:19 +0700

    feature/fsel-6017: Update Migration

--- Services/Fsel.Course/Fsel.Course.Infrastructure/Configs/CourseEntityTypeConfiguration.cs
+++ Services/Fsel.Course/Fsel.Course.Infrastructure/Configs/CourseEntityTypeConfiguration.cs
@@ -25,8 +25,7 @@ namespace Fsel.Course.Infrastructure.Configs
                     v => v.ToString(),
                     v => v.EnumParse<EnumCourseStatus>());
 
-            builder.HasIndex(c => new { c.ParentCourseId, c.Priority }).IsUnique().HasFilter("[IsDeleted] IS FALSE");
-            ;
+            builder.HasIndex(c => new { c.ParentCourseId, c.Priority }).IsUnique().HasFilter("ParentCourseId IS NOT NULL AND [IsDeleted] = 0");
         }
     }
 }
--- Services/Fsel.Course/Fsel.Course.Infrastructure/Configs/FinalTestAnswerEntityTypeConfiguration.cs
+++ Services/Fsel.Course/Fsel.Course.Infrastructure/Configs/FinalTestAnswerEntityTypeConfiguration.cs
@@ -36,7 +36,7 @@ namespace Fsel.Course.Infrastructure.Configs
                       v => v.ToString(),
                       v => v.EnumParse<EnumAnswerStatus>());
 
-            builder.HasIndex(c => new { c.FinalTestResultId, c.SectionQuestionId, c.SectionGroupResultId }).IsUnique().HasFilter("[IsDeleted] IS FALSE");
+            builder.HasIndex(c => new { c.FinalTestResultId, c.SectionQuestionId, c.SectionGroupResultId }).IsUnique().HasFilter("[IsDeleted] = 0");
             ;
         }
     }
