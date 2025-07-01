e90266bca59b511b3c4df4cff11423468cbe9578
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2025-07-01 15:16:13 +0700

    feature-staging/fsel-6093: add dashboard report permission

--- Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/Admin/ReportDashboardController.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/Admin/ReportDashboardController.cs
@@ -4,20 +4,19 @@ namespace Fsel.Course.Lms.Api.Controllers.Admin
 {
     using System.Net;
     using Fsel.Common.ActionResults;
+    using Fsel.Common.Attributes;
     using Fsel.Common.Constants;
     using Fsel.Course.Domain.Models.EntityModels.BaseChartModels;
     using Fsel.Course.Domain.Models.QueryModels.ReportDashboard;
     using Fsel.Course.Lms.Application.Queries.ReportDashboardQuery;
     using Fsel.Shared.Attributes;
     using Fsel.Shared.Constants;
-    using Fsel.Shared.Enums;
     using MediatR;
     using Microsoft.AspNetCore.Mvc;
 
     [ApiVersions(ApiSettings.APIVersion1)]
     [Route(Settings.APIDefaultRoute + "/admin/report-dashboard")]
     [ApiController]
-    [Common.Attributes.Permission(roles: new string[] { nameof(EnumRole.Admin), nameof(EnumRole.AdminSchool), nameof(EnumRole.CSO) })]
     public class ReportDashboard : ControllerBase
     {
         private readonly IMediator _mediator;
@@ -33,6 +32,7 @@ namespace Fsel.Course.Lms.Api.Controllers.Admin
         [HttpGet]
         [ProducesResponseType(typeof(MethodResult<ReportPTResultModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(DashboardManagementByAdminSchool.ViewPTResultsReport)]
         public async Task<IActionResult> ReportDashboardPT([FromQuery] GetReportPTResultQuery query)
         {
             MethodResult<ReportPTResultModel> queryResult = await _mediator.Send(query).ConfigureAwait(false);
@@ -45,6 +45,7 @@ namespace Fsel.Course.Lms.Api.Controllers.Admin
         [HttpGet("learning-result-statistical")]
         [ProducesResponseType(typeof(MethodResult<DashBoardLearningResultModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(DashboardManagementByAdminSchool.ViewLearningResultsReport)]
         public async Task<IActionResult> ReportLearningResult([FromQuery] GetReportLearningResultQuery query)
         {
             MethodResult<DashBoardLearningResultModel> queryResult = await _mediator.Send(query).ConfigureAwait(false);
@@ -57,6 +58,7 @@ namespace Fsel.Course.Lms.Api.Controllers.Admin
         [HttpGet("student-learning-statistical")]
         [ProducesResponseType(typeof(MethodResult<StackBarChartsModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(DashboardManagementByAdminSchool.ViewLearningResultsReport)]
         public async Task<IActionResult> ReportLearningResultStack([FromQuery] GetReportLearningResultStackQuery query)
         {
             MethodResult<StackBarChartsModel> queryResult = await _mediator.Send(query).ConfigureAwait(false);
@@ -69,6 +71,7 @@ namespace Fsel.Course.Lms.Api.Controllers.Admin
         [HttpGet("learning-progress-statistical")]
         [ProducesResponseType(typeof(MethodResult<DashBoardLearningProgressModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(DashboardManagementByAdminSchool.ViewLearningProgressReport)]
         public async Task<IActionResult> ReportLearningProgress([FromQuery] GetReportLearningProgressQuery query)
         {
             MethodResult<DashBoardLearningProgressModel> queryResult = await _mediator.Send(query).ConfigureAwait(false);
@@ -81,6 +84,7 @@ namespace Fsel.Course.Lms.Api.Controllers.Admin
         [HttpGet("student-learning-progress-statistical")]
         [ProducesResponseType(typeof(MethodResult<StackBarChartsModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(DashboardManagementByAdminSchool.ViewLearningProgressReport)]
         public async Task<IActionResult> ReportLearningProgressStack([FromQuery] GetReportLearningProgressStackQuery query)
         {
             MethodResult<StackBarChartsModel> queryResult = await _mediator.Send(query).ConfigureAwait(false);
@@ -93,6 +97,7 @@ namespace Fsel.Course.Lms.Api.Controllers.Admin
         [HttpGet("student-diligence")]
         [ProducesResponseType(typeof(MethodResult<DashBoardDiligenceModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        [Permission(DashboardManagementByAdminSchool.ViewAttendanceReport)]
         public async Task<IActionResult> ReportDashboardDiligence([FromQuery] GetReportDiligenceResultQuery query)
         {
             var queryResult = await _mediator.Send(query).ConfigureAwait(false);
