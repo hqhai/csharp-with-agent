a60b0a26f312b0a86ec4320056cec7e491a2cd74
Khúc Xuân Hào <xuanhao@atlantic.edu.vn>
2025-07-01 09:19:15 +0700

    feature/fsel-5883: create controller

--- Services/Fsel.Course/Fsel.Course.Lcms.Api/Controllers/TestConfigController.cs
+++ Services/Fsel.Course/Fsel.Course.Lcms.Api/Controllers/TestConfigController.cs
@@ -7,20 +7,20 @@ namespace Fsel.Course.Lcms.Api.Controllers
     using Fsel.Common.ActionResults;
     using Fsel.Common.Constants;
     using Fsel.Core.Base.BaseModels;
-    using Fsel.Course.Application.Commands.MockTestCmd;
-    using Fsel.Course.Application.Queries.MockTestQuery;
-    using Fsel.Course.Domain.Models.EntityModels;
+    using Fsel.Course.Application.Commands.TestConfigCmd;
+    using Fsel.Course.Application.Commands.TestConfigSectionCmd;
+    using Fsel.Course.Application.Queries.TestConfigQuery;
+    using Fsel.Course.Domain.Models.EntityModels.TestConfig;
     using Fsel.Shared.Constants;
     using Fsel.Shared.Enums;
-    using MassTransit.Mediator;
-    using Microsoft.AspNetCore.Http;
+    using MediatR;
     using Microsoft.AspNetCore.Mvc;
 
     [ApiVersion(ApiSettings.APIVersion1)]
     [ApiVersion(ApiSettings.APIVersion1i1)]
     [Route(Settings.APIDefaultRoute + "/test-config")]
-    [Common.Attributes.Permission(role: nameof(EnumRole.MasterAdmin))]
     [ApiController]
+    [Common.Attributes.Permission(role: nameof(EnumRole.MasterAdmin))]
     public class TestConfigController : ControllerBase
     {
         private readonly IMediator _mediator;
@@ -30,66 +30,66 @@ namespace Fsel.Course.Lcms.Api.Controllers
             _mediator = mediator;
         }
 
-        ///// <summary>
-        ///// Search Test Config
-        ///// </summary>
-        //[HttpGet]
-        //[ProducesResponseType(typeof(MethodResult<PagingItemsModel<MockTestModel>>), (int)HttpStatusCode.OK)]
-        //[ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
-        //public async Task<IActionResult> Search([FromQuery] SearchMockTestQuery query)
-        //{
-        //    MethodResult<PagingItemsModel<TestConfigSearchModel>> queryResult = await _mediator.Send(query).ConfigureAwait(false);
-        //    return queryResult.GetActionResult();
-        //}
+        /// <summary>
+        /// Search Test Config
+        /// </summary>
+        [HttpGet]
+        [ProducesResponseType(typeof(MethodResult<PagingItemsModel<TestConfigModel>>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> Search([FromQuery] SearchTestConfigQuery query)
+        {
+            MethodResult<PagingItemsModel<TestConfigModel>> queryResult = await _mediator.Send(query).ConfigureAwait(false);
+            return queryResult.GetActionResult();
+        }
 
-        ///// <summary>
-        ///// Get Test Config
-        ///// </summary>
-        //[HttpGet("{id}")]
-        //[ProducesResponseType(typeof(MethodResult<MockTestModel>), (int)HttpStatusCode.OK)]
-        //[ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
-        //public async Task<IActionResult> Get([FromRoute] Guid id)
-        //{
-        //    MethodResult<MockTestModel> queryResult = await _mediator.Send(new GetMockTestQuery { Id = id }).ConfigureAwait(false);
-        //    return queryResult.GetActionResult();
-        //}
+        /// <summary>
+        /// Get Test Config
+        /// </summary>
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(MethodResult<TestConfigModel>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> Get([FromRoute] Guid id)
+        {
+            MethodResult<TestConfigModel> queryResult = await _mediator.Send(new GetTestConfigQuery { Id = id }).ConfigureAwait(false);
+            return queryResult.GetActionResult();
+        }
 
-        ///// <summary>
-        ///// Create a Test Config
-        ///// </summary>
-        //[HttpPost]
-        //[ProducesResponseType(typeof(MethodResult<MockTestModel>), (int)HttpStatusCode.OK)]
-        //[ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
-        //public async Task<IActionResult> Create([FromBody] CreateMockTestCommand command)
-        //{
-        //    MethodResult<MockTestModel> commandResult = await _mediator.Send(command).ConfigureAwait(false);
-        //    return commandResult.GetActionResult();
-        //}
+        /// <summary>
+        /// Create a Test Config
+        /// </summary>
+        [HttpPost]
+        [ProducesResponseType(typeof(MethodResult<TestConfigModel>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> Create([FromBody] CreateTestConfigCommand command)
+        {
+            MethodResult<TestConfigModel> commandResult = await _mediator.Send(command).ConfigureAwait(false);
+            return commandResult.GetActionResult();
+        }
 
-        ///// <summary>
-        ///// Update a Test Config
-        ///// </summary>
-        //[HttpPut("{id}")]
-        //[ProducesResponseType(typeof(MethodResult<MockTestModel>), (int)HttpStatusCode.OK)]
-        //[ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
-        //public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateMockTestCommand command)
-        //{
-        //    ArgumentNullException.ThrowIfNull(command);
-        //    command.Id = id;
-        //    MethodResult<MockTestModel> commandResult = await _mediator.Send(command).ConfigureAwait(false);
-        //    return commandResult.GetActionResult();
-        //}
+        /// <summary>
+        /// Update a Test Config
+        /// </summary>
+        [HttpPut("{id}")]
+        [ProducesResponseType(typeof(MethodResult<TestConfigModel>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateTestConfigCommand command)
+        {
+            ArgumentNullException.ThrowIfNull(command);
+            command.Id = id;
+            MethodResult<TestConfigModel> commandResult = await _mediator.Send(command).ConfigureAwait(false);
+            return commandResult.GetActionResult();
+        }
 
-        ///// <summary>
-        ///// Delete a Test Config
-        ///// </summary>
-        //[HttpDelete("{id}")]
-        //[ProducesResponseType(typeof(MethodResult<MockTestModel>), (int)HttpStatusCode.OK)]
-        //[ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
-        //public async Task<IActionResult> Delete([FromRoute] Guid id)
-        //{
-        //    MethodResult<bool> commandResult = await _mediator.Send(new DeleteMockTestCommand { Id = id }).ConfigureAwait(false);
-        //    return commandResult.GetActionResult();
-        //}
+        /// <summary>
+        /// Delete a Test Config Section
+        /// </summary>
+        [HttpDelete("/section/{id}")]
+        [ProducesResponseType(typeof(MethodResult<TestConfigModel>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> DeleteSection([FromRoute] Guid id)
+        {
+            MethodResult<bool> commandResult = await _mediator.Send(new DeleteTestConfigSectionCommand { Id = id }).ConfigureAwait(false);
+            return commandResult.GetActionResult();
+        }
     }
 }
