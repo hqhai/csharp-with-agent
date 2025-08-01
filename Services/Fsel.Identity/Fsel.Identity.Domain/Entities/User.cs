a40aeefcf7efc1364be2c2f6824f032fd8b5c90f
Hà Quang Hải <quanghai@atlantic.edu.vn>
2025-08-01 14:58:26 +0700

    fixbug/fsel-6479: update logic modified role of user

--- Services/Fsel.Identity/Fsel.Identity.Domain/Entities/User.cs
+++ Services/Fsel.Identity/Fsel.Identity.Domain/Entities/User.cs
@@ -40,6 +40,5 @@ namespace Fsel.Identity.Domain.Entities
         public virtual ICollection<UserSchool> UserSchools { get; set; } = new List<UserSchool>();
         public virtual UserReferral? Receiver { get; set; }
         public virtual ICollection<UserDeletion> UserDeletions { get; set; } = new List<UserDeletion>();
-        public virtual ICollection<UserGroupMemberShip> UserGroups { get; set; } = new List<UserGroupMemberShip>();
     }
 }
