0da51bf83b9b03490802b2f2b6b1fd23fd107328
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2025-08-11 17:23:37 +0700

    feature/fsel-5897: return status homework

--- Services/Fsel.Course/Fsel.Course.Application/Queries/HomeWorkQuery/GetHomeWorkQuery.cs
+++ Services/Fsel.Course/Fsel.Course.Application/Queries/HomeWorkQuery/GetHomeWorkQuery.cs
@@ -7,10 +7,12 @@ namespace Fsel.Course.Application.Queries.HomeWorkQuery
     using AutoMapper;
     using Fsel.Common.ActionResults;
     using Fsel.Common.Enums.ErrorCodes;
+    using Fsel.Course.Domain.Enums;
     using Fsel.Course.Domain.IRepositories;
     using Fsel.Course.Domain.Models.EntityModels;
     using MediatR;
     using Microsoft.AspNetCore.Http;
+    using Microsoft.EntityFrameworkCore;
 
     public class GetHomeWorkQuery : IRequest<MethodResult<HomeWorkModel>>
     {
@@ -21,11 +23,13 @@ namespace Fsel.Course.Application.Queries.HomeWorkQuery
     {
         private readonly IMapper _mapper;
         private readonly IHomeWorkRepository _homeWorkRepository;
+        private readonly ILessonModuleRepository _lessonModuleRepository;
 
-        public GetHomeWorkQueryHandler(IMapper mapper, IHomeWorkRepository homeWorkRepository)
+        public GetHomeWorkQueryHandler(IMapper mapper, IHomeWorkRepository homeWorkRepository, ILessonModuleRepository lessonModuleRepository)
         {
             _mapper = mapper;
             _homeWorkRepository = homeWorkRepository;
+            _lessonModuleRepository = lessonModuleRepository;
         }
 
         public async Task<MethodResult<HomeWorkModel>> Handle(GetHomeWorkQuery request, CancellationToken cancellationToken)
@@ -40,6 +44,10 @@ namespace Fsel.Course.Application.Queries.HomeWorkQuery
                 methodResult.AddErrorBadRequest(nameof(EnumSystemErrorCode.DataNotExist), nameof(homeWork));
                 return methodResult;
             }
+
+            var lessonModules = await _lessonModuleRepository.Queryable.Where(p => p.OriginalId == homeWork.OriginalId).Where(p => p.LessonConfigType == EnumLessonConfigType.HomeWork).ToListAsync(cancellationToken);
+
+            homeWork.IsActive = lessonModules.Any();
 
             methodResult.Result = _mapper.Map<HomeWorkModel>(homeWork);
             methodResult.StatusCode = StatusCodes.Status200OK;
