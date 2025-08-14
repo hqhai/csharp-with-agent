d0ff7be7a6271a66fa60f58aa4a66b3e1ec586ac
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2025-08-14 15:02:58 +0700

    fixbug/fsel-6720: correct data mapping issue when changing level

--- Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/V1i1/CourseController.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/V1i1/CourseController.cs
@@ -7,7 +7,6 @@ using Fsel.Common.Constants;
 using Fsel.Core.Base;
 using Fsel.Course.Domain.Models.EntityModels;
 using Fsel.Course.Lms.Application.Commands.CourseResultCmd;
-using Fsel.Course.Lms.Application.Commands.CourseResultCmd.AdminCmd;
 using Fsel.Course.Lms.Application.Queries.CourseQuery.V1i1;
 using Fsel.Shared.Attributes;
 using Fsel.Shared.Constants;
@@ -60,7 +59,7 @@ namespace Fsel.Course.Lms.Api.Controllers.V1i1
         [HttpPost("change-course-level")]
         [ProducesResponseType(typeof(MethodResult<IList<LevelDtoModel>>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
-        public async Task<IActionResult> ChangeCourseLevel([FromBody] ChangeCourseLevelByAdminCommand command)
+        public async Task<IActionResult> ChangeCourseLevel([FromBody] ChangeCourseLevelCommand command)
         {
             var commandResult = await _mediator.Send(command).ConfigureAwait(false);
             return commandResult.GetActionResult();
@@ -72,7 +71,7 @@ namespace Fsel.Course.Lms.Api.Controllers.V1i1
         [HttpPost("retake-course")]
         [ProducesResponseType(typeof(MethodResult<CourseResultModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
-        public async Task<IActionResult> RetakeCourse([FromBody] RetakeCourseResultByAdminCommand command)
+        public async Task<IActionResult> RetakeCourse([FromBody] RetakeCourseResultCommand command)
         {
             var commandResult = await _mediator.Send(command).ConfigureAwait(false);
             return commandResult.GetActionResult();
