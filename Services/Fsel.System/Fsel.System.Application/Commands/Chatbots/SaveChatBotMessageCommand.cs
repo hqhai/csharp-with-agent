9ba348d25a697a258fd04a4630be90482353f5da
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2025-08-20 14:39:03 +0700

    fixbug/fsel-6753: Update Save ChatBot Message

--- Services/Fsel.System/Fsel.System.Application/Commands/Chatbots/SaveChatBotMessageCommand.cs
+++ Services/Fsel.System/Fsel.System.Application/Commands/Chatbots/SaveChatBotMessageCommand.cs
@@ -212,8 +212,14 @@ namespace Fsel.System.Application.Commands.Chatbots
                 });
                 filePath = audioResult?.Content?.Result!;
 
-                //var reponse = await _mediator.Send(new ConvertFileWavCommand { File = filePath }, CancellationToken.None);
-                //filePath = reponse.Result ?? filePath;
+                try
+                {
+                    var reponse = await _mediator.Send(new ConvertFileWavCommand { File = filePath }, CancellationToken.None);
+                    filePath = reponse.Result ?? filePath;
+                }
+                catch
+                {
+                }
             }
             return filePath;
         }
