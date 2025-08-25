d2a5a3a9702cfd4d9f88065b24f1b0b1b51bddde
viktor <nguyentrongphuong@atlantic.edu.vn>
2025-08-25 15:30:40 +0700

    feature/FSEL-5882: fix bug delete unit and search with teacher

--- Services/Fsel.Course/Fsel.Course.Infrastructure/Repositories/UnitModuleRepository.cs
+++ Services/Fsel.Course/Fsel.Course.Infrastructure/Repositories/UnitModuleRepository.cs
@@ -4,13 +4,12 @@ namespace Fsel.Course.Infrastructure.Repositories
 {
     using AutoMapper;
     using Fsel.Core.Base;
-    using Fsel.Course.Domain.Entities;
     using Fsel.Course.Domain.Entities.V1i1;
     using Fsel.Course.Domain.IRepositories;
 
-    public class LessonModuleRepository : BaseRepository<LessonModule>, ILessonModuleRepository
+    public class UnitModuleRepository : BaseRepository<UnitModule>, IUnitModuleRepository
     {
-        public LessonModuleRepository(CourseDbContext dbContext, CourseReadDbContext readDbContext, AuthContext authContext, IMapper mapper)
+        public UnitModuleRepository(CourseDbContext dbContext, CourseReadDbContext readDbContext, AuthContext authContext, IMapper mapper)
             : base(dbContext, readDbContext, authContext, mapper)
         {
         }
