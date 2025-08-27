3be3fb7e29b8f62fd1eab7b1b6a9db3a3dffaf7a
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2025-08-27 09:06:49 +0700

    feature/fsel-6055: Update Model Test

--- Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/TestModels/TestModel.cs
+++ Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/TestModels/TestModel.cs
@@ -2,6 +2,7 @@
 
 namespace Fsel.Course.Domain.Models.EntityModels.TestModels
 {
+    using Fsel.Common.Enums;
     using Fsel.Core.Base.BaseModels;
 
     public class TestModel : BaseModel
@@ -12,6 +13,10 @@ namespace Fsel.Course.Domain.Models.EntityModels.TestModels
         public Guid LevelId { get; set; }
         public CategoryModel? Program { get; set; }
         public LevelModel? Level { get; set; }
+        public Guid OriginalId { get; set; }
+        public bool IsArchive { get; set; }
+        public int Version { get; set; }
+        public EnumVersionStatus VersionStatus { get; set; }
         public bool IsActive { get; set; }
         public IList<TestSectionModel> TestSections { get; set; } = new List<TestSectionModel>();
     }
