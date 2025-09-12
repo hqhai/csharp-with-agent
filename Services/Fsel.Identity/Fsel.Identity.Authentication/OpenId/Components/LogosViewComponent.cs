a784870e6b15241641c92da4bacb1d0584d2c9cc
Nguyễn Thành Đô <thanhdo@atlantic.edu.vn>
2025-09-12 16:27:36 +0700

    feature/fsel-5847: openid: add logo tenant

--- Services/Fsel.Identity/Fsel.Identity.Authentication/OpenId/Components/LogosViewComponent.cs
+++ Services/Fsel.Identity/Fsel.Identity.Authentication/OpenId/Components/LogosViewComponent.cs
@@ -2,12 +2,22 @@
 
 namespace Fsel.Identity.Authentication.OpenId.Components
 {
+    using Fsel.Core.Base.Interfaces;
     using Microsoft.AspNetCore.Mvc;
 
     public class LogosViewComponent : ViewComponent
     {
+        private readonly ITenantProvider _tenantProvider;
+
+        public LogosViewComponent(ITenantProvider tenantProvider)
+        {
+            _tenantProvider = tenantProvider;
+        }
+
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            var tenant = await _tenantProvider.GetCurrentTenantAsync();
+            ViewBag.LogoPath = tenant?.LogoPath;
             return View();
         }
     }
