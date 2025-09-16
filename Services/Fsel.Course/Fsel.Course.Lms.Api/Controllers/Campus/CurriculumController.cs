c75a74eefe40fe3f868f8c6851b8672eb00d207c
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2025-09-16 15:05:47 +0700

    feature/fsel-6868: update controller

--- Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/Campus/CurriculumController.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/Campus/CurriculumController.cs
@@ -6,8 +6,10 @@ namespace Fsel.Course.Lms.Api.Controllers.Campus
     using Fsel.Common.ActionResults;
     using Fsel.Common.Attributes;
     using Fsel.Common.Constants;
+    using Fsel.Core.Base.BaseModels;
     using Fsel.Course.Domain.Models.EntityModels;
     using Fsel.Course.Lms.Application.Commands.CurriculumCmd;
+    using Fsel.Course.Lms.Application.Queries.CourseQuery.V1i1;
     using Fsel.Course.Lms.Application.Queries.CurriculumQuery;
     using Fsel.Shared.Attributes;
     using Fsel.Shared.Constants;
@@ -125,6 +127,18 @@ namespace Fsel.Course.Lms.Api.Controllers.Campus
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
         [Permission(CurriculumManagement.View)]
         public async Task<IActionResult> SearchStudents([FromQuery] SearchStudentsByCurriculumIdQuery query)
+        {
+            var commandResult = await _mediator.Send(query).ConfigureAwait(false);
+            return commandResult.GetActionResult();
+        }
+
+        /// <summary>
+        /// search
+        /// </summary>
+        [HttpGet("search")]
+        [ProducesResponseType(typeof(MethodResult<PagingItemsModel<CourseSearchModel>>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> Search([FromQuery] SearchCourseQuery query)
         {
             var commandResult = await _mediator.Send(query).ConfigureAwait(false);
             return commandResult.GetActionResult();
