db0e758ed751922d998687a9bee0c8bad132fe65
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2025-09-26 10:45:28 +0700

    feature/fsel-7090: update HomeWorkConfigModel

--- Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/HomeWorkConfigModel.cs
+++ Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/HomeWorkConfigModel.cs
@@ -15,8 +15,8 @@ namespace Fsel.Course.Domain.Models.EntityModels
         public DateTime EndDate { get; set; }
         public Guid HomeWorkId { get; set; }
         public Guid CurriculumId { get; set; }
-        public EnumCourseLevel? CourseLevel { get; set; }
-        public EnumCourseSkill? CourseSkill { get; set; }
+        public EnumCourseLevel CourseLevel { get; set; }
+        public EnumCourseSkill CourseSkill { get; set; }
 
         public EnumCourseType CourseType
         {
