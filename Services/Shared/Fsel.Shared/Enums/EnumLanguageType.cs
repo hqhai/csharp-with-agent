26a965c43a5645e225be602a3a399e6f9fd9d1c5
Đinh Văn Tân - FSEL <dinhtan@atlantic.edu.vn>
2025-10-01 16:23:33 +0700

    feture/FSEL-6913_ruby_text: sửa enum

--- Services/Shared/Fsel.Shared/Enums/EnumLanguageType.cs
+++ Services/Shared/Fsel.Shared/Enums/EnumLanguageType.cs
@@ -7,14 +7,14 @@ namespace Fsel.Shared.Enums
     public enum EnumLanguageType
     {
         [Description("Tiếng Nhật")]
-        ja,
+        Ja,
         [Description("Tiếng Trung")]
-        zh,
+        Zh,
         [Description("Tiếng Hàn")]
-        ko,
+        Ko,
         [Description("Tiếng Latin")]
-        latin,
+        Latin,
         [Description("Không xác định")]
-        unknow
+        Unknow
     }
 }
