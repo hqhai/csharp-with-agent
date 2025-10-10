effc261919861fd642d7d587a17289cb279cf8f3
Nguyễn Thành Đô <thanhdo170698@gmail.com>
2025-10-10 18:01:36 +0700

    openid: add UseForwardedHeaders

--- Services/Fsel.Identity/Fsel.Identity.Authentication/Program.cs
+++ Services/Fsel.Identity/Fsel.Identity.Authentication/Program.cs
@@ -92,6 +92,7 @@ builder.AddMassTransit(appSetting);
 
 //App config
 var app = builder.Build();
+app.UseForwardedHeaders();
 app.UseLanguages();
 app.UseStaticFiles();
 app.UseCertificateForwarding();
