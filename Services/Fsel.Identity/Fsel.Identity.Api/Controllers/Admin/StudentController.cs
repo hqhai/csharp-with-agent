f7cff2ac1129b86530c873804bbc911a4a23f04d
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2025-11-17 09:59:37 +0700

    fixbug/fsel-7614: Update FixBug Author

--- Services/Fsel.Identity/Fsel.Identity.Api/Controllers/Admin/StudentController.cs
+++ Services/Fsel.Identity/Fsel.Identity.Api/Controllers/Admin/StudentController.cs
@@ -296,7 +296,7 @@ namespace Fsel.Identity.Api.Controllers.Admin
         [HttpGet("get-parent-by-student-id/{studentId}")]
         [ProducesResponseType(typeof(MethodResult<ParentProfileModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
-        [Permission(permissionCodes: new[] { StudentManagement.View, SchoolStudentManagement.View })]
+        [Permission(permissionCodes: new[] { StudentManagement.View, SchoolStudentManagement.View, StudentProgressWeeklyManagement.View })]
         public async Task<IActionResult> GetParentByStudentId([FromRoute] Guid studentId)
         {
             var queryResult = await _mediator.Send(new GetParentByStudentIdQuery { StudentId = studentId }).ConfigureAwait(false);
@@ -309,7 +309,7 @@ namespace Fsel.Identity.Api.Controllers.Admin
         [HttpGet("get-otp-for-student")]
         [ProducesResponseType(typeof(MethodResult<OTPModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
-        [Permission(permissionCodes: new[] { StudentManagement.View, SchoolStudentManagement.View })]
+        [Permission(permissionCodes: new[] { StudentManagement.View, SchoolStudentManagement.View, StudentProgressWeeklyManagement.View })]
         public async Task<IActionResult> GetOTPForStudent([FromQuery] GetOTPForStudentQuery query)
         {
             var queryResult = await _mediator.Send(query).ConfigureAwait(false);
