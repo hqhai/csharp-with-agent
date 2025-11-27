148be923a6d3a44a3a33988ff511b11a33423fbd
Hà Quang Hải <quanghai@atlantic.edu.vn>
2025-11-27 19:05:49 +0700

    update/fsel-5301: set schemaname

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/AiCmd/SubmitClassforumAICommand.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Commands/AiCmd/SubmitClassforumAICommand.cs
@@ -41,6 +41,7 @@ namespace Fsel.Course.Lms.Application.Commands.AiCmd
         private readonly ISenderService _senderService;
         private const int Max_Time_Retry = 4;
         private const int _intervalRetryTime = 30;
+        private const string NameSchema = "criteria_schema";
         private readonly IUserService _userService;
         private readonly ILogger<SubmitAIResponseCommandHandler> _logger;
 
@@ -145,7 +146,8 @@ namespace Fsel.Course.Lms.Application.Commands.AiCmd
                         SettingTopP = request.SettingTopP,
                         SystemRoleAlConfig = request.SystemRoleAlConfig,
                         UserAIConfig = userAiConfig,
-                        Text = successCriteriaSchema
+                        Text = successCriteriaSchema,
+                        NameSchema = NameSchema
                     }, cancellationToken).ConfigureAwait(false);
 
                     aIResponse = Shared.Helpers.StringHelper.RemoveMarkdownFromJson(aIResponse ?? string.Empty);
