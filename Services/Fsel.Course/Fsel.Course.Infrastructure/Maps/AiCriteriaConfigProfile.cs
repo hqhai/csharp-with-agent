78015c1690921c013186d7668c9481d916b2d0e3
Đinh Văn Tân - FSEL <dinhtan@atlantic.edu.vn>
2025-11-29 16:08:54 +0700

    fsel-7330-resolve-comment

--- Services/Fsel.Course/Fsel.Course.Infrastructure/Maps/AiCriteriaConfigProfile.cs
+++ Services/Fsel.Course/Fsel.Course.Infrastructure/Maps/AiCriteriaConfigProfile.cs
@@ -13,12 +13,14 @@ namespace Fsel.Course.Infrastructure.Maps
         public AiCriteriaConfigProfile()
         {
             CreateMap<CreateAiCriteriaConfigCommandModel, AICriteriaConfigs>()
-                .ForMember(d => d.JsonConfig,      o => o.MapFrom(s => s.JsonConfig))
-                .ForMember(d => d.SettingAiJson,   o => o.Ignore())
+                .ForMember(d => d.Id, opt => opt.Ignore())
+                .ForMember(d => d.JsonConfig, o => o.MapFrom(s => s.JsonConfig))
+                .ForMember(d => d.SettingAiJson, o => o.Ignore())
                 .IgnoreAllNonExisting();
             CreateMap<UpdateAiCriteriaCommand, AICriteriaConfigs>()
-                .ForMember(d => d.JsonConfig,      o => o.MapFrom(s => s.JsonConfig))
-                .ForMember(d => d.SettingAiJson,   o => o.Ignore())
+                .ForMember(d => d.JsonConfig, o => o.MapFrom(s => s.JsonConfig))
+                .ForMember(d => d.SettingAiJson, o => o.Ignore())
+                .ForMember(d => d.AiPromptManager, o => o.Ignore())
                 .ForMember(d => d.AiPromptManager, o => o.Ignore())
                 .IgnoreAllNonExisting();
             CreateMap<AICriteriaConfigsModel, AICriteriaConfigs>().IgnoreAllNonExisting();
@@ -28,6 +30,16 @@ namespace Fsel.Course.Infrastructure.Maps
             CreateMap<AICriteriaConfigs, AiCriteriaModel>().IgnoreAllNonExisting();
             CreateMap<AICriteriaConfigs, CreateAiCriteriaConfigCommandModel>().IgnoreAllNonExisting();
             CreateMap<UpdateSettingAiFeatureCommandModel, AICriteriaConfigs>().IgnoreAllNonExisting();
+            CreateMap<CreateOrUpdateAiCriteriaCommand, AICriteriaConfigs>()
+                .ForMember(d => d.AiPromptManagerId, opt => opt.MapFrom(s => s.AiPromptManagerId))
+                .ForMember(d => d.SettingTemperature, opt => opt.MapFrom(s => s.SettingTemperature))
+                .ForMember(d => d.SettingWordMaxLength, opt => opt.MapFrom(s => s.SettingWordMaxLength))
+                .ForMember(d => d.SettingTopP, opt => opt.MapFrom(s => s.SettingTopP))
+                .ForMember(d => d.SettingFrequency, opt => opt.MapFrom(s => s.SettingFrequency))
+                .ForMember(d => d.SettingPresence, opt => opt.MapFrom(s => s.SettingPresence))
+                .ForMember(d => d.MaximumNumber, opt => opt.MapFrom(s => s.MaximumNumber))
+                .IgnoreAllNonExisting();
+
         }
     }
 }
