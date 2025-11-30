68ce508bd17fd34843be359f4412407eeff3dc1d
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2025-11-30 16:23:41 +0700

    feature/fsel-7739: Update Field Model

---
+++ Services/Fsel.Course/Fsel.Course.Domain/Enums/EnumVectorLibrary.cs
@@ -0,0 +1,10 @@
+// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Course.Domain.Enums
+{
+    public enum EnumVectorLibrary
+    {
+        Kvg = 1,   // KanjiVG Library
+        Svg = 2    // SVG Vector Library
+    }
+}
--- Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/KeyboardTextModel.cs
+++ Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/KeyboardTextModel.cs
@@ -3,13 +3,13 @@
 namespace Fsel.Course.Domain.Models.EntityModels
 {
     using Fsel.Core.Base.BaseModels;
+    using Fsel.Course.Domain.Enums;
 
     public class KeyboardTextModel : BaseModel
     {
         public string? Name { get; set; }
-
+        public EnumVectorLibrary VectorLibrary { get; set; }
         public int Unicode { get; set; }
-
         public string? FilePath { get; set; }
     }
 }
