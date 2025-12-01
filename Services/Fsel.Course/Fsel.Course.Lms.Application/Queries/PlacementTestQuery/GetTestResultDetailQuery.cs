645521cdfdad168311d713789cd1496408a21ec4
viktor <nguyentrongphuong@atlantic.edu.vn>
2025-12-01 14:39:22 +0700

    feature/FSEL-6878: add get test result detail

---
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/PlacementTestQuery/GetTestResultDetailQuery.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Atlantic. All rights reserved.
+
+namespace Fsel.Course.Lms.Application.Queries.PlacementTestQuery
+{
+    using Common.ActionResults;
+    using Core.Base.Interfaces;
+    using Domain.Entities.TestConfigs;
+    using Domain.Models.EntityModels;
+    using Domain.Models.EntityModels.PlacementTestModels;
+    using MediatR;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    using Services.ApplicationServices;
+    using Services.ApplicationServices.Aggregates;
+
+    public class GetTestResultDetailQuery : IRequest<MethodResult<TestStateModel>>
+    {
+        public Guid TestResultId { get; set; }
+    }
+
+    public class GetTestResultDetailQueryHandler : IRequestHandler<GetTestResultDetailQuery, MethodResult<TestStateModel>>
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ITestService _testService;
+
+        public GetTestResultDetailQueryHandler(ITestService testService, IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            _testService = testService;
+        }
+
+        public async Task<MethodResult<TestStateModel>> Handle(GetTestResultDetailQuery request, CancellationToken cancellationToken)
+        {
+            var testResult = await _testService.LoadHierachicalTestResult(x => x.Id == request.TestResultId);
+            if (testResult == null)
+            {
+                return new MethodResult<TestStateModel>();
+            }
+            var testResultComposite = new TestResultComposite { Result = testResult, ServiceProvider = _serviceProvider };
+            testResultComposite.GenerateChildren();
+            var testResultState = testResultComposite.ExportState();
+            return new MethodResult<TestStateModel>
+            {
+                Result = testResultState as TestStateModel,
+            };
+        }
+    }
+}
