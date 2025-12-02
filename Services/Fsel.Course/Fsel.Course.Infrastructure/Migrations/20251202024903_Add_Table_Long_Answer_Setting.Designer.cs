f874c80c2725747625c0393448567ff03a1b855d
Đinh Văn Tân - FSEL <dinhtan@atlantic.edu.vn>
2025-12-02 09:53:32 +0700

    feature/fsel-7776-update-table-long-answer-for-class-forum

--- Services/Fsel.Course/Fsel.Course.Infrastructure/Migrations/20251202024903_Add_Table_Long_Answer_Setting.Designer.cs
+++ Services/Fsel.Course/Fsel.Course.Infrastructure/Migrations/20251202024903_Add_Table_Long_Answer_Setting.Designer.cs
@@ -4,6 +4,7 @@ using Fsel.Course.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fsel.Course.Infrastructure.Migrations
 {
     [DbContext(typeof(CourseDbContext))]
-    partial class CourseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251202024903_Add_Table_Long_Answer_Setting")]
+    partial class Add_Table_Long_Answer_Setting
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
