6ec0ea052fc69edd6aa97f872775b4183badcc74
Đinh Văn Tân - FSEL <dinhtan@atlantic.edu.vn>
2025-12-15 16:06:40 +0700

    feature-fsel-7802-resolve-comment

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Services/ApplicationServices/Aggregates/TestAnswerLeaf.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Services/ApplicationServices/Aggregates/TestAnswerLeaf.cs
@@ -33,7 +33,7 @@ namespace Fsel.Course.Lms.Application.Services.ApplicationServices.Aggregates
             {
                 QuestionId = TestAnswer.QuestionId,
                 TestAnswerId = TestAnswer.Id,
-                Answer = new AnswerModel { Answer = TestAnswer.Answer, CorrectCount = TestAnswer.CorrectCount, IsCorrect = TestAnswer.IsCorrect, },
+                Answer = new AnswerModel { Answer = TestAnswer.Answer, CorrectCount = TestAnswer.CorrectCount, IsCorrect = TestAnswer.IsCorrect },
                 Status = TestAnswer.Status == EnumAnswerStatus.Done ? EnumResultStatus.Done : EnumResultStatus.Process,
                 UpdatedDate = TestAnswer?.UpdatedDate ?? TestAnswer?.CreatedDate
             };
