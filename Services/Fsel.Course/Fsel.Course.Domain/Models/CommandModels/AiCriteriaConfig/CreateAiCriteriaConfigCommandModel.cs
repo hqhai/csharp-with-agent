e145df98da189566a32744462df47e146ddb261f
Đinh Văn Tân - FSEL <dinhtan@atlantic.edu.vn>
2025-12-15 11:25:12 +0700

    update

--- Services/Fsel.Course/Fsel.Course.Domain/Models/CommandModels/AiCriteriaConfig/CreateAiCriteriaConfigCommandModel.cs
+++ Services/Fsel.Course/Fsel.Course.Domain/Models/CommandModels/AiCriteriaConfig/CreateAiCriteriaConfigCommandModel.cs
@@ -14,7 +14,7 @@ namespace Fsel.Course.Domain.Models.CommandModels.AiCriteriaConfig
         public EnumCriteriaAi? TypeCriteriaAi { get; set; }
         public EnumSubFeatureType? SubFeatureType { get; set; }
         public EnumFeatureMultiple? FeatureMultiple { get; set; }
-        public EnumDefaultType DefaultType { get; set; } = EnumDefaultType.Default;
+        public EnumDefaultType DefaultType { get; set; }
         public string? UserRole { get; set; }
         public string? SettingAiConfig { get; set; }
         public object? JsonConfig { get; set; }
