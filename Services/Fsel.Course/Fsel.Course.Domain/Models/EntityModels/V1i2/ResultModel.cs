134a6c12cea3f78dec585f1d7f7d87a096339221
viktor <nguyentrongphuong@atlantic.edu.vn>
2026-01-02 15:11:03 +0700

    FSEL-7942: add score information when get modules of lesson

--- Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/V1i2/ResultModel.cs
+++ Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/V1i2/ResultModel.cs
@@ -3,11 +3,13 @@
 namespace Fsel.Course.Domain.Models.EntityModels.V1i2
 {
     using Fsel.Core.Base.BaseModels;
+    using Fsel.Course.Domain.Entities.SkillScoresConfigs;
     using Fsel.Course.Domain.Enums;
 
     public class ResultModel : BaseModel
     {
         public Guid StudentId { get; set; }
         public EnumResultStatus Status { get; set; }
+        public IList<SkillScores>? SkillScores { get; set; }
     }
 }
