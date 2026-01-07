80484cacfb7b94b534f70c9ab97b0ebdd04c6209
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2026-01-07 13:24:16 +0700

    fixbug/fsel: Update Model Test

--- Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/TestModels/TestModel.cs
+++ Services/Fsel.Course/Fsel.Course.Domain/Models/EntityModels/TestModels/TestModel.cs
@@ -10,6 +10,7 @@ namespace Fsel.Course.Domain.Models.EntityModels.TestModels
     {
         public string? Name { get; set; }
         public string? Code { get; set; }
+        public string? Description { get; set; }
         public Guid ProgramId { get; set; }
         public Guid LevelId { get; set; }
         public EnumScoringFormulaType ScoringFormulaType { get; set; }
