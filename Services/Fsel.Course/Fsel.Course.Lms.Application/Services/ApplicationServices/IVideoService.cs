ef56b016d7326ff0faa84a0842a7a6667b82f99a
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2026-01-21 17:49:13 +0700

    feature/fsel-8095: Update HomeWork

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Services/ApplicationServices/IVideoService.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Services/ApplicationServices/IVideoService.cs
@@ -179,7 +179,6 @@ namespace Fsel.Course.Lms.Application.Services.ApplicationServices
         private async Task<Dictionary<Guid, VideoTimeCodeResult?>> LoadVideoTimeCodeResultDictAsync(VideoResult videoResult)
         {
             var videoTimeCodeResults = await _videoTimeCodeResultRepository.ReadQueryable.Where(x => x.VideoResultId == videoResult.Id && x.CreatedDate >= videoResult.CreatedDate)
-                                                                           .Where(x => !(videoResult.Status == EnumResultStatus.Done) || x.UpdatedDate <= videoResult.UpdatedDate)
                                                                            .ToListAsync();
             return videoTimeCodeResults.ToDictionary(x => x.VideoTimeCodeId, x => (VideoTimeCodeResult?)x);
         }
