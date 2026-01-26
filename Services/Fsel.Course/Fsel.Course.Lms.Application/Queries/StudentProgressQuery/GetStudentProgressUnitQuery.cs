0fb1c5b3f72ed238c69b8669b4596912b66cc6ba
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2026-01-26 10:57:02 +0700

    feature/fsel-8165: update code

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/StudentProgressQuery/GetStudentProgressUnitQuery.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/StudentProgressQuery/GetStudentProgressUnitQuery.cs
@@ -33,7 +33,6 @@ namespace Fsel.Course.Lms.Application.Queries.StudentProgressQuery
         private readonly IUnitRepository _unitRepository;
         private readonly IUserService _userService;
         private readonly ISystemService _systemService;
-        private readonly ICourseResultRepository _courseResultRepository;
         private readonly ILearningService _learningService;
 
         public GetStudentProgressUnitQueryHandler(ICourseRepository courseRepository, ManagerProgressHelper managerProgressHelper, IUnitRepository unitRepository, IUserService userService, ISystemService systemService, ICourseResultRepository courseResultRepository, ILearningService learningService)
