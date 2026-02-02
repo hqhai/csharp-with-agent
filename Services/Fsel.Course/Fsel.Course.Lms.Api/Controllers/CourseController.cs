8b55a32b284e5e31ef0c43441c2e2d200153abb6
Nguyễn Hữu Khoa <huukhoa@atlantic.edu.vn>
2026-02-02 09:17:05 +0700

    feature/fsel-8165: add permission

--- Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/CourseController.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Api/Controllers/CourseController.cs
@@ -135,7 +135,7 @@ namespace Fsel.Course.Lms.Api.Controllers
         }
 
         [HttpPost("select-course-by-choose-level")]
-        [Permission(role: nameof(EnumRole.Student))]
+        [Permission(roles: new string[] { nameof(EnumRole.Student), nameof(EnumRole.StudentCampus) })]
         public async Task<IActionResult> GetLevelsBySelectedProgram([FromBody] ChooseCourseByLevelCommand request)
         {
             var queryResult = await _mediator.Send(request).ConfigureAwait(false);
@@ -143,16 +143,15 @@ namespace Fsel.Course.Lms.Api.Controllers
         }
 
         [HttpPost("change-subject")]
-        [Permission(role: nameof(EnumRole.Student))]
+        [Permission(roles: new string[] { nameof(EnumRole.Student), nameof(EnumRole.StudentCampus) })]
         public async Task<IActionResult> ChangeSubject([FromBody] ChangeSubjectCommand request)
         {
             var queryResult = await _mediator.Send(request).ConfigureAwait(false);
             return queryResult.GetActionResult();
         }
 
-
         [HttpGet("get-levels-for-change")]
-        [Permission(role: nameof(EnumRole.Student))]
+        [Permission(roles: new string[] { nameof(EnumRole.Student), nameof(EnumRole.StudentCampus) })]
         public async Task<IActionResult> GetLevelsForChange()
         {
             var queryResult = await _mediator.Send(new GetLevelsForChangeQuery()).ConfigureAwait(false);
