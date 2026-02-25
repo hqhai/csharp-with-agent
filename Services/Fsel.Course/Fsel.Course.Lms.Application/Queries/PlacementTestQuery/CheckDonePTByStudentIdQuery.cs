4c6df5c7a160b72a93b69804e8d7d2251a216dea
viktor <nguyentrongphuong@atlantic.edu.vn>
2026-02-25 16:24:30 +0700

    add check done pt

--- Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/PlacementTestQuery/CheckDonePTByStudentIdQuery.cs
+++ Services/Fsel.Course/Fsel.Course.Lms.Application/Queries/PlacementTestQuery/CheckDonePTByStudentIdQuery.cs
@@ -7,11 +7,13 @@ namespace Fsel.Course.Lms.Application.Queries.PlacementTestQuery
     using CourseChangeQuery;
     using Domain.Models.EntityModels.PlacementTestModels;
     using Fsel.Common.ActionResults;
+    using Fsel.Core.Base;
     using Fsel.Core.Base.Interfaces;
     using Fsel.Course.Domain.Entities.TestConfigs;
     using Fsel.Course.Domain.Enums;
     using Fsel.Course.Domain.IRepositories;
     using Fsel.Course.Domain.Models.EntityModels.UserNavigationActionModels;
+    using Fsel.Course.Lms.Application.Services.UserServices;
     using MediatR;
     using Microsoft.AspNetCore.Http;
     using Microsoft.EntityFrameworkCore;
@@ -26,19 +28,27 @@ namespace Fsel.Course.Lms.Application.Queries.PlacementTestQuery
         private readonly IRepository<TestGroupResult> _testGroupResult;
         private readonly ICourseResultRepository _courseResultRepository;
         private readonly IMediator _mediator;
+        private readonly IUserService _userService;
 
-        public CheckDonePtByStudentIdHandler(IRepository<TestGroupResult> testGroupResult, ICourseResultRepository courseResultRepository, IMediator mediator)
+        public CheckDonePtByStudentIdHandler(IRepository<TestGroupResult> testGroupResult,
+            ICourseResultRepository courseResultRepository,
+            IUserService userService,
+            IMediator mediator)
         {
             _testGroupResult = testGroupResult;
             _courseResultRepository = courseResultRepository;
             _mediator = mediator;
+            _userService = userService;
         }
 
         public async Task<MethodResult<PtStateModel>> Handle(CheckDonePtByStudentIdQuery request, CancellationToken cancellationToken)
         {
             ArgumentNullException.ThrowIfNull(request);
-
-            var getUserNavigationResult = await _mediator.Send(new GetUserNavigationQuery(), cancellationToken);
+            var student = await _userService.GetUserByStudentId(request.StudentId);
+            var getUserNavigationResult = await _mediator.Send(new GetUserNavigationQuery
+            {
+                UserId = student?.Content?.Result?.UserId
+            }, cancellationToken);
             if (!getUserNavigationResult.IsOK || getUserNavigationResult.Result == null)
             {
                 return new MethodResult<PtStateModel> { StatusCode = StatusCodes.Status400BadRequest, };
