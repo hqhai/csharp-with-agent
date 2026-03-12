0d3b531f88810f955da4f1321180909debb4ca47
Phan Minh Phuc <minhphuc@atlantic.edu.vn>
2026-03-12 17:26:06 +0700

    feature/fsel-8410: Add Data stroke Keyboard Text

--- Services/Fsel.Course/Fsel.Course.Lcms.Api/Controllers/KeyboardTextController.cs
+++ Services/Fsel.Course/Fsel.Course.Lcms.Api/Controllers/KeyboardTextController.cs
@@ -40,6 +40,18 @@ namespace Fsel.Course.Lcms.Api.Controllers
         }
 
         /// <summary>
+        /// Keyboard Text
+        /// </summary>
+        [HttpPost("crawler-data-file")]
+        [ProducesResponseType(typeof(MethodResult<bool>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(VoidMethodResult), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> CrawlerDataKeyboardText([FromBody] CrawlerDataKeyboardTextFileCommand command)
+        {
+            MethodResult<bool> queryResult = await _mediator.Send(command).ConfigureAwait(false);
+            return queryResult.GetActionResult();
+        }
+
+        /// <summary>
         /// Get Keyboard Text
         /// </summary>
         [HttpGet]
